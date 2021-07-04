b37d11cfb7b60f6857e400aec63d2e1035bf6efa
X.Q. Chen <31237954+brenner8023@users.noreply.github.com>
2021-07-04 18:33:21 +0800

    feat: 增加checkbox
    描述：
    - 新增checkbox组件和checkboxGroup组件
    
    自测：
    - npm run test通过
    - demo效果符合预期

---
+++ devui/checkbox/__tests__/checkbox.spec.ts
@@ -0,0 +1,146 @@
+import { mount } from '@vue/test-utils';
+import { ref, nextTick } from 'vue';
+import DCheckbox from '../src/checkbox';
+
+describe('checkbox', () => {
+  it('checkbox render work', async () => {
+    const checked = ref(true);
+    const wrapper = mount({
+      components: { DCheckbox },
+      template: `<d-checkbox v-model:checked="checked" value="a">1024</d-checkbox>`,
+      setup () {
+        return {
+          checked
+        };
+      }
+    });
+    const container = wrapper.find('.devui-checkbox');
+
+    expect(wrapper.text()).toEqual('1024');
+    expect(container.exists()).toBeTruthy();
+    expect(container.classes()).toContain('active');
+
+    checked.value = false;
+    await nextTick();
+
+    expect(container.classes()).not.toContain('active');
+    expect(container.classes()).toContain('unchecked');
+  });
+
+  it('checkbox title work', async () => {
+    const wrapper = mount(DCheckbox, {
+      props: {
+        value: 'a',
+        label: '1314'
+      }
+    });
+
+    expect(wrapper.text()).toEqual('1314');
+    const label = wrapper.find('label');
+    expect(label.attributes('title')).toEqual('1314');
+
+    await wrapper.setProps({
+      title: '520',
+      label: '1314'
+    });
+    expect(label.attributes('title')).toEqual('520');
+
+    await wrapper.setProps({
+      isShowTitle: false
+    });
+    expect(label.attributes('title')).toEqual('');
+  });
+
+  it('checkbox showAnimation work', async () => {
+    const wrapper = mount(DCheckbox, {
+      props: {
+        value: 'a'
+      }
+    });
+
+    expect(wrapper.findAll('.devui-no-animation').length).toBe(0);
+
+    await wrapper.setProps({
+      showAnimation: false
+    });
+    expect(wrapper.findAll('.devui-no-animation').length).toBe(2);
+  });
+
+  it('checkbox disabled work', async () => {
+    const onChange = jest.fn();
+    const wrapper = mount(DCheckbox, {
+      props: {
+        value: 'a',
+        disabled: true,
+        onChange
+      }
+    });
+    const label = wrapper.find('label');
+
+    await label.trigger('click');
+    expect(wrapper.find('.devui-checkbox').classes()).toContain('disabled');
+    expect(onChange).toBeCalledTimes(0);
+
+    await wrapper.setProps({
+      disabled: false
+    });
+    await label.trigger('click');
+    expect(wrapper.find('.devui-checkbox').classes()).not.toContain('disabled');
+    expect(onChange).toBeCalledTimes(1);
+  });
+
+  it('checkbox halfchecked work', async () => {
+    const wrapper = mount(DCheckbox, {
+      props: {
+        value: '555',
+        halfchecked: false
+      }
+    });
+
+    const container = wrapper.find('.devui-checkbox');
+    expect(container.classes()).not.toContain('halfchecked');
+    expect(container.find('.devui-checkbox-default-background').exists()).toBe(true);
+
+    await wrapper.setProps({
+      halfchecked: true
+    });
+    expect(container.classes()).toContain('halfchecked');
+    expect(container.find('.devui-checkbox-default-background').exists()).toBe(false)
+  });
+
+  it('checkbox beforeChange work', async () => {
+    const beforeChange = jest.fn(() => false);
+    const onChange = jest.fn();
+    const checked = ref(false);
+    const wrapper = mount({
+      components: { DCheckbox },
+      template: `
+        <d-checkbox
+          v-model:checked="checked"
+          value="666"
+          @change="onChange"
+          :before-change="beforeChange">
+          666
+        </d-checkbox>`,
+      setup () {
+        return {
+          beforeChange,
+          onChange,
+          checked
+        };
+      }
+    });
+
+    const label = wrapper.find('label');
+    await label.trigger('click');
+    expect(beforeChange).toBeCalledTimes(1);
+    expect(onChange).toBeCalledTimes(0);
+    expect(checked.value).toBe(false);
+
+    beforeChange.mockReturnValue(true);
+    await label.trigger('click');
+    expect(beforeChange).toBeCalledTimes(2);
+    expect(onChange).toBeCalledTimes(1);
+    expect(checked.value).toBe(true);
+  });
+});
---
+++ devui/checkbox/src/use-checkbox.ts
@@ -0,0 +1,106 @@
+import { PropType, InjectionKey, Ref, ExtractPropTypes } from 'vue';
+
+type Direction = 'row' | 'column';
+
+const commonProps = {
+  name: {
+    type: String,
+    default: undefined
+  },
+  isShowTitle: {
+    type: Boolean,
+    default: true
+  },
+  color: {
+    type: String,
+    default: undefined
+  },
+  showAnimation: {
+    type: Boolean,
+    default: true
+  },
+  disabled: {
+    type: Boolean,
+    default: false
+  },
+  beforeChange: {
+    type: Function as PropType<(isChecked: boolean, v: string) => boolean | Promise<boolean>>,
+    default: undefined
+  }
+} as const;
+
+export const checkboxProps = {
+  ...commonProps,
+  halfchecked: {
+    type: Boolean,
+    default: false
+  },
+  checked: {
+    type: Boolean,
+    default: false
+  },
+  value: {
+    type: String,
+    required: true
+  },
+  label: {
+    type: String,
+    default: undefined
+  },
+  title: {
+    type: String,
+    default: undefined
+  },
+  'onUpdate:checked' : {
+    type: Function as PropType<(v: boolean) => void>,
+    default: undefined
+  },
+  onChange: {
+    type: Function as PropType<(v: boolean) => void>,
+    default: undefined
+  }
+} as const;
+
+export type CheckboxProps = ExtractPropTypes<typeof checkboxProps>;
+
+export const checkboxGroupProps = {
+  ...commonProps,
+  value: {
+    type: Array as PropType<string[]>,
+    required: true
+  },
+  direction: {
+    type: String as PropType<Direction>,
+    default: 'column'
+  },
+  itemWidth: {
+    type: Number,
+    default: undefined
+  },
+  options: {
+    type: Array as PropType<({value: string;} & Partial<CheckboxProps>)[]>,
+    default: () => []
+  },
+  onChange: {
+    type: Function as PropType<(v: string[]) => void>,
+    default: undefined
+  },
+  'onUpdate:value': {
+    type: Function as PropType<(v: string[]) => void>,
+    default: undefined
+  }
+} as const;
+
+interface checkboxGroupInjection {
+  disabled: Ref<boolean>
+  isShowTitle: Ref<boolean>
+  color: Ref<string | undefined>
+  showAnimation: Ref<boolean>
+  beforeChange: undefined | ((isChecked: boolean, v: string) => boolean | Promise<boolean>)
+  toggleGroupVal: (v: string) => void
+  isItemChecked: (v: string) => boolean
+  itemWidth: Ref<number | undefined>
+  direction: Ref<Direction>
+}
+
+export const checkboxGroupInjectionKey: InjectionKey<checkboxGroupInjection> = Symbol('d-checkbox-group');
