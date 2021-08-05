96f28730ed2cb49dbd6d2001b97b8aca6d6b2f84
kagol <kagol@sina.com>
2021-08-06 00:34:01 +0800

    feat: 补充vitepress组件文档

---
+++ sites/.vitepress/devui-theme/components/Home.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import HomeHero from './HomeHero.vue'
+import HomeFeatures from './HomeFeatures.vue'
+import HomeFooter from './HomeFooter.vue'
+</script>
+
+<template>
+  <main class="home" aria-labelledby="main-title">
+    <HomeHero />
+    <slot name="hero" />
+    <HomeFeatures />
+    <div class="home-content">
+      <Content />
+    </div>
+    <slot name="features" />
+    <HomeFooter />
+    <slot name="footer" />
+  </main>
+</template>
+
+<style scoped>
+.home {
+  padding-top: var(--header-height);
+}
+
+.home-content {
+  max-width: 960px;
+  margin: 0px auto;
+  padding: 0 1.5rem;
+}
+
+@media (max-width: 720px) {
+  .home-content {
+    max-width: 392px;
+    padding: 0;
+  }
+}
+</style>
---
+++ sites/.vitepress/devui-theme/index.d.ts
@@ -0,0 +1,8 @@
+import './styles/vars.css';
+import './styles/layout.css';
+import './styles/code.css';
+import './styles/custom-blocks.css';
+import './styles/sidebar-links.css';
+import { Theme } from 'vitepress';
+declare const theme: Theme;
+export default theme;
