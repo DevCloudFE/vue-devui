97349edb0a91eb4da6abb6b02a82cdb4cc280144
vergil <821006222@qq.com>
2021-08-09 16:09:02 +0800

    feat: 修改ts文件

--- sites/.vitepress/config/sidebar.ts
+++ sites/.vitepress/config/sidebar.ts
@@ -35,6 +35,7 @@ const sidebar = {
       text: '数据展示',
       children: [
         { text: 'Avatar 头像', link: '/components/avatar/' },
+        { text: 'Card 卡片', link: '/components/card/' },
       ]
     }
   ],
