1b1091487d4578192f2a9960a9d00189d072adb7
Marvin <imnull@outlook.com>
2021-08-09 08:07:21 +0800

    feat: code clean up

--- devui/datepicker/components/calendar/index.tsx
+++ devui/datepicker/components/calendar/index.tsx
@@ -1,8 +1,4 @@
 import { defineComponent } from 'vue'
-import { getMonthWeeklyDays, WEEK_DAYS, invokeCallback, compareDate } from './utils'
-import { handleDateEnter, cellClassName, trigEvent } from './helper'
-import CalendaToolbar, { CalendaToolbarProps } from './components/toobar'
-import { TProps, TDatePanelProps } from './types'
 import CalendaDatePanel, { CalendaDatePanelProps } from './components/panel'
 
 import './index.css'
@@ -22,18 +18,12 @@ export default defineComponent({
       if(!(next instanceof Date)) {
         next = new Date(current.getFullYear(), current.getMonth() + 1, 1)
       }
-      const P = props as TProps
       return (
         <div class="calendar-container">
-          <CalendaDatePanel
-            { ...P }
-            pos={0}
-            current={current}
-            compare={next}
-          />
+          <CalendaDatePanel { ...props } pos={0} current={current} compare={next} />
           {
             props.type === 'range'
-            ? <CalendaDatePanel { ...P } pos={1} current={next} compare={current} /> 
+            ? <CalendaDatePanel { ...props } pos={1} current={next} compare={current} /> 
             : null
           }
         </div>
