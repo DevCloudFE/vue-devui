abda9aad39cf0c3c0eb5421a2fb15326a1a3495b
ElsaOOo <yjjjng0901@126.com>
2021-08-11 17:53:13 +0800

    feat: 添加rate组件文档

--- devui/rate/index.ts
+++ devui/rate/index.ts
@@ -1,16 +1,16 @@
-import type { App } from 'vue'
-import Rate from './src/rate'
+import type { App } from "vue";
+import Rate from "./src/rate";
 
-Rate.install = function(app: App) {
-  app.component(Rate.name, Rate)
-}
+Rate.install = function (app: App) {
+  app.component(Rate.name, Rate);
+};
 
-export { Rate }
+export { Rate };
 
 export default {
-  title: 'Rate 评分',
-  category: '数据录入',
-  install(app: App): void {    
-		app.use(Rate as any)
-  }
-}
+  title: "Rate 评分",
+  category: "数据录入",
+  install(app: App): void {
+    app.use(Rate as any);
+  },
+};
