07f66301a573f7c34424e9f4cf04529f4a4d30a7
kagol <kagol@sina.com>
2021-08-12 23:44:02 +0800

    refactor: rename tab to tabs

---
+++ devui/tabs/index.ts
@@ -0,0 +1,16 @@
+import type { App } from 'vue'
+import Tabs from './src/tabs'
+
+Tabs.install = function(app: App) {
+  app.component(Tabs.name, Tabs)
+}
+
+export { Tabs }
+
+export default {
+  title: 'Tabs 选项卡',
+  category: '导航',
+  install(app: App): void {    
+		app.use(Tabs as any)
+  }
+}
