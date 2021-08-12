e35b607af98c249d992393883da6ea64b8f1cde0
mrundef <imnull@outlook.com>
2021-08-12 09:32:20 +0800

    feat: 去掉demo组件输出

--- devui/datepicker/index.ts
+++ devui/datepicker/index.ts
@@ -1,13 +1,11 @@
 import { App } from 'vue'
 import DatePicker from './datepicker'
-import DatePickerDemo1 from './doc-demo/demo1'
 
-export { DatePicker, DatePickerDemo1 }
+export { DatePicker }
 
 export default {
   install(app: App) {
     DatePicker.version = '0.0.1'
     app.component(DatePicker.name, DatePicker)
-    app.component(DatePickerDemo1.name, DatePickerDemo1)
   }
 }
