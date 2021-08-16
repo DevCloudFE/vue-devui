3a3f6dbd7c1667590e9c2c24e90e9fbb04856b73
lwl <1181102955@qq.com>
2021-08-16 21:24:48 +0800

    chore: 新增vitepress-theme-demoblock插件注册到vitepress配置中

---
+++ sites/.vitepress/theme/register-components.js
@@ -0,0 +1,6 @@
+import Demo from 'vitepress-theme-demoblock/components/Demo.vue'
+import DemoBlock from 'vitepress-theme-demoblock/components/DemoBlock.vue'
+export function registerComponents(app) {
+  app.component('Demo', Demo)
+  app.component('DemoBlock', DemoBlock)
+}
