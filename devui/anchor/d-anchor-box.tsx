1527fb43a243d6de917ccb5087115a8bb7e5ad9f
tangwei <tangwei@jiangduoduo.com>
2021-08-18 17:43:04 +0800

    feat: 分析情况

--- devui/anchor/d-anchor-box.tsx
+++ devui/anchor/d-anchor-box.tsx
@@ -4,9 +4,71 @@ export default  {
     },
     // 当被绑定的元素挂载到 DOM 中时……
     mounted(el: HTMLElement) {
+      let addEvent = (function(){
+        if(window.addEventListener){
+         return function(elm, type, handle){
+           console.log(elm,"__________")
+          elm.addEventListener(type, handle, false);
+         }
+        }
+        
+       })();
        
-      // 聚焦元素
+       
+      // 添加ng class名
         el.className = 'mycontainer mymain'
+        // 监听windo
+        let windoScrollTop;
+        let toTheBottom: Boolean = false;
+        let div = document.getElementsByClassName("mycontainer")[0];
+        let mysidebar  =  document.getElementsByClassName('mysidebar')[0] as HTMLElement
+
+
+        window.onscroll = function() {
+          //为了保证兼容性，这里取两个值，哪个有值取哪一个
+          //scrollTop就是触发滚轮事件时滚轮的高度
+          windoScrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+      
+          if (windoScrollTop > mysidebar.clientHeight) {
+            console.log("滚动距离" + windoScrollTop);
+              mysidebar.style.position = 'absolute';
+              mysidebar.style.top = div.getBoundingClientRect().top-windoScrollTop +'px';
+              mysidebar.style.left =  '0px';
+          }else if (toTheBottom){
+            // div滚动条到最底部,window滚动条开始滚动
+            console.log(div.getBoundingClientRect().top,windoScrollTop)
+              mysidebar.style.top = div.getBoundingClientRect().top-windoScrollTop +'px';
+              (mysidebar.children[0] as HTMLElement).style.padding = windoScrollTop + "px 0 0 0"
+          }else if (!toTheBottom) {
+
+          }
+        }
+       
+   
+       addEvent(div,'scroll', function(){
+            let scrollHeight = div.scrollHeight;
+            let scrollTop    = div.scrollTop;
+            let height       = div.clientHeight;
+            console.log(scrollTop,"scroll________")
+            
+            console.log()
+            if((scrollTop + height) >= scrollHeight) {
+              console.log(scrollHeight,'到底了');
+              console.log("window滚动距离" + windoScrollTop);
+              // mysidebar.style.position = 'absolute';
+              // mysidebar.style.top =  '0px';
+              // mysidebar.style.left =  '0px';
+              toTheBottom = true;
+         
+              // mysidebar.style.height =  'auto';
+            } else {
+              toTheBottom = false;
+              mysidebar.style.position = 'fixed';
+              mysidebar.style.top = div.getBoundingClientRect().top+'px';
+              mysidebar.style.left = div.getBoundingClientRect().left+'px';
+              mysidebar.style.height = height+'px';
+            }
+       });
         // let AllNodes:HTMLCollection = el.children ;
         // console.log( AllNodes,'DivNodesinserted0');
         // let UlNodes:string, DivNodes:string;
