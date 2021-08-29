6837af7fefeffd713c26c3ee7e39d0b14977b040
1445654576@qq.com <1445654576@qq.com>
2021-08-29 23:15:55 +0800

    feat: 增加子组件

--- devui/form/index.ts
+++ devui/form/index.ts
@@ -1,16 +1,40 @@
 import type { App } from 'vue'
 import Form from './src/form'
+import FormLabel from './src/form-label/form-label';
+import FormItem from './src/form-item/form-item';
+import FormControl from './src/form-control/form-control';
+import FormOperation from './src/form-operation/form-operation';
 
 Form.install = function(app: App) {
   app.component(Form.name, Form)
 }
 
-export { Form }
+FormLabel.install = function(app: App) {
+  app.component(FormLabel.name, FormLabel)
+}
+
+FormItem.install = function(app: App) {
+  app.component(FormItem.name, FormItem)
+}
+
+FormControl.install = function(app: App) {
+  app.component(FormControl.name, FormControl)
+}
+
+FormOperation.install = function(app: App) {
+  app.component(FormOperation.name, FormOperation)
+}
+
+export { Form, FormLabel, FormItem, FormControl, FormOperation }
 
 export default {
   title: 'Form 表单',
   category: '数据录入',
   install(app: App): void {
-    app.use(Form as any)
+    app.use(Form as any);
+    app.use(FormLabel as any);
+    app.use(FormItem as any);
+    app.use(FormControl as any);
+    app.use(FormOperation as any);
   }
 }
