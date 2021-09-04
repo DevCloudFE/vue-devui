a5330144ea28bc2aa966e3f8ca27609e76b3dd75
1445654576@qq.com <1445654576@qq.com>
2021-09-04 20:31:45 +0800

    fix: 增加验证器

--- devui/form/src/form-types.ts
+++ devui/form/src/form-types.ts
@@ -112,3 +112,40 @@ export type FormProps = ExtractPropTypes<typeof formProps>
 export type FormItemProps = ExtractPropTypes<typeof formItemProps>
 export type FormLabelProps = ExtractPropTypes<typeof formLabelProps>
 export type FormControlProps = ExtractPropTypes<typeof formControlProps>
+
+
+export interface IValidators {
+  required: boolean
+  minlength: number
+  maxlength: number
+  min: number
+  max: number
+  requiredTrue: boolean
+  email: boolean
+  pattern: RegExp
+  whiteSpace: boolean
+}
+
+const Validators: IValidators = {
+  required: false,
+  minlength: 0,
+  maxlength: 0,
+  min: 0,
+  max: 0,
+  requiredTrue: false,
+  email: false,
+  pattern: undefined,
+  whiteSpace: false
+}
+
+export const dDefaultValidators = {
+  'required': Validators.required, // 配置不能为空限制，rule中使用：{ required: true }
+  'minlength': Validators.minlength, // 配置最小长度限制，rule中使用：{ minlength: 5 }
+  'maxlength': Validators.maxlength, // 配置最大长度限制，rule中使用：{ maxlength: 128 }
+  'min': Validators.min, // 配置最小值限制，rule中使用：{ min: 0 }
+  'max': Validators.max, // 配置最大值限制，rule中使用：{ max: 100 }
+  'requiredTrue': Validators.requiredTrue, // 配置需要为true，rule中使用：{ requiredTrue: true }
+  'email': Validators.email, // 配置邮箱校验，rule中使用：{ email: true }
+  'pattern': Validators.pattern, // 配置正则校验，rule中使用：{ pattern: RegExp }
+  'whitespace': Validators.whiteSpace, // 配置输入不能全为空格限制，rule中使用：{ whitespace: true }
+};
