10f290b910068da31938f0478da9dde80be2a4fd
CatsAndMice <lihai_js@163.com>
2021-09-11 22:31:23 +0800

    feat: 完成hover展示效果,修改Dome代码

--- devui/popover/src/popover.tsx
+++ devui/popover/src/popover.tsx
@@ -1,14 +1,22 @@
 import './popover.scss'
 import { defineComponent, toRefs, ref, CSSProperties } from 'vue'
+import clickoutsideDirective from '../../shared/devui-directive/clickoutside'
+type positionType = 'top' | 'right' | 'bottom' | 'left'
+type triggerType = 'click' | 'hover'
 export default defineComponent({
   name: 'DPopover',
+
+  directives: {
+    clickoutside: clickoutsideDirective
+  },
+
   props: {
     visible: {
       type: Boolean,
       default: false
     },
     position: {
-      type: [String, Array],
+      type: [String as () => positionType, Array],
       default: () => {
         return ['top', 'right', 'bottom', 'left']
       }
@@ -18,27 +26,33 @@ export default defineComponent({
       default: ''
     },
 
-    // trigger: {
-    //   type: String,
-    //   default: 'click',
-    //   validator: function (value: string) {
-    //     return ['click', 'hover'].includes(value);
-    //   }
-    // },
+    trigger: {
+      type: String as () => triggerType,
+      default: 'click',
+    },
 
     zIndex: {
       type: Number as () => CSSProperties,
       default: 1060
     }
-
-
   },
 
   setup(props, ctx) {
-    const { position, content, zIndex } = toRefs(props);
     const visible = ref(props.visible);
-    const onClick = function () {
-      visible.value = !visible.value;
+    const { position, content, zIndex, trigger } = toRefs(props);
+    const isClick = trigger.value === 'click'
+    const event = function () {
+      if (visible.value) {
+        visible.value = false;
+        return
+      }
+      visible.value = true
+    }
+    const onClick = isClick ? event : null;
+    const onMouseenter = isClick ? null : () => { visible.value = true }
+    const onMouseleave = isClick ? null : () => { visible.value = false }
+    const hiddenContext = function () {
+      visible.value = false
     }
 
     return () => {
@@ -47,12 +61,17 @@ export default defineComponent({
         zIndex: zIndex.value
       }
       return (
-        <div class={['devui-popover', position.value, { 'devui-popover-isVisible': visible.value }]}>
+        <div class={['devui-popover',
+          position.value,
+          {
+            'devui-popover-isVisible': visible.value
+          }
+        ]}>
+          <div class='devui-popover-reference' onMouseenter={onMouseenter} onMouseleave={onMouseleave} onClick={onClick} v-clickoutside={hiddenContext}>
+            {slots.reference?.()}
+          </div>
           <div class='devui-popover-content' style={style}>
             {slots.content?.() || content.value}
-          </div>
-          <div class="devui-popover-reference" onClick={onClick}>
-            {slots.reference?.() || content.value}
           </div>
         </div>
       )
--- sites/components/popover/index.md
+++ sites/components/popover/index.md
@@ -1,25 +1,22 @@
-:::demo
+# Popover 悬浮提示  
 
-```vue
-<template>
-<d-popover content="popover" position="left">
-    <template #content>
-      <span >我是内容</span>
+<d-popover content="popover" position="left-top"  visible>
+<!-- 使用v-slot缩写#报错，可能是与markdown语法冲突了 -->
+   <template v-slot:content>
+      <span >我是内容权scvsvsvs权根本</span>
     </template>
-    <template #reference>
-        <d-button bsStyle="common">contentdev</d-button>
-    <template>
+  <template v-slot:reference>
+    <d-button bsStyle="common">contentdev</d-button>
+  </template>
 </d-popover>
-</template>
 
-<script>
-import { defineComponent } from 'vue'
-export default defineComponent({
-  setup() {
-    return {
-     
-    }
-  },
-})
-</script>
+```html
+<d-popover content="popover" position="left-top"  visible>
+   <template v-slot:content>
+      <span >我是内容权scvsvsvs权根本</span>
+    </template>
+  <template v-slot:reference>
+    <d-button bsStyle="common">contentdev</d-button>
+  </template>
+</d-popover>
 ```
