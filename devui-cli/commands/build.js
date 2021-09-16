c777f9f50a19d1583fce978b14fb363bfb805d5d
kagol <kagol@sina.com>
2021-09-16 23:11:32 +0800

    feat(theme): 增加生成组件package.json文件的逻辑

--- devui-cli/commands/build.js
+++ devui-cli/commands/build.js
@@ -1,11 +1,12 @@
 const path = require('path')
 const fs = require('fs')
+const fsExtra = require('fs-extra')
 const { defineConfig, build } = require('vite')
 const vue = require('@vitejs/plugin-vue')
 const vueJsx = require('@vitejs/plugin-vue-jsx')
 
-const entryDir = path.resolve(__dirname, '../devui')
-const outputDir = path.resolve(__dirname, '../build')
+const entryDir = path.resolve(__dirname, '../../devui')
+const outputDir = path.resolve(__dirname, '../../build')
 
 const baseConfig = defineConfig({
   configFile: false,
@@ -54,6 +55,22 @@ const buildAll = async () => {
   }))
 }
 
+const createPackageJson = (name) => {
+  const fileStr = `{
+  "name": "${name}",
+  "version": "0.0.0",
+  "main": "index.umd.js",
+  "module": "index.es.js",
+  "style": "style.css"
+}`
+
+  fsExtra.outputFile(
+    path.resolve(outputDir, `${name}/package.json`),
+    fileStr,
+    'utf-8'
+  )
+}
+
 exports.build = async () => {
   await buildAll()
 
@@ -65,5 +82,6 @@ exports.build = async () => {
 
   for(const name of components) {
     await buildSingle(name)
+    createPackageJson(name)
   }
 }
