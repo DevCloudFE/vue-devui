c309e65aea3a664da3e9a117ea3645102c27241a
populus <mazhaowe7222@qq.com>
2021-10-03 16:29:01 +0800

    feat: 增加基础版steps-guide组件及demo

--- devui/steps-guide/src/steps-guide-types.ts
+++ devui/steps-guide/src/steps-guide-types.ts
@@ -1,8 +1,16 @@
 import type { ExtractPropTypes, PropType } from 'vue'
-type Step = {
+type positionType = 'top-left' | 'top' | 'top-right' | 'left' | 'right' | 'bottom-left' | 'bottom' | 'bottom-right'
+export type positionConf = {
+  left: number
+  top: number
+  type: string
+} 
+export type Step = {
   title: string
   content: string
-  selector: string
+  trigger: string
+  target?: string
+  position?: positionType | positionConf
 }
 export const stepsGuideProps = {
   steps: Array as PropType<Step[]>
--- devui/steps-guide/src/steps-guide.tsx
+++ devui/steps-guide/src/steps-guide.tsx
@@ -1,33 +1,78 @@
 import './steps-guide.scss'
 
 import { defineComponent, Teleport, ref, onMounted, reactive, computed } from 'vue'
-import { stepsGuideProps, StepsGuideProps } from './steps-guide-types'
+import { stepsGuideProps, StepsGuideProps, Step, positionConf } from './steps-guide-types'
 
-// 
 export default defineComponent({
   name: 'DStepsGuide',
   props: stepsGuideProps,
   emits: [],
   setup(props: StepsGuideProps, ctx) {
     const teleport = Teleport
+    const stepsRef = ref(null)
     const stepsCount = computed<number>(() => props.steps.length - 1)
     const stepIndex = ref<number>(0), showSteps = ref<boolean>(true)
-    let stepsNodes
+    const currentStep = computed<Step>(() => {
+      const _step = props.steps[stepIndex.value]
+      _step.position = _step.position || 'top'
+      return _step
+    })
+    const guideClassName = ['d-steps-guide']
     const position = reactive({
       left: '',
       top: '',
       zIndex: 1100
     })
-
     onMounted(() => {
-      stepsNodes = props.steps.map(step => document.querySelector(step.selector))
       updateGuidePosition()
     })
     function updateGuidePosition(){
-      const triggerElement = stepsNodes[stepIndex.value]
-      const targetRect = triggerElement.getBoundingClientRect();
-      position.left = targetRect.left + triggerElement.clientWidth / 2 + 'px'
-      position.top = targetRect.top + triggerElement.clientHeight / 2 + 'px'
+      const baseTop = window.pageYOffset - document.documentElement.clientTop
+      const baseLeft = window.pageXOffset - document.documentElement.clientLeft
+      const currentStepPosition = currentStep.value.position
+      let left, top
+      // 自定义 position位置
+      if(typeof currentStepPosition !== 'string'){
+        guideClassName[1] = currentStepPosition.type
+
+        left = currentStepPosition.left
+        top = currentStepPosition.top
+      } else {
+        guideClassName[1] = currentStepPosition
+        const stepGuideElement = stepsRef.value
+        const triggerSelector = currentStep.value.target || currentStep.value.trigger
+        const triggerElement = document.querySelector(triggerSelector)
+        const targetRect = triggerElement.getBoundingClientRect()
+        left = targetRect.left + triggerElement.clientWidth / 2 - stepGuideElement.clientWidth / 2 + baseLeft
+        top = targetRect.top + triggerElement.clientHeight / 2 - stepGuideElement.clientHeight / 2 + baseTop
+        const positionTypes = currentStepPosition.split('-')
+        switch(positionTypes[0]){
+          case 'top':
+            top += (-stepGuideElement.clientHeight / 2 - triggerElement.clientHeight)
+          break
+          case 'bottom':
+            top += (stepGuideElement.clientHeight / 2 + triggerElement.clientHeight)
+          break
+          case 'left':
+            top += (stepGuideElement.clientHeight / 2 - triggerElement.clientHeight)
+            left += (-stepGuideElement.clientWidth / 2 - triggerElement.clientWidth / 2)
+            break
+          case 'right':
+            top += (stepGuideElement.clientHeight / 2 - triggerElement.clientHeight)
+            left += (stepGuideElement.clientWidth / 2 + triggerElement.clientWidth / 2)
+            break
+        }
+        switch(positionTypes[1]){
+          case 'left':
+            left += (stepGuideElement.clientWidth / 2 - triggerElement.clientWidth / 2)
+            break
+          case 'right':
+            left += (-stepGuideElement.clientWidth / 2 + triggerElement.clientWidth / 2)
+            break
+        }
+      }
+      position.left = left + 'px'
+      position.top = top + 'px'
     }
     function renderPrev(){
       return stepIndex.value > 0 ? <div class="devui-prev-step" onClick={ prevStep }>{ '上一步' }</div> : null
@@ -50,13 +95,13 @@ export default defineComponent({
     }
     return () => showSteps.value ? 
       (<teleport to="body">
-        <div class="d-steps-guide" style={ position }>
+        <div style={ position } class={ guideClassName } ref={ stepsRef }>
           <div class="devui-shining-dot"></div>
           <div class="devui-shining-plus"></div>
           <div class="devui-arrow"></div>
           <div class="devui-guide-container">
-            <p class="devui-title">{ props.steps[stepIndex.value].title }</p>
-            <div class="devui-content">{ props.steps[stepIndex.value].content }</div>
+            <p class="devui-title">{ currentStep.value.title }</p>
+            <div class="devui-content">{ currentStep.value.content }</div>
             <div class="devui-ctrl">
               <div class="devui-dots">
                 <em class="icon icon-dot-status devui-active"></em>
