8f94c0e431a1732bbaa9d77cd5d35276a3aecbb4
chenshiming <728988492@qq.com>
2021-10-06 16:48:21 +0800

    feat(fullscreen): 新增测试用例

---
+++ devui/fullscreen/__tests__/fullscreen.spec.ts
@@ -0,0 +1,53 @@
+import { mount, VueWrapper } from '@vue/test-utils'
+import { ref, nextTick } from 'vue'
+
+describe('fullscreen', () => {
+  let wrapper: VueWrapper<any>
+
+  beforeEach(() => {
+    wrapper = mount({
+      template: `
+        <d-fullscreen :mode='"normal"' @fullscreenLaunch='fullscreenLaunch'>
+          <div fullscreen-target>
+            <button test fullscreen-launch>{{btnContent}}</button>
+          </div>
+        </d-fullscreen>
+      `,
+      setup() {
+        const btnContent = ref('FullScreen')
+        const fullscreenLaunch = (val) => {
+          if (val) {
+            btnContent.value = 'Exit'
+          } else {
+            btnContent.value = 'FullScreen'
+          }
+        }
+        return {
+          btnContent,
+          fullscreenLaunch
+        }
+      }
+    })
+  })
+
+  // 样式判断
+  it('judge html class correctly', async () => {
+
+    // 初始样式
+    expect(document.getElementsByTagName('html')[0].classList).not.toContain('devui-fullscreen')
+    // 点击之后，增加class
+    await wrapper.find('[test]').trigger('click')
+    await nextTick()
+    expect(document.getElementsByTagName('html')[0].classList).not.toContain('devui-fullscreen')
+    
+    // 再次点击，删除class
+    await wrapper.find('[test]').trigger('click')
+    await nextTick()
+    expect(document.getElementsByTagName('html')[0].classList).not.toContain('devui-fullscreen')
+  })
+
+  // 判断属性
+  it('attr', () => {
+    expect(wrapper.attributes('mode')).toBe('normal')
+  })
+})
