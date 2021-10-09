18e84e172bd8ce5ac4ed605c4b2af56ad6755bde
pandeng <657524896@qq.com>
2021-10-09 15:10:47 +0800

    feat: 增加tree-select基本功能

--- devui/tree-select/src/tree-select-types.ts
+++ devui/tree-select/src/tree-select-types.ts
@@ -1,9 +1,69 @@
 import type { PropType, ExtractPropTypes } from 'vue'
 
+export interface TreeItem {
+  id?: number | string
+  label?: string
+  data?: any
+  parent?: TreeItem | null
+  children?: Array<TreeItem> | null
+  level?: number
+  loading?: boolean
+  isOpen?: boolean
+  isChecked?: boolean
+  disabled?: boolean
+  
+  [prop: string]: any
+}
+
+export type TreeData = Array<TreeItem>
+
+export type ModelValue = number | string | Array<number | string>;
+
 export const treeSelectProps = {
-  /* test: {
-    type: Object as PropType<{ xxx: xxx }>
-  } */
+  modelValue: {
+    type: [String, Number, Array] as PropType<ModelValue>,
+    default: '',
+  },
+  treeData: {
+    type: Array as PropType<TreeData>,
+    default: () => [],
+  },
+  placeholder: {
+    type: String,
+    default: '请选择',
+  },
+  disabled: {
+    type: Boolean,
+    default: false
+  },
+  expandTree: {
+    type: Boolean,
+    default: false
+  },
+  multiple: {
+    type: Boolean,
+    default: false,
+  },
+  leafOnly: {
+    type: Boolean,
+    default: false,
+  },
+  searchable: {
+    type: Boolean,
+    default: false,
+  },
+  allowClear: {
+    type: Boolean,
+    default: false
+  },
+  onToggleChange: {
+    type: Function as PropType<(bool: boolean) => void>,
+    default: undefined,
+  },
+  onValueChange: {
+    type: Function as PropType<(item: TreeItem, index: number) => void>,
+    default: undefined,
+  },
 } as const
 
 export type TreeSelectProps = ExtractPropTypes<typeof treeSelectProps>
--- devui/tree-select/src/tree-select.tsx
+++ devui/tree-select/src/tree-select.tsx
@@ -1,18 +1,119 @@
 import './tree-select.scss'
 
-import { defineComponent } from 'vue'
+import { defineComponent, ref, reactive, toRefs, computed } from 'vue'
 import { treeSelectProps, TreeSelectProps } from './tree-select-types'
+import { className } from './utils'
 
 export default defineComponent({
   name: 'DTreeSelect',
   props: treeSelectProps,
-  emits: [],
+  emits: ['toggleChange', 'update:modelValue'],
   setup(props: TreeSelectProps, ctx) {
-    return {}
+    
+    const visible = ref<boolean>(false)
+    const origin = ref()
+    const position = reactive({
+      originX: 'left', 
+      originY: 'bottom', 
+      overlayX: 'left', 
+      overlayY: 'top'
+    })
+
+    const { treeData } = toRefs(props)
+
+    const inputValue = computed(() => {
+      return ''
+    })
+
+    function toggleChange() {
+      if(props.disabled) return
+      visible.value = !visible.value
+      ctx.emit('toggleChange', visible.value)
+    }
+
+    function handleClear(e: MouseEvent) {
+      e.preventDefault()
+      e.stopPropagation()
+      if (props.multiple) {
+        ctx.emit('update:modelValue', [])
+      } else {
+        ctx.emit('update:modelValue', '')
+      }
+    }
+
+    return {
+      visible,
+      origin,
+      position,
+      treeData,
+      inputValue,
+      handleClear,
+      toggleChange,
+    }
   },
   render() {
-    const {} = this
+    const {
+      origin,
+      position,
+      treeData,
+      inputValue,
+      placeholder,
+      disabled,
+      handleClear,
+      toggleChange,
+    } = this
 
-    return <div class="d-tree-select"></div>
+    const renderNode = (item) => (
+      <div style={{ paddingLeft: `${20 * (item.level - 1)}px` }}>
+        { item.children ? <d-icon name="select-arrow" /> : <span>{'\u00A0\u00A0\u00A0'}</span>}
+        {item.label}
+      </div>
+    )
+
+    const renderTree = (treeData) => {
+      return treeData.map(item => {
+        if (item.children) {
+          return (
+            <>
+              { renderNode(item) }
+              { renderTree(item.children) }
+            </>
+          )
+        }
+        return renderNode(item)
+      })
+    }
+
+    const treeSelectCls = className('devui-tree-select', {
+      'devui-tree-select-open': this.visible,
+      'devui-tree-select-disabled': disabled,
+    })
+
+    return (
+      <div class={treeSelectCls}>
+        <div ref="origin" onClick={toggleChange}>
+          <input
+            value={inputValue}
+            type="text"
+            class="devui-tree-select-input"
+            placeholder={placeholder}
+            readonly
+            disabled={disabled}
+          />
+          <span onClick={handleClear} class="devui-tree-select-clear">
+            <d-icon name="close" />
+          </span>
+          <span class="devui-tree-select-arrow">
+            <d-icon name="select-arrow" />
+          </span>
+        </div>
+        <d-flexible-overlay origin={origin} v-model={[this.visible, 'visible']} position={position}>
+          <div class="devui-tree-select-dropdown">
+            <ul class="devui-tree-select-dropdown-list devui-scrollbar">{renderTree(treeData)}</ul>
+          </div>
+          {/* {renderTree(treeData)} */}
+        </d-flexible-overlay>
+      </div>
+    )
   }
 })
