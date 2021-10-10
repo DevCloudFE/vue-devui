61d1a05100e6e47d5b9b5e09434d78b24705a658
zhongfabin <zhongfb@yealink.com>
2021-10-10 16:06:12 +0800

    fix: 修改错误的单词

--- devui/button/src/button.tsx
+++ devui/button/src/button.tsx
@@ -22,10 +22,10 @@ export default defineComponent({
     const btnClass = computed(() => {
       const { btnStyle, size, position, bordered, icon } = props;
       const origin = `devui-btn devui-btn-${btnStyle} devui-btn-${size} devui-btn-${position}`;
-      const broderedClazz = bordered ? 'bordered' : '';
+      const borderedClazz = bordered ? 'bordered' : '';
       const btnIcon = !!icon && !hasContent.value && btnStyle !== 'primary' ? 'd-btn-icon' : '';
       const btnIconWrap = !!icon ? 'd-btn-icon-wrap' : '';
-      return `${origin} ${broderedClazz} ${btnIcon} ${btnIconWrap}`;
+      return `${origin} ${borderedClazz} ${btnIcon} ${btnIconWrap}`;
     });
 
     const iconClass = computed(() => {
