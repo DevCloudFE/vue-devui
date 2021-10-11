aafd163a5bde46756da8b61a7ece316fc0fa234c
kagol <kagol@sina.com>
2021-10-11 22:52:55 +0800

    fix: 修复几个组件的分类和完成状态

--- devui/input-number/index.ts
+++ devui/input-number/index.ts
@@ -9,7 +9,7 @@ export { InputNumber }
 
 export default {
   title: 'InputNumber 数字输入框',
-  category: '导航',
+  category: '输入录入',
   status: '50%',
   install(app: App):void {    
     app.use(InputNumber as any)
