97d36012ff5a9ad4fcc4fc170cc5db5ca7e2fcaf
ElsaOOo <yjjjng0901@126.com>
2021-10-11 10:07:29 +0800

    feat(backToTop): 给站点添加一个 返回顶部 按钮

--- docs/.vitepress/devui-theme/components/Page.vue
+++ docs/.vitepress/devui-theme/components/Page.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import PageFooter from './PageFooter.vue'
 import NextAndPrevLinks from './NextAndPrevLinks.vue'
+import BackToTop from './BackToTop.vue'
 </script>
 
 <template>
@@ -13,6 +14,7 @@ import NextAndPrevLinks from './NextAndPrevLinks.vue'
       <NextAndPrevLinks />
 
       <slot name="bottom" />
+      <BackToTop />
     </div>
   </main>
 </template>
