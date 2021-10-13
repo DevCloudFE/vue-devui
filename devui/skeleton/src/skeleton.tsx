af83c8fc802cb6a986ce9a7bd56cf1aea88e96f5
无声 <ivestszheng@qq.com>
2021-10-13 17:39:35 +0800

    refactor: 柯里化

--- devui/skeleton/src/skeleton.tsx
+++ devui/skeleton/src/skeleton.tsx
@@ -8,22 +8,33 @@ export default defineComponent({
   props: skeletonProps,
   setup(props: SkeletonProps, ctx) {
     const { slots } = ctx;
-    const arr = []
-    for (let index = 0; index < props.row; index++) {
-      arr.push(1)
+
+    function renderAnimate(bool) {
+      return bool ? 'devui-skeleton-animated' : ''
     }
+    function renderAvatar(bool) {
+      return bool ? <div class="devui-skeleton__avatar"><div class="avatar" /></div> : ''
+    }
+    function renderParagraph(num) {
+      const arr = []
+      for (let index = 0; index < num; index++) {
+        arr.push(1)
+      }
+      return <div class="devui-skeleton__item__group">{
+        arr.map(() => {
+          return <div class="devui-skeleton__item" />
+        })
+      }</div>
+    }
+
     return () => {
       if (props.loading) {
-        return <div class={`devui-skeleton ${props.animate ? 'devui-skeleton-animated' : 'devui-skeleton-no-animated'}`}>
-          {props.avatar ? <div class="devui-skeleton__avatar"><div class="avatar" /></div> : null}
-          <div class="devui-skeleton__item__group">{
-            arr.map(() => {
-              return <div class="devui-skeleton__item" />
-            })
-          }</div>
+        return <div class={`devui-skeleton ${renderAnimate(props.animate)}`}>
+          {renderAvatar(props.avatar)}
+          {renderParagraph(props.row)}
         </div>
       }
-      return <div class={`devui-skeleton ${props.animate ? 'devui-skeleton-animated' : ''}`}>{slots.default?.()}
+      return <div class={`devui-skeleton ${renderAnimate(props.animate)}`}>{slots.default?.()}
       </div>
     }
   }
