c338bbc09cc86ba86156d32c8feb9992e15c1dd5
jenson <1763417179@qq.com>
2021-10-14 11:33:36 +0800

    feat(time-axis): 完成时间轴的基本用法

---
+++ devui/time-axis/src/components/time-axis-item/index.tsx
@@ -0,0 +1,33 @@
+import {defineComponent} from 'vue'
+
+import {timeAxisItemProps, TimeAxisItemProps} from './types'
+import './index.scss'
+
+export default defineComponent({
+    name: 'DTimeAxisItem',
+    props: timeAxisItemProps,
+    emits: [],
+    setup(props: TimeAxisItemProps, ctx) {
+        const itemClass = 'devui-time-axis-item'
+        return () => {
+            return (
+                <div class={itemClass}>
+                    <div class={`${itemClass}-data-left ${itemClass}-data-top`}>
+                        <span>
+                        {props.time}
+
+                        </span>
+                    </div>
+                    <div class={`${itemClass}-axis`}>
+                        <div class={`${itemClass}-line ${itemClass}-line-style-solid`}></div>
+                        <div class={`${itemClass}-dot ${itemClass}-type-primary`} style={{borderColor: props.dotColor}}></div>
+                        <div class={`${itemClass}-line ${itemClass}-line-style-solid`}></div>
+                    </div>
+                    <div class={`${itemClass}-data-right ${itemClass}-data-bottom`}>
+                        {props.text}
+                    </div>
+                </div>
+            )
+        }
+    }
+})
