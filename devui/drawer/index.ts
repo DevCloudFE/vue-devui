281f5be21bb568b3e5bb734f09e2eafbabc301a5
nowisfuture <lnzhangsong@live.com>
2021-10-14 10:47:52 +0800

    feat(drawer): 修改组件完成度,增加组件属性文档

--- devui/drawer/index.ts
+++ devui/drawer/index.ts
@@ -10,7 +10,7 @@ export { Drawer }
 export default {
   title: 'Drawer 抽屉板',
   category: '反馈',
-  status: undefined, // TODO: 组件若开发完成则填入"已完成"，并删除该注释
+  status: '10%',
   install(app: App): void {
     
     app.use(Drawer as any)
