8b8f02418e3d6f23efaca7fb514f09577bfaffca
gxuud <igaoxian@foxmail.com>
2021-10-18 13:07:55 +0800

    fix: check操作封装为use-check.ts

--- devui/tree/src/tree.tsx
+++ devui/tree/src/tree.tsx
@@ -1,12 +1,14 @@
-import { defineComponent, toRefs, provide, ref,  unref } from 'vue'
-import { treeProps, TreeProps, TreeItem, TreeRootType, SelectType, ReverseTree } from './tree-types'
+import { defineComponent, toRefs, provide } from 'vue'
+import type { SetupContext } from 'vue'
+import { treeProps, TreeProps, TreeItem, TreeRootType } from './tree-types'
 import { CHECK_CONFIG } from  './config';
-import { flatten, precheckTree } from './util'
+import { precheckTree } from './util'
 import Loading from '../../loading/src/service'
 import Checkbox from '../../checkbox/src/checkbox'
 import useToggle from './composables/use-toggle'
 import useMergeNode from './composables/use-merge-node'
 import useHighlightNode from './composables/use-highlight'
+import useChecked from './composables/use-checked'
 import useLazy from './composables/use-lazy'
 import IconOpen from './assets/open.svg'
 import IconClose from './assets/close.svg'
@@ -17,141 +19,19 @@ export default defineComponent({
   name: 'DTree',
   props: treeProps,
   emits: ['nodeSelected'],
-  setup(props: TreeProps, ctx) {
+  setup(props: TreeProps, ctx: SetupContext) {
     const { data, checkable, checkableRelation: cbr } = toRefs({ ...props, data: precheckTree(props.data) })
-    const flatData = flatten(data.value)
-    const selected = ref<SelectType>({})
-
     const { mergeData } = useMergeNode(data.value)
     const { openedData, toggle } = useToggle(mergeData.value)
     const { nodeClassNameReflect, handleInitNodeClassNameReflect, handleClickOnNode } = useHighlightNode()
     const { lazyNodesReflect, handleInitLazyNodeReflect, getLazyData } = useLazy()
+    const { selected, onNodeClick } = useChecked(cbr, ctx, data.value)
 
     provide<TreeRootType>('treeRoot', { ctx, props });
     const Indent = () => {
       return <span style="display: inline-block; width: 16px; height: 16px; margin-left: 8px"></span>
     }
-
-    const findPedigreeById = (id: string) => {
-      const treeData = unref(data)
-      let parentLevel: ReverseTree = {}
-      let childLevel: string[] = []
-      let target = undefined
-      const ergodic = (curr: any[], parentNode: ReverseTree) => {
-        curr.every(({ children, id: itemId }) => {
-          if (target) {
-            return false
-          }
-          if (itemId === id) {
-            parentLevel = parentNode
-            childLevel = Array.isArray(children)
-              ? flatten(children).map(({ id: key }) => key)
-              : [];
-            target = itemId
-            return false
-          }
-          if (Array.isArray(children)) {
-            ergodic(children, {
-              id: itemId,
-              children: children.map(({ id: key }) => key),
-              parent: parentNode,
-            });
-          }
-          return true
-        })
-      }
-      ergodic(treeData, {})
-      return {
-        parentLevel,
-        childLevel,
-      }
-    }
-
-    const generateParentObject = (parentLevel: ReverseTree, isSelected: boolean, checkId: string): SelectType => {
-      const state: SelectType = {}
-      const currentSelected = unref(selected)
-      const ergodic = (currObj: ReverseTree, isOverflow = false) => {
-        const { id, children, parent } = currObj;
-        if (!parent) {
-          return
-        }
-        if (isSelected) {
-          const optional = children.filter(
-            (item) => !currentSelected[item] || currentSelected[item] === 'none'
-          );
-          if (optional.length <= 1) {
-            if (optional[0] === checkId) {
-              state[id] = 'select'
-            } else if (isOverflow) {
-              state[id] = 'half'
-            }
-          } else {
-            state[id] = 'half'
-          }
-          ergodic(parent, state[id] === 'select');
-        } else {
-          const optional = children.filter(
-            (item) => currentSelected[item] && currentSelected[item] !== 'none'
-          );
-          if (optional.length <= 1) {
-            if (optional[0] === checkId || isOverflow) {
-              state[id] = 'none'
-            }
-          } else {
-            state[id] = 'half'
-          }
-          ergodic(parent, state[id] === 'none')
-        }
-      }
-      ergodic(parentLevel)
-      return state
-    }
-
-    const onNodeClick = (item: TreeItem) => {
-      const { id } = item
-      let currentSelected = Object.assign({}, unref(selected))
-      const isSelected = currentSelected[id] === 'none' || !currentSelected[id];
-      if (cbr.value === 'none') {
-        currentSelected = Object.assign(currentSelected, { [id]: isSelected ? 'select' : 'none' })
-      } else if (cbr.value === 'both') {
-        const { parentLevel, childLevel } = findPedigreeById(id);
-        currentSelected = Object.assign(
-          currentSelected,
-          Object.fromEntries(
-            childLevel.map((key) => [
-              key,
-              isSelected ? 'select' : 'none',
-            ])
-          ),
-          generateParentObject(parentLevel, isSelected, id),
-          { [id]: isSelected ? 'select' : 'none' }
-        )
-      } else if (cbr.value === 'upward') {
-        const { parentLevel } = findPedigreeById(id);
-        currentSelected = Object.assign(
-          currentSelected,
-          generateParentObject(parentLevel, isSelected, id),
-          { [id]: isSelected ? 'select' : 'none' }
-        )
-      } else if (cbr.value === 'downward') {
-        const { childLevel } = findPedigreeById(id);
-        currentSelected = Object.assign(
-          currentSelected,
-          Object.fromEntries(
-            childLevel.map((key) => [
-              key,
-              isSelected ? 'select' : 'none',
-            ])
-          ),
-          { [id]: isSelected ? 'select' : 'none' }
-        )
-      }
-      selected.value = currentSelected
-      const currentSelectedItem = flatData.filter(
-        ({ id }) => currentSelected[id] && currentSelected[id] !== 'none'
-      )
-      ctx.emit('nodeSelected', currentSelectedItem)
-    }
+    
     const renderNode = (item: TreeItem) => {
       const { id = '', label, disabled, open, isParent, level, children } = item
       handleInitNodeClassNameReflect(disabled, id)
