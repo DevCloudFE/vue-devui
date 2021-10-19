d8dca2cdcc0d45ba5efeb6ba797ae4bbb6301cb8
无声 <ivestszheng@qq.com>
2021-10-19 11:28:52 +0800

    feat: 修改单测

--- devui/skeleton/__tests__/skeleton.spec.ts
+++ devui/skeleton/__tests__/skeleton.spec.ts
@@ -3,59 +3,59 @@ import { ref } from 'vue';
 import DSkeleton from '../src/skeleton';
 
 describe('skeleton 组件', () => {
-  it('render basic skeleton successfully', () => {
-    const row = ref(4);
+  it('render basic skeleton', () => {
+    const row = ref(4)
     const wrapper = mount({
       components: { DSkeleton },
       template: `<d-skeleton :row="row" />`,
       setup() {
         return {
           row
-        };
+        }
       },
-    });
+    })
 
     expect(wrapper.classes()).toContain('devui-skeleton')
     expect(wrapper.classes()).toContain('devui-skeleton-animated')
-    expect(wrapper.element.childElementCount).toBe(1)
-    // 渲染个数应当与传入的 row 的数量相同
-    expect(wrapper.element.children[0].childElementCount).toBe(4)
+    expect(wrapper.element.childElementCount).toBe(2)
+    expect(wrapper.element.children[1].children[1].childElementCount).toBe(4)
   })
 
   it('render skeleton without animate', () => {
-    const animate = ref(false);
+    const animate = ref(false)
     const wrapper = mount({
       components: { DSkeleton },
       template: `<d-skeleton :animate="animate" />`,
       setup() {
         return {
           animate
-        };
+        }
       },
-    });
+    })
 
-    expect(wrapper.classes()).toContain('devui-skeleton-no-animated')
+    expect(wrapper.classes()).toEqual(['devui-skeleton'])
   })
 
-  it('render skeleton with avatar', () => {
-    const avatar = ref(true);
+  it('render skeleton without title and paragraph', () => {
+    const title = ref(false)
+    const paragraph = ref(false)
     const wrapper = mount({
       components: { DSkeleton },
-      template: `<d-skeleton :avatar="avatar" />`,
+      template: `<d-skeleton :title="title" :paragraph="paragraph" />`,
       setup() {
         return {
-          avatar
-        };
+          title,
+          paragraph
+        }
       },
-    });
-
-    expect(wrapper.element.childElementCount).toBe(2)
-    expect(wrapper.element.children[0].innerHTML).toBe('<div class="avatar"></div>')
+    })
+    
+    expect(wrapper.element.children[1].childElementCount).toBe(2)
   })
 
   it('hide skeleton and show real content', () => {
-    const row = ref(4);
-    const loading = ref(false);
+    const row = ref(4)
+    const loading = ref(false)
     const wrapper = mount({
       components: { DSkeleton },
       template: `
@@ -71,9 +71,9 @@ describe('skeleton 组件', () => {
         return {
           row,
           loading
-        };
+        }
       },
-    });
+    })
 
     expect(wrapper.classes()).toContain('devui-skeleton')
     expect(wrapper.element.children[0].innerHTML).toBe('<div>content1</div><div>content2</div><div>content3</div><div>content4</div>')
