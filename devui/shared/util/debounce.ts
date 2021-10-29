eb2bb616e287a713ab3888037ff3a67535537234
chenxi_24 <chengxi_24@163.com>
2021-10-29 19:23:16 +0800

    refactor: 重构组件结构

--- devui/shared/util/debounce.ts
+++ devui/shared/util/debounce.ts
@@ -3,7 +3,7 @@
  * @param func The function to debounce.
  * @param wait The number of milliseconds to delay.
  * @param immediate Whether to execute immediately
- * @returns  Returns the new debounced function.
+ * @returns Returns the new debounced function.
  */
 export function debounce<A extends Array<any>, R = void>(func: (...args: A) => R, wait: number, immediate: boolean): (...args: A) => R {
     let timer: number, result: R;
