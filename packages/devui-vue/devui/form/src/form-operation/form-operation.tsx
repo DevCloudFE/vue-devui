43f31dc0e8f31c0c39bc0bb9403d2a2a94676e54
1445654576@qq.com <1445654576@qq.com>
2021-10-30 12:40:25 +0800

    fix: 将tab缩进改为空格

--- packages/devui-vue/devui/form/src/form-operation/form-operation.tsx
+++ packages/devui-vue/devui/form/src/form-operation/form-operation.tsx
@@ -2,15 +2,15 @@ import { defineComponent } from 'vue';
 import './form-operation.scss';
 
 export default defineComponent({
-	name: 'DFormOperation',
-	props: {
-		
-	},
-	setup(props, ctx) {
-		return () => {
-			return <div class="form-operation">
-				{ctx.slots.default?.()}
-			</div>
-		}
-	}
+  name: 'DFormOperation',
+  props: {
+    
+  },
+  setup(props, ctx) {
+    return () => {
+      return <div class="form-operation">
+        {ctx.slots.default?.()}
+      </div>
+    }
+  }
 })
