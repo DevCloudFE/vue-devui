7eb489c7904e0224b45749b65707dbba4edb27e7
1445654576@qq.com <1445654576@qq.com>
2021-10-30 12:20:39 +0800

    fix: 注释log语句

--- packages/devui-vue/devui/form/src/form-item/form-item.tsx
+++ packages/devui-vue/devui/form/src/form-item/form-item.tsx
@@ -37,7 +37,7 @@ export default defineComponent({
 		const tipMessage = ref('');
 
 		const validate = (trigger: string) => {
-			console.log('trigger', trigger);
+			// console.log('trigger', trigger);
 			
 			const ruleKey = props.prop;
 			const ruleItem = rules[ruleKey];
@@ -50,7 +50,7 @@ export default defineComponent({
 				showMessage.value = false;
 				tipMessage.value = '';
 			}).catch(({ errors }) => {
-				console.log('validator errors', errors);
+				// console.log('validator errors', errors);
 				showMessage.value = true;
 				tipMessage.value = errors[0].message;
 			});
