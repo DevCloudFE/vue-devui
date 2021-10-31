55d002ef6783d92ec3e92142b66c532ece9c1b27
kagol <kagol@sina.com>
2021-10-31 12:42:24 +0800

    fix: update status

--- packages/devui-vue/devui/dragdrop/index.ts
+++ packages/devui-vue/devui/dragdrop/index.ts
@@ -7,7 +7,7 @@ export { DraggableDirective, DroppableDirective }
 export default {
   title: 'Dragdrop 拖拽',
   category: '通用',
-  status: undefined, // TODO: 组件若开发完成则填入"已完成"，并删除该注释
+  status: '10%',
   install(app: App): void {
     app.directive('DDraggable', DraggableDirective)
     app.directive('DDroppable', DroppableDirective)
