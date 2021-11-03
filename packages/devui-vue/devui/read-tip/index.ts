13b406e2769c371db750541a37226798aae18412
panyongxu <panyx_1002@163.com>
2021-11-03 17:44:57 +0800

    feat(readTip): 更新组件开发状态

--- packages/devui-vue/devui/read-tip/index.ts
+++ packages/devui-vue/devui/read-tip/index.ts
@@ -11,7 +11,7 @@ export { ReadTip, ReadTipDirective }
 export default {
   title: 'ReadTip 阅读提示',
   category: '反馈',
-  status: undefined, // TODO: 组件若开发完成则填入"已完成"，并删除该注释
+  status: '5%', // TODO: 组件若开发完成则填入"已完成"，并删除该注释
   install(app: App): void {
         app.use(ReadTip as any)
     app.directive('ReadTip', ReadTipDirective)
