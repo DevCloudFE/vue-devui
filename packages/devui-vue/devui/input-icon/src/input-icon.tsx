5e3b4e98f34a7fb34bf9aee7d190227e85f75e1b
mrundef <imnull@outlook.com>
2021-11-16 22:11:08 +0800

    feat: 增加组件InputIcon

---
+++ packages/devui-vue/devui/input-icon/src/input-icon.tsx
@@ -0,0 +1,50 @@
+import { defineComponent, reactive, PropType } from 'vue'
+import Input from '../../input/src/input'
+import { inputProps } from '../../input/src/use-input'
+import Icon from '../../icon/src/icon'
+// import { inputProps, InputType } from './use-input'
+// import './input.scss'
+// import { dFormItemEvents, IFormItem, formItemInjectionKey } from '../../form/src/form-types'
+
+import './input-icon.scss'
+
+const inputIconProps = {
+    ...inputProps,
+    name: {
+        type: String,
+        value: 'calendar',
+        required: false,
+    },
+    onIconclick: {
+        type: Function as PropType<(e: MouseEvent) => void>,
+        required: false,
+    }
+}
+
+export default defineComponent({
+    name: 'DInputIcon',
+    props: inputIconProps,
+    setup(props, ctx) {
+        const { name, onIconclick, onChange, ...inputProps } = props
+        const state = reactive({ value: '' })
+        const onInputChange = (v: string) => {
+            state.value = v
+            typeof onChange === 'function' && onChange(state.value)
+        }
+        const onIconClick = (e: MouseEvent) => {
+            typeof onIconclick === 'function' && onIconclick(state.value, e)
+        }
+        return () => {
+            return (
+                <div class="d-input-icon-container">
+                    <label>
+                        <Input { ...inputProps } onChange={onInputChange} />
+                    </label>
+                    <span onClick={onIconClick}>
+                        <Icon size="small" name={name} />
+                    </span>
+                </div>
+            )
+        }
+    },
+})
