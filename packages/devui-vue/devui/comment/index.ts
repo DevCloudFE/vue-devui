6b95d756e490bc1356bdb6b126c92d6ff0a4a9d9
lishiyao <lishiyao@dgg.net>
2021-11-25 14:30:08 +0800

    feat:只提交comment组件代码

---
+++ packages/devui-vue/devui/comment/index.ts
@@ -0,0 +1,17 @@
+import type { App } from 'vue'
+import Comment from './src/comment'
+
+Comment.install = function(app: App): void {
+  app.component(Comment.name, Comment)
+}
+
+export { Comment }
+
+export default {
+  title: 'Comment 评论',
+  category: '数据展示',
+  status: '50%', // TODO: 组件若开发完成则填入"已完成"，并删除该注释
+  install(app: App): void {
+        app.use(Comment as any)
+  }
+}
