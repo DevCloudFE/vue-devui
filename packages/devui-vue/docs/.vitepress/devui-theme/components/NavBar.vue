090559ec66f396935ade95ca1edd0ae4249b8319
ElsaOOo <yjjjng0901@126.com>
2021-11-26 11:15:10 +0800

    fix: vitepress文档的header层级过低，容易被demo的内容遮挡

--- packages/devui-vue/docs/.vitepress/devui-theme/components/NavBar.vue
+++ packages/devui-vue/docs/.vitepress/devui-theme/components/NavBar.vue
@@ -14,12 +14,12 @@ const theme = new Theme('light')
 const darkMode = ref(false)
 
 const defaultLanguage = ref(localStorage.getItem('preferred_lang'))
-function useTranslation (target){
+function useTranslation(target) {
   defaultLanguage.value = target
   localStorage.setItem('preferred_lang', target)
-  if (target === 'en-US'){
+  if (target === 'en-US') {
     location.pathname = `/en-US${location.pathname}`
-  }else if (target === 'zh-CN'){
+  } else if (target === 'zh-CN') {
     location.pathname = `${location.pathname.split('/en-US')[1]}`
   }
 }
@@ -48,7 +48,11 @@ defineEmits(['toggle'])
       </div>
 
       <div class="custom-nav flex items-center ml-l">
-        <div class="custom-nav-item ml-m" style="font-size: 0;" @click="() => useTranslation( defaultLanguage === 'zh-CN' ? 'en-US' : 'zh-CN' )">
+        <div
+          class="custom-nav-item ml-m"
+          style="font-size: 0"
+          @click="() => useTranslation(defaultLanguage === 'zh-CN' ? 'en-US' : 'zh-CN')"
+        >
           <ZhLang v-if="defaultLanguage === 'zh-CN'"></ZhLang>
           <EnLang v-else></EnLang>
         </div>
@@ -56,8 +60,13 @@ defineEmits(['toggle'])
           <DarkMode v-if="darkMode" @click="darkMode = !darkMode"></DarkMode>
           <LightMode v-else @click="darkMode = !darkMode"></LightMode>
         </div>
-        <a class="ml-m" style="font-size: 0;" href="https://gitee.com/devui/vue-devui/stargazers">
-          <img :src="`https://gitee.com/devui/vue-devui/badge/star.svg?theme=${darkMode ? 'dark' : 'white'}`" alt="star" />
+        <a class="ml-m" style="font-size: 0" href="https://gitee.com/devui/vue-devui/stargazers">
+          <img
+            :src="`https://gitee.com/devui/vue-devui/badge/star.svg?theme=${
+              darkMode ? 'dark' : 'white'
+            }`"
+            alt="star"
+          />
         </a>
       </div>
     </div>
@@ -74,7 +83,7 @@ defineEmits(['toggle'])
   top: 0;
   right: 0;
   left: 0;
-  z-index: var(--z-index-navbar);
+  z-index: 20;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -108,7 +117,8 @@ defineEmits(['toggle'])
 }
 
 .custom-nav-item:hover {
-  svg, path {
+  svg,
+  path {
     fill: $devui-brand;
   }
 }
