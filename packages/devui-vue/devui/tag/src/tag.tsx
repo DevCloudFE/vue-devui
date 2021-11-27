f47c0c6a6754d8188038f8dfe24e37e78801bc76
codedance <codedance@sohu.com>
2021-11-27 22:48:50 +0800

    feat: 新增可移除属性deletable

--- packages/devui-vue/devui/tag/src/tag.tsx
+++ packages/devui-vue/devui/tag/src/tag.tsx
@@ -1,22 +1,42 @@
-import { defineComponent, ref, toRefs } from 'vue'
+import { defineComponent, toRefs } from 'vue'
 import { tagProps, TagProps } from './tag-types'
 import { useClass, useColor } from './hooks'
+import IconClose from './assets/close.svg'
+import IconDefaultClose from './assets/defaultClose.svg'
 import './tag.scss'
 // 类型声明
 
 export default defineComponent({
   name: 'DTag',
   props: tagProps,
-  emits: ['click'],
+  emits: ['click', 'tagDelete'],
   setup(props: TagProps, { slots, emit }) {
-    const { type, color, checked, titleContent } = toRefs(props)
+    const { type, color, checked, titleContent, deletable } = toRefs(props)
     const tagClass = useClass(props)
     const themeColor = useColor(props)
     const tagTitle = titleContent.value || ''
+    const isDefaultTag = () => !type.value && !color.value
     // 子组件的点击事件
     const Click = () => {
       emit('click')
     }
+    const handleDelete = () => {
+      emit('tagDelete')
+    }
+    const closeIconEl = () => {
+      return deletable.value ? (
+        <a
+          class='remove-button'
+          style={{
+            color: checked.value ? '#fff' : themeColor.value
+          }}
+          onClick={handleDelete}
+        >
+          {isDefaultTag() ? <IconDefaultClose /> : <IconClose />}
+        </a>
+      ) : null
+    }
+
     return () => (
       <div class='devui-tag' onClick={Click}>
         <span
@@ -35,6 +55,7 @@ export default defineComponent({
           title={tagTitle}
         >
           {slots.default?.()}
+          {closeIconEl()}
         </span>
       </div>
     )
