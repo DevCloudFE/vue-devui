921f50eac7358b2a9a492f55e400d9ca99f60bf9
codedance <codedance@sohu.com>
2021-11-27 23:10:40 +0800

    feat: 新增tag点击移除后销毁

--- packages/devui-vue/devui/tag/src/tag.tsx
+++ packages/devui-vue/devui/tag/src/tag.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, toRefs } from 'vue'
+import { defineComponent, toRefs, ref } from 'vue'
 import { tagProps, TagProps } from './tag-types'
 import { useClass, useColor } from './hooks'
 import IconClose from './assets/close.svg'
@@ -16,11 +16,13 @@ export default defineComponent({
     const themeColor = useColor(props)
     const tagTitle = titleContent.value || ''
     const isDefaultTag = () => !type.value && !color.value
+    const isShow = ref(true)
     // 子组件的点击事件
     const Click = () => {
       emit('click')
     }
     const handleDelete = () => {
+      isShow.value = false
       emit('tagDelete')
     }
     const closeIconEl = () => {
@@ -37,21 +39,22 @@ export default defineComponent({
       ) : null
     }
 
-    return () => (
-      <div class='devui-tag' onClick={Click}>
-        <span
-          class={tagClass.value}
-          style={{
-            display: 'block',
-            color: checked.value ? '#fff' : themeColor.value,
-            backgroundColor: checked.value ? themeColor.value : (!color.value ? '' : '#fff')
-          }}
-          title={tagTitle}
-        >
-          {slots.default?.()}
-          {closeIconEl()}
-        </span>
-      </div>
-    )
+    return () =>
+      isShow.value && (
+        <div class='devui-tag' onClick={Click} v-show={isShow.value}>
+          <span
+            class={tagClass.value}
+            style={{
+              display: 'block',
+              color: checked.value ? '#fff' : themeColor.value,
+              backgroundColor: checked.value ? themeColor.value : !color.value ? '' : '#fff'
+            }}
+            title={tagTitle}
+          >
+            {slots.default?.()}
+            {closeIconEl()}
+          </span>
+        </div>
+      )
   }
 })
