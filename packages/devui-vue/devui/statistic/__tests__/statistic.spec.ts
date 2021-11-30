423fd1c82eb5733a69bf2e9a6cd8f54209e933dd
erkelost <1256029807@qq.com>
2021-11-30 17:23:21 +0800

    feat(statistic): '新增统计数值组件'

---
+++ packages/devui-vue/devui/statistic/__tests__/statistic.spec.ts
@@ -0,0 +1,8 @@
+import { mount } from '@vue/test-utils'
+import { Statistic } from '../index'
+
+describe('statistic test', () => {
+  it('statistic init render', async () => {
+    // todo
+  })
+})
