fc20ab589a07dd762536eae4c6efa38bb81b2c72
chenxi24 <chengxi_24@163.com>
2021-11-30 17:48:52 +0800

    fix :移除EditableSelect组件内dropdown的install方法

--- packages/devui-vue/devui/editable-select/index.ts
+++ packages/devui-vue/devui/editable-select/index.ts
@@ -1,6 +1,5 @@
 import type { App } from 'vue'
 import EditableSelect from './src/editable-select'
-import EselectDropdown from './src/components/dropdown'
 EditableSelect.install = function (app: App): void {
   app.component(EditableSelect.name, EditableSelect)
 }
@@ -13,6 +12,5 @@ export default {
   status: undefined, // TODO: 组件若开发完成则填入"已完成"，并删除该注释
   install(app: App): void {
     app.use(EditableSelect as any)
-    app.use(EselectDropdown as any)
   }
 }
