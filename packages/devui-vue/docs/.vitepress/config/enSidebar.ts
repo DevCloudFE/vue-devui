e85e795d2077eeab3cf8f576f1e1281b5cd639da
ElsaOOo <yjjjng0901@126.com>
2021-12-01 10:50:10 +0800

    fix: 切换为英文版后左侧导航名称出现NaN, 英文版enSider.ts改为用devui-cli生成

--- packages/devui-vue/docs/.vitepress/config/enSidebar.ts
+++ packages/devui-vue/docs/.vitepress/config/enSidebar.ts
@@ -1,101 +1,364 @@
-const enSidebar = {
-    '/en-US/': [
-        { text: 'Quick Start', link: '/en-US/' },
-        {
-        text: 'General',
-        children: [
-            { text: 'Button', link: '/en-US/components/button/', status: 'completed' },
-            { text: 'Icon', link: '/en-US/components/icon/', status: 'completed' },
-            { text: 'DragDrop', link: '/en-US/components/dragdrop/' },
-            { text: 'Fullscreen', link: '/en-US/components/fullscreen/' },
-            { text: 'Panel', link: '/en-US/components/panel/', status: 'completed' },
-            { text: 'Search', link: '/en-US/components/search/', status: 'completed' },
-            { text: 'Status', link: '/en-US/components/status/', status: 'completed' },
-            { text: 'Sticky', link: '/en-US/components/sticky/' },
-            { text: 'Overlay', link: '/en-US/components/overlay/'}
-        ]
-        },
-        {
-        text: 'Navigation',
-        children: [
-            { text: 'Accordion', link: '/en-US/components/accordion/' },
-            { text: 'Anchor', link: '/en-US/components/anchor/' },
-            { text: 'BackTop', link: '/en-US/components/back-top/' },
-            { text: 'Breadcrumb', link: '/en-US/components/breadcrumb/' },
-            { text: 'Dropdown', link: '/en-US/components/dropdown/' },
-            { text: 'NavSprite', link: '/en-US/components/nav-sprite/' },
-            { text: 'Pagination', link: '/en-US/components/pagination/', status: 'progress' },
-            { text: 'StepsGuide', link: '/en-US/components/steps-guide/' },
-            { text: 'Tabs', link: '/en-US/components/tabs/', status: 'completed' },
-            { text: 'Anchor', link: '/en-US/components/Anchor/' },
-        ]
-        },
-        {
-        text: 'Feedback',
-        children: [
-            { text: 'Alert', link: '/en-US/components/alert/', status: 'completed' },
-            { text: 'Drawer', link: '/en-US/components/drawer/' },
-            { text: 'Loading', link: '/en-US/components/loading/', status: 'completed' },
-            { text: 'Mention', link: '/en-US/components/mention/' },
-            { text: 'Modal', link: '/en-US/components/modal/' },
-            { text: 'Popover', link: '/en-US/components/popover/', status: "progress" },
-            { text: 'ReadTip', link: '/en-US/components/read-tip/' },
-            { text: 'Toast', link: '/en-US/components/toast/', status: 'completed' },
-            { text: 'Tooltip', link: '/en-US/components/tooltip/', status: 'completed' },
-        ]
-        },
-        {
-        text: 'Data Entry',
-        children: [
-            { text: 'AutoComplete', link: '/en-US/components/auto-complete/' },
-            { text: 'Cascader', link: '/en-US/components/cascader/' },
-            { text: 'CategorySearch', link: '/en-US/components/category-search/' },
-            { text: 'Checkbox', link: '/en-US/components/checkbox/', status: 'completed' },
-            { text: 'DatePicker', link: '/en-US/components/date-picker/', status: 'progress' },
-            { text: 'DatePickerPro', link: '/en-US/components/date-picker-pro/' },
-            { text: 'EditableSelect', link: '/en-US/components/editable-select/' },
-            { text: 'Form', link: '/en-US/components/form/' },
-            { text: 'Input', link: '/en-US/components/input/', status: 'completed' },
-            { text: 'InputNumber', link: '/en-US/components/input-number/' },
-            { text: 'MultiAutoComplete', link: '/en-US/components/multi-auto-complete/' },
-            { text: 'Radio', link: '/en-US/components/radio/', status: 'completed' },
-            { text: 'Select', link: '/en-US/components/select/', status: 'progress' },
-            { text: 'Slider', link: '/en-US/components/slider/' },
-            { text: 'Switch', link: '/en-US/components/switch/', status: 'completed' },
-            { text: 'TagInput', link: '/en-US/components/tag-input/', status: 'completed' },
-            { text: 'Textarea', link: '/en-US/components/textarea/' },
-            { text: 'TimePicker', link: '/en-US/components/time-picker/' },
-            { text: 'Transfer', link: '/en-US/components/transfer/' },
-            { text: 'TreeSelect', link: '/en-US/components/tree-select/' },
-            { text: 'Upload', link: '/en-US/components/upload/', status: 'progress' },
-        ]
-        },
-        {
-        text: 'Data Display',
-        children: [
-            { text: 'Avatar', link: '/en-US/components/avatar/', status: 'completed' },
-            { text: 'Badge', link: '/en-US/components/badge/', status: 'completed' },
-            { text: 'Card', link: '/en-US/components/card/', status: 'completed' },
-            { text: 'Carousel', link: '/en-US/components/carousel/', status: 'completed' },
-            { text: 'DataTable', link: '/en-US/components/data-table/' },
-            { text: 'Gantt', link: '/en-US/components/gantt/' },
-            { text: 'ImagePreview', link: '/en-US/components/image-preview/' },
-            { text: 'Progress', link: '/en-US/components/progress/', status: 'completed' },
-            { text: 'QuadrantDiagram', link: '/en-US/components/quadrant-diagram/' },
-            { text: 'Rate', link: '/en-US/components/rate/', status: 'completed' },
-            { text: 'Tag', link: '/en-US/components/tag/' },
-            { text: 'Tree', link: '/en-US/components/tree/' },
-        ]
-        },
-        {
-        text: 'Layout',
-        children: [
-            { text: 'Layout', link: '/en-US/components/layout/' },
-            { text: 'Splitter', link: '/en-US/components/splitter/' }
-        ]
-        },
-    ]
-}
-
-export default enSidebar
-  
+      export default {
+        '/': [
+	  {
+	    "text": "Quick Start",
+	    "link": "/en-US/"
+	  },
+	  {
+	    "text": "General",
+	    "children": [
+	      {
+	        "text": "Button",
+	        "link": "/en-US/components/button/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Dragdrop",
+	        "link": "/en-US/components/dragdrop/",
+	        "status": "10%"
+	      },
+	      {
+	        "text": "Fullscreen",
+	        "link": "/en-US/components/fullscreen/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Icon",
+	        "link": "/en-US/components/icon/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Overlay",
+	        "link": "/en-US/components/overlay/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Panel",
+	        "link": "/en-US/components/panel/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Ripple",
+	        "link": "/en-US/components/ripple/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Search",
+	        "link": "/en-US/components/search/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Status",
+	        "link": "/en-US/components/status/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Sticky",
+	        "link": "/en-US/components/sticky/",
+	        "status": "50%"
+	      }
+	    ]
+	  },
+	  {
+	    "text": "Navigation",
+	    "children": [
+	      {
+	        "text": "Accordion",
+	        "link": "/en-US/components/accordion/",
+	        "status": "10%"
+	      },
+	      {
+	        "text": "Anchor",
+	        "link": "/en-US/components/anchor/",
+	        "status": "50%"
+	      },
+	      {
+	        "text": "BackTop",
+	        "link": "/en-US/components/back-top/",
+	        "status": "60%"
+	      },
+	      {
+	        "text": "Breadcrumb",
+	        "link": "/en-US/components/breadcrumb/",
+	        "status": "50%"
+	      },
+	      {
+	        "text": "Dropdown",
+	        "link": "/en-US/components/dropdown/",
+	        "status": "10%"
+	      },
+	      {
+	        "text": "NavSprite",
+	        "link": "/en-US/components/nav-sprite/",
+	        "status": "10%"
+	      },
+	      {
+	        "text": "Pagination",
+	        "link": "/en-US/components/pagination/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "StepsGuide",
+	        "link": "/en-US/components/steps-guide/",
+	        "status": "80%"
+	      },
+	      {
+	        "text": "Tabs",
+	        "link": "/en-US/components/tabs/",
+	        "status": "60%"
+	      }
+	    ]
+	  },
+	  {
+	    "text": "Feedback",
+	    "children": [
+	      {
+	        "text": "Alert",
+	        "link": "/en-US/components/alert/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Drawer",
+	        "link": "/en-US/components/drawer/",
+	        "status": "30%"
+	      },
+	      {
+	        "text": "Loading",
+	        "link": "/en-US/components/loading/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Modal",
+	        "link": "/en-US/components/modal/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Popover",
+	        "link": "/en-US/components/popover/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "ReadTip",
+	        "link": "/en-US/components/read-tip/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Result",
+	        "link": "/en-US/components/result/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Toast",
+	        "link": "/en-US/components/toast/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Tooltip",
+	        "link": "/en-US/components/tooltip/",
+	        "status": "50%"
+	      }
+	    ]
+	  },
+	  {
+	    "text": "Data Entry",
+	    "children": [
+	      {
+	        "text": "Cascader",
+	        "link": "/en-US/components/cascader/",
+	        "status": "30%"
+	      },
+	      {
+	        "text": "Checkbox",
+	        "link": "/en-US/components/checkbox/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "DatePicker",
+	        "link": "/en-US/components/date-picker/",
+	        "status": "50%"
+	      },
+	      {
+	        "text": "EditableSelect",
+	        "link": "/en-US/components/editable-select/",
+	        "status": "10%"
+	      },
+	      {
+	        "text": "Form",
+	        "link": "/en-US/components/form/",
+	        "status": "75%"
+	      },
+	      {
+	        "text": "Input",
+	        "link": "/en-US/components/input/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "InputIcon",
+	        "link": "/en-US/components/input-icon/",
+	        "status": "75%"
+	      },
+	      {
+	        "text": "InputNumber",
+	        "link": "/en-US/components/input-number/",
+	        "status": "50%"
+	      },
+	      {
+	        "text": "Radio",
+	        "link": "/en-US/components/radio/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Select",
+	        "link": "/en-US/components/select/",
+	        "status": "10%"
+	      },
+	      {
+	        "text": "Slider",
+	        "link": "/en-US/components/slider/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Switch",
+	        "link": "/en-US/components/switch/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "TagInput",
+	        "link": "/en-US/components/tag-input/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Textarea",
+	        "link": "/en-US/components/textarea/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "TimePicker",
+	        "link": "/en-US/components/time-picker/",
+	        "status": "90%"
+	      },
+	      {
+	        "text": "Transfer",
+	        "link": "/en-US/components/transfer/",
+	        "status": "10%"
+	      },
+	      {
+	        "text": "TreeSelect",
+	        "link": "/en-US/components/tree-select/",
+	        "status": "20%"
+	      },
+	      {
+	        "text": "Upload",
+	        "link": "/en-US/components/upload/",
+	        "status": "100%"
+	      }
+	    ]
+	  },
+	  {
+	    "text": "Data Display",
+	    "children": [
+	      {
+	        "text": "Avatar",
+	        "link": "/en-US/components/avatar/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Badge",
+	        "link": "/en-US/components/badge/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Card",
+	        "link": "/en-US/components/card/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Carousel",
+	        "link": "/en-US/components/carousel/",
+	        "status": "80%"
+	      },
+	      {
+	        "text": "Comment",
+	        "link": "/en-US/components/comment/",
+	        "status": "50%"
+	      },
+	      {
+	        "text": "Countdown",
+	        "link": "/en-US/components/countdown/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Gantt",
+	        "link": "/en-US/components/gantt/",
+	        "status": "10%"
+	      },
+	      {
+	        "text": "ImagePreview",
+	        "link": "/en-US/components/image-preview/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Progress",
+	        "link": "/en-US/components/progress/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "QuadrantDiagram",
+	        "link": "/en-US/components/quadrant-diagram/",
+	        "status": "10%"
+	      },
+	      {
+	        "text": "Rate",
+	        "link": "/en-US/components/rate/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Skeleton",
+	        "link": "/en-US/components/skeleton/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Table",
+	        "link": "/en-US/components/table/",
+	        "status": "10%"
+	      },
+	      {
+	        "text": "Tag",
+	        "link": "/en-US/components/tag/",
+	        "status": "70%"
+	      },
+	      {
+	        "text": "TimeAxis",
+	        "link": "/en-US/components/time-axis/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Tree",
+	        "link": "/en-US/components/tree/",
+	        "status": "20%"
+	      }
+	    ]
+	  },
+	  {
+	    "text": "Layout",
+	    "children": [
+	      {
+	        "text": "Grid",
+	        "link": "/en-US/components/grid/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Layout",
+	        "link": "/en-US/components/layout/",
+	        "status": "100%"
+	      },
+	      {
+	        "text": "Splitter",
+	        "link": "/en-US/components/splitter/",
+	        "status": "100%"
+	      }
+	    ]
+	  }
+	]
+      }
+      
