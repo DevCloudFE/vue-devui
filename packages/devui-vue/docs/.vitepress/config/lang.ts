32e0ddbd50d68b44ec7bebad9a9ad083af0e7e73
ElsaOOo <yjjjng0901@126.com>
2021-12-01 16:51:48 +0800

    fix: demoblock的折叠板，在英文环境下仍然显示中文,以及快速前往文字切换英文

--- packages/devui-vue/docs/.vitepress/config/lang.ts
+++ packages/devui-vue/docs/.vitepress/config/lang.ts
@@ -1,10 +1,10 @@
 const lang = {
-  '/zh': {
+  '/': {
     'hide-text': '隐藏代码',
     'show-text': '显示代码',
     'copy-button-text': '复制代码片段'
   },
-  '/en': {
+  '/en-US': {
     'hide-text': 'Hide',
     'show-text': 'Expand',
     'copy-button-text': 'Copy'
