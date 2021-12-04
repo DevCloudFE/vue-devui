88daef98fc2d5a4204f310bc4928a85a0382f17d
1445654576@qq.com <1445654576@qq.com>
2021-12-04 15:49:33 +0800

    fix: 重构popover的显示方式

--- packages/devui-vue/devui/form/src/form-control/form-control.tsx
+++ packages/devui-vue/devui/form/src/form-control/form-control.tsx
@@ -1,8 +1,8 @@
-import { defineComponent, inject, ref, computed, reactive, onMounted } from 'vue';
+import { defineComponent, inject, ref, computed, reactive, onMounted, Teleport } from 'vue';
 import { uniqueId } from 'lodash-es';
 import { IForm, formControlProps, formInjectionKey } from '../form-types';
 import { ShowPopoverErrorMessageEventData } from '../directive/d-validate-rules'
-import { EventBus } from '../util';
+import { EventBus, getElOffset } from '../util';
 import Icon from '../../../icon/src/icon';
 import Popover from '../../../popover/src/popover';
 import './form-control.scss';
@@ -21,10 +21,25 @@ export default defineComponent({
     const showPopover = ref(false);
     const tipMessage = ref("");
     const popPosition = ref<positionType>("bottom");
+    let rectInfo: Partial<DOMRect> = {
+      width: 0,
+      height: 0
+    };
+    let elOffset = {
+      left: 0,
+      top: 0
+    }
+    let popoverLeftPosition = 0 ;
+    let popoverTopPosition = 0 ;
 
     onMounted(() => {
+      const el = document.getElementById(uid);
+      elOffset = getElOffset(el);
       EventBus.on("showPopoverErrorMessage", (data: ShowPopoverErrorMessageEventData) => {
         if (uid === data.uid) {
+          rectInfo = el.getBoundingClientRect();
+          popoverLeftPosition = popPosition.value === "top" || popPosition.value === "bottom" ? rectInfo.right - (rectInfo.width / 2) : rectInfo.right;
+          popoverTopPosition = popPosition.value === "top" ? elOffset.top + (rectInfo.height / 2) - rectInfo.height : elOffset.top + (rectInfo.height / 2);
           showPopover.value = data.showPopover;
           tipMessage.value = data.message;
           popPosition.value = data.popPosition as any; // todo: 待popover组件positionType完善类型之后再替换类型
@@ -51,14 +66,20 @@ export default defineComponent({
         extraInfo,
       } = props;
       return <div class="form-control" ref={formControl} data-uid={uid}>
+        <Teleport to="body">
+            <div style={{
+              position: 'absolute',
+              left: popoverLeftPosition + 'px',
+              top: popoverTopPosition + 'px',
+              width: rectInfo.width + 'px',
+              height: rectInfo.height + 'px',
+            }}>
+              <Popover visible={showPopover.value} content={tipMessage.value} popType={"error"} position={popPosition.value} />
+            </div>
+          </Teleport>
         <div class={`devui-form-control-container${isHorizontal ? ' devui-form-control-container-horizontal' : ''}${feedbackStatus ? ' has-feedback' : ''}${feedbackStatus === 'error' ? ' feedback-error' : ''}`}>
-          <div class={`devui-control-content-wrapper ${showPopover.value ? 'with-popover' : ''}`}>
+          <div class="devui-control-content-wrapper" id={uid}>
             {ctx.slots.default?.()}
-            { showPopover.value && 
-              <div class="devui-popover-wrapper">
-                <Popover visible={true} content={tipMessage.value} popType={"error"} position={popPosition.value} />
-              </div>
-            }
           </div>
           {
             (feedbackStatus || ctx.slots.suffixTemplate?.()) &&
