c2dd1f6df2d6af3e63fc1c9389d35b7c58f2c75b
小九九 <61737780+asdlml6@users.noreply.github.com>
2021-12-26 21:47:25 +0800

    实现dragdrop指令基础用法 (#101)
    * docs: 增加tooltip英文文档
    
    * fixed: v-d-draggable、droppable基本用法完成
    
    * feat: draggable、droppable最基础指令完成
    
    * 拖拽指令基础用法已完成
    
    * feat: 基础用法已完成
    
    * feat: 新增dragable指令不可排序功能

--- packages/devui-vue/devui/dragdrop/src/draggable-directive.ts
+++ packages/devui-vue/devui/dragdrop/src/draggable-directive.ts
@@ -1,11 +1,41 @@
+import { changeDragState, deleteInsertedSortableShadow } from './utils'
+import { shadowId } from './constant'
+
 export default {
-  mounted(el: HTMLElement): void {
-    el.setAttribute('draggable', 'true')
+  /**
+   * 
+   * @param el 
+   * @description
+   *   1、绑定该指令的element将会具备拖拽能力
+   *   2、为各元素进行初始化配置
+   *      2.1、dragFlag:      是否处于拖拽中
+   *      2.2、dragOverFlag： 是否处于可放置区域
+   * 
+   *   1、整体思路
+   *      1.1、为每个绑定drag指令的元素维护状态
+   *        1.1.1、状态集合：dragStart、drag、dragover、drop、shouldCreateShadow
+   * 
+   *      1.2、进入drop区域后，确保drop区域能够获取正在进行drag的元素
+   */
+  mounted(el: HTMLElement, binding: unknown): void {
+    el.setAttribute('draggable', 'true');
     el.style.cursor = 'grab'
 
     // dragstart/drag/dragend
-    el.addEventListener('dragstart', (event: DragEvent) => {
-      event.dataTransfer.setData('originId', el.id)
-    })
+    el.addEventListener('drag', () => {
+      changeDragState(el, el.id, 'true', 'true', 'false', 'false', 'false', 'true')
+      if (binding.instance.$root.dropElement && document.getElementById(shadowId)){
+        deleteInsertedSortableShadow(binding.instance.$root.dropElement)     // 如何让它仅执行1次？
+        binding.instance.$root.dropElement = null
+      }
+    }, false)
+
+    // dragStart事件为每个绑定元素进行初始化
+    el.addEventListener('dragstart', ()=>{
+      // el  or  binding.instance  or  vnode.context
+      changeDragState(el, el.id, 'true', 'true', 'false', 'false', 'false', 'false')
+      binding.instance.$root.identity = el.id
+      el.dataset.dragArea = el.parentNode.className
+    }, false)
   },
 }
