0f2dc3ccae64be5d5d76b72648d5b89b2c59ed65
xiaojiujiu <3507440900@qq.com>
2022-01-08 11:28:52 +0800

    合并冲突

---
+++ packages/devui-vue/devui/splitter/__mocks__/event-helper.ts
@@ -0,0 +1,36 @@
+export function mouseMoveTrigger(
+  el: HTMLElement,
+  from: { x: number; y: number; },
+  to: { x: number; y: number; }
+): void {
+  if (typeof window === 'undefined') {
+    return
+  }
+  dispatchMouseEvent(el, 'mousedown', from.x, from.y)
+  dispatchMouseEvent(window.document, 'mousemove', to.x, to.y)
+  dispatchMouseEvent(window.document, 'mouseup')
+}
+
+export function dispatchMouseEvent(
+  node: Node,
+  type: string,
+  x = 0,
+  y = 0,
+  event: MouseEvent = createMouseEvent(type, x, y)
+): void {
+  node.dispatchEvent(event)
+}
+
+export function createMouseEvent(
+  type: string,
+  x = 0,
+  y = 0
+): MouseEvent {
+  const event = new MouseEvent(type, {
+    bubbles: true,
+    cancelable: false,
+    clientX: x,
+    clientY: y,
+  })
+  return event
+}
