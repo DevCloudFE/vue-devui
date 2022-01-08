fc1ba6281747c981f1cc731b8d1391d9b341d90c
Echo <nts318@yeah.net>
2022-01-08 10:40:35 +0800

    feat: transfer add drag sort (#51)
    * fix: 穿梭框进度更改
    
    * feat: 穿梭框新增拖拽排序功能
    
    * fix: 解决ExtractPropTypes、PropType引用不正确
    
    * feat: 穿梭框全选增加半选中效果

--- packages/devui-vue/devui/transfer/index.ts
+++ packages/devui-vue/devui/transfer/index.ts
@@ -2,7 +2,7 @@ import type { App } from 'vue'
 import Transfer from './src/transfer'
 
 Transfer.install = function (app: App) {
-    app.component(Transfer.name, Transfer)
+  app.component(Transfer.name, Transfer)
 }
 
 export { Transfer }
@@ -10,7 +10,7 @@ export { Transfer }
 export default {
   title: 'Transfer 穿梭框',
   category: '数据录入',
-  status: '10%',
+  status: '40%',
   install(app: App): void {
     app.use(Transfer as any)
   }
