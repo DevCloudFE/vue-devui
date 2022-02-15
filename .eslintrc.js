4bb5d9a13a961d90de654b81ead08b9fdeef2696
sufuwang <1137120948@qq.com>
2022-02-15 22:05:55 +0800

    fix: eslint no-undef

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,5 +44,13 @@ module.exports = {
     ],
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': ['error']
-  }
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'no-undef': 'off'
+      }
+    }
+  ]
 }
