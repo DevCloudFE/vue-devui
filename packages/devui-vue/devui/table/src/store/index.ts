b80b85b25750f388e4739ee4297d354182407b9c
wangyupei <2311595895@qq.com>
2022-03-08 15:08:15 +0800

    fix: 修复列排序bug，d-column增加默认插槽

--- packages/devui-vue/devui/table/src/store/index.ts
+++ packages/devui-vue/devui/table/src/store/index.ts
@@ -3,27 +3,31 @@ import { Column, CompareFn, FilterResults } from '../column/column.type';
 import { SortDirection } from '../table.type';
 export interface TableStore<T = Record<string, any>> {
   states: {
-    _data: Ref<T[]>
-    _columns: Ref<Column[]>
-    _checkList: Ref<boolean[]>
-    _checkAll: Ref<boolean>
-    _halfChecked: Ref<boolean>
-    isFixedLeft: Ref<boolean>
-  }
-  insertColumn(column: Column): void
-  sortColumn(): void
-  removeColumn(column: Column): void
-  getCheckedRows(): T[]
-  sortData(field: string, direction: SortDirection, compareFn: CompareFn<T>): void
-  filterData(field: string, results: FilterResults): void
-  resetFilterData(): void
+    _data: Ref<T[]>;
+    _columns: Ref<Column[]>;
+    _checkList: Ref<boolean[]>;
+    _checkAll: Ref<boolean>;
+    _halfChecked: Ref<boolean>;
+    isFixedLeft: Ref<boolean>;
+  };
+  insertColumn(column: Column): void;
+  sortColumn(): void;
+  removeColumn(column: Column): void;
+  getCheckedRows(): T[];
+  sortData(field: string, direction: SortDirection, compareFn: CompareFn<T>): void;
+  filterData(field: string, results: FilterResults): void;
+  resetFilterData(): void;
 }
 
 export function createStore<T>(dataSource: Ref<T[]>): TableStore<T> {
   const _data: Ref<T[]> = ref([]);
-  watch(dataSource, (value: T[]) => {
-    _data.value = [...value];
-  }, { deep: true, immediate: true });
+  watch(
+    dataSource,
+    (value: T[]) => {
+      _data.value = [...value];
+    },
+    { deep: true, immediate: true }
+  );
 
   const { _columns, insertColumn, removeColumn, sortColumn } = createColumnGenerator();
   const { _checkAll, _checkList, _halfChecked, getCheckedRows } = createSelection(dataSource, _data);
@@ -39,7 +43,7 @@ export function createStore<T>(dataSource: Ref<T[]>): TableStore<T> {
       _checkList,
       _checkAll,
       _halfChecked,
-      isFixedLeft
+      isFixedLeft,
     },
     insertColumn,
     sortColumn,
@@ -47,38 +51,38 @@ export function createStore<T>(dataSource: Ref<T[]>): TableStore<T> {
     getCheckedRows,
     sortData,
     filterData,
-    resetFilterData
+    resetFilterData,
   };
 }
 
 /**
  * 列生成器
- * @returns 
+ * @returns
  */
 const createColumnGenerator = () => {
   const _columns: Ref<Column[]> = ref([]);
 
   /**
    * 插入当前列
-   * @param {Column} column 
+   * @param {Column} column
    */
   const insertColumn = (column: Column) => {
     _columns.value.push(column);
     // 实际上就是插入排序
-    _columns.value.sort((a, b) => a.order > b.order ? 1 : -1);
+    _columns.value.sort((a, b) => a.order - b.order);
   };
 
   /**
    * 对 column 进行排序
    */
   const sortColumn = () => {
-    _columns.value.sort((a, b) => a.order > b.order ? 1 : -1);
-  }
+    _columns.value.sort((a, b) => (a.order > b.order ? 1 : -1));
+  };
 
   /**
    * 移除当前列
-   * @param {Column} column 
-   * @returns 
+   * @param {Column} column
+   * @returns
    */
   const removeColumn = (column: Column) => {
     const i = _columns.value.findIndex((v) => v === column);
@@ -86,15 +90,15 @@ const createColumnGenerator = () => {
       return;
     }
     _columns.value.splice(i, 1);
-  }
+  };
   return { _columns, insertColumn, removeColumn, sortColumn };
-}
+};
 
 /**
  * 选择功能
- * @param dataSource 
- * @param _data 
- * @returns 
+ * @param dataSource
+ * @param _data
+ * @returns
  */
 const createSelection = <T>(dataSource: Ref<T[]>, _data: Ref<T[]>) => {
   const _checkList: Ref<boolean[]> = ref([]);
@@ -107,30 +111,37 @@ const createSelection = <T>(dataSource: Ref<T[]>, _data: Ref<T[]>) => {
       for (let i = 0; i < _checkList.value.length; i++) {
         _checkList.value[i] = val;
       }
-    }
+    },
   });
   const _halfChecked = ref(false);
 
-  watch(dataSource, (value: T[]) => {
-    _checkList.value = new Array(value.length).fill(false);
-  }, { deep: true, immediate: true });
+  watch(
+    dataSource,
+    (value: T[]) => {
+      _checkList.value = new Array(value.length).fill(false);
+    },
+    { deep: true, immediate: true }
+  );
 
   // checkList 只有全为true的时候
-  watch(_checkList, (list) => {
-    if (list.length === 0) {
-      return;
-    }
-    let allTrue = true;
-    let allFalse = true;
-    for (let i = 0; i < list.length; i++) {
-      allTrue &&= list[i];
-      allFalse &&= !list[i];
-    }
+  watch(
+    _checkList,
+    (list) => {
+      if (list.length === 0) {
+        return;
+      }
+      let allTrue = true;
+      let allFalse = true;
+      for (let i = 0; i < list.length; i++) {
+        allTrue &&= list[i];
+        allFalse &&= !list[i];
+      }
 
-    _checkAllRecord.value = allTrue;
-    _halfChecked.value = !(allFalse || allTrue);
-
-  }, { immediate: true, deep: true });
+      _checkAllRecord.value = allTrue;
+      _halfChecked.value = !(allFalse || allTrue);
+    },
+    { immediate: true, deep: true }
+  );
 
   /**
    * 获取当前已选数据
@@ -138,51 +149,51 @@ const createSelection = <T>(dataSource: Ref<T[]>, _data: Ref<T[]>) => {
    */
   const getCheckedRows = (): T[] => {
     return _data.value.filter((_, index) => _checkList.value[index]);
-  }
+  };
 
   return {
     _checkList,
     _checkAll,
     _halfChecked,
-    getCheckedRows
+    getCheckedRows,
   };
-}
+};
 
 /**
  * 排序功能
  * @template T
- * @param dataSource 
- * @param _data 
+ * @param dataSource
+ * @param _data
  */
 const createSorter = <T>(dataSource: Ref<T[]>, _data: Ref<T[]>) => {
   /**
    * 对数据进行排序
-   * @param {string} field 
-   * @param {SortDirection} direction 
+   * @param {string} field
+   * @param {SortDirection} direction
    * @param {CompareFn<T>} compareFn
    */
   const sortData = (
     field: string,
     direction: SortDirection,
-    compareFn: CompareFn<T> = (field: string, a: T, b: T) => a[field] > b[field]
+    compareFn: CompareFn<T> = (fieldKey: string, a: T, b: T) => a[fieldKey] > b[fieldKey]
   ) => {
     if (direction === 'ASC') {
-      _data.value = _data.value.sort((a, b) => compareFn(field, a, b) ? 1 : -1);
+      _data.value = _data.value.sort((a, b) => (compareFn(field, a, b) ? 1 : -1));
     } else if (direction === 'DESC') {
-      _data.value = _data.value.sort((a, b) => !compareFn(field, a, b) ? 1 : -1);
+      _data.value = _data.value.sort((a, b) => (!compareFn(field, a, b) ? 1 : -1));
     } else {
       _data.value = [...dataSource.value];
     }
-  }
+  };
   return { sortData };
-}
+};
 
 /**
  * 过滤功能
  * @template T
- * @param dataSource 
- * @param _data 
- * @returns 
+ * @param dataSource
+ * @param _data
+ * @returns
  */
 const createFilter = <T>(dataSource: Ref<T[]>, _data: Ref<T[]>) => {
   // 过滤数据所需要的
@@ -193,26 +204,26 @@ const createFilter = <T>(dataSource: Ref<T[]>, _data: Ref<T[]>) => {
   const filterData = (field: string, results: FilterResults) => {
     fieldSet.add(field);
     const fields = [...fieldSet];
-    _data.value = dataSource.value.filter(item => {
-      return fields.reduce<boolean>((prev, field) => {
-        return prev && (results.indexOf(item[field]) !== -1)
+    _data.value = dataSource.value.filter((item) => {
+      return fields.reduce<boolean>((prev, fieldKey) => {
+        return prev && results.indexOf(item[fieldKey]) !== -1;
       }, true);
     });
-  }
+  };
   /**
    * 重置数据为最开始的状态
    */
   const resetFilterData = () => {
     fieldSet.clear();
     _data.value = [...dataSource.value];
-  }
+  };
   return { filterData, resetFilterData };
-}
+};
 
 const createFixedLogic = (columns: Ref<Column[]>) => {
   const isFixedLeft = computed(() => {
     return columns.value.reduce((prev, current) => prev || !!current.fixedLeft, false);
   });
 
-  return { isFixedLeft }
-}
+  return { isFixedLeft };
+};
