eff09a92e0f5cfb93beb97d4d3dda031dfd6f0cf
行言 <2311595895@qq.com>
2022-03-11 17:09:26 +0800

    refactor(Popover): 重构Popover (#243)

---
+++ packages/devui-vue/devui/popover/src/popover-types.ts
@@ -0,0 +1,64 @@
+import type { PropType, ExtractPropTypes } from 'vue';
+
+export type TriggerType = 'click' | 'hover' | 'manually';
+export type PopType = 'success' | 'error' | 'warning' | 'info' | 'default';
+export type Placement =
+  | 'top'
+  | 'right'
+  | 'bottom'
+  | 'left'
+  | 'top-start'
+  | 'top-end'
+  | 'right-start'
+  | 'right-end'
+  | 'bottom-start'
+  | 'bottom-end'
+  | 'left-start'
+  | 'left-end';
+export type Alignment = 'start' | 'end';
+export type OffsetOptions = { mainAxis?: number; crossAxis?: number };
+
+export const popoverProps = {
+  isOpen: {
+    type: Boolean,
+    default: false,
+  },
+  position: {
+    type: Array as PropType<Array<Placement>>,
+    default: ['bottom'],
+  },
+  align: {
+    type: String as PropType<Alignment> | null,
+    default: null,
+  },
+  offset: {
+    type: [Number, Object] as PropType<number | OffsetOptions>,
+    default: 8,
+  },
+  content: {
+    type: String,
+    default: '',
+  },
+  trigger: {
+    type: String as PropType<TriggerType>,
+    default: 'click',
+  },
+  popType: {
+    type: String as PropType<PopType>,
+    default: 'default',
+  },
+  showAnimation: {
+    type: Boolean,
+    default: true,
+  },
+  mouseEnterDelay: {
+    type: Number,
+    default: 150,
+  },
+  mouseLeaveDelay: {
+    type: Number,
+    default: 100,
+  },
+};
+
+export type PopoverProps = ExtractPropTypes<typeof popoverProps>;
---
+++ packages/devui-vue/devui/popover/src/use-popover.ts
@@ -0,0 +1,92 @@
+import { toRefs, ref, computed, watch, onUnmounted, onMounted } from 'vue';
+import type { Ref, ComputedRef } from 'vue';
+import { debounce } from 'lodash';
+import { PopoverProps } from './popover-types';
+
+const TransformOriginMap: Record<string, string> = {
+  top: '50% calc(100% + 8px)',
+  bottom: '50% -8px',
+  left: 'calc(100% + 8px)',
+  right: '-8px 50%',
+};
+
+export function usePopover(
+  props: PopoverProps,
+  visible: Ref<boolean>,
+  placement: Ref<string>,
+  origin: Ref<HTMLElement>,
+  popoverRef: Ref
+): { overlayStyles: ComputedRef<Record<string, number | string>> } {
+  const { trigger, isOpen } = toRefs(props);
+  const overlayStyles = computed(() => ({
+    zIndex: 1060,
+    transformOrigin: TransformOriginMap[placement.value],
+  }));
+
+  const onDocumentClick: (e: Event) => void = (e: Event) => {
+    if (!origin.value.contains(<HTMLElement>e.target) && !popoverRef.value.$el?.contains(e.target)) {
+      visible.value = false;
+    }
+  };
+
+  watch(isOpen, (isOpenVal) => {
+    visible.value = isOpenVal;
+  });
+
+  watch(visible, () => {
+    if (visible.value && trigger.value !== 'manually') {
+      document.addEventListener('click', onDocumentClick);
+    } else {
+      document.removeEventListener('click', onDocumentClick);
+    }
+  });
+  onUnmounted(() => {
+    document.removeEventListener('click', onDocumentClick);
+  });
+
+  return { overlayStyles };
+}
+
+export function usePopoverEvent(
+  props: PopoverProps,
+  visible: Ref<boolean>,
+  origin: Ref
+): { placement: Ref<string>; handlePositionChange: (pos: string) => void } {
+  const { trigger, position, mouseEnterDelay, mouseLeaveDelay } = toRefs(props);
+  const isClick: ComputedRef<boolean> = computed(() => trigger.value === 'click');
+  const placement: Ref<string> = ref(position.value[0].split('-')[0]);
+  const isEnter: Ref<boolean> = ref(false);
+
+  const onClick = () => isClick.value && (visible.value = !visible.value);
+  const enter = debounce(() => {
+    isEnter.value && (visible.value = true);
+  }, mouseEnterDelay.value);
+  const leave = debounce(() => {
+    !isEnter.value && (visible.value = false);
+  }, mouseLeaveDelay.value);
+  const onMouseenter = () => {
+    if (!isClick.value) {
+      isEnter.value = true;
+      enter();
+    }
+  };
+  const onMouseleave = () => {
+    if (!isClick.value) {
+      isEnter.value = false;
+      leave();
+    }
+  };
+  const handlePositionChange: (pos: string) => void = (pos: string) => {
+    placement.value = pos.split('-')[0];
+  };
+  onMounted(() => {
+    if (trigger.value === 'click') {
+      origin.value.addEventListener('click', onClick);
+    } else if (trigger.value === 'hover') {
+      origin.value.addEventListener('mouseenter', onMouseenter);
+      origin.value.addEventListener('mouseleave', onMouseleave);
+    }
+  });
+
+  return { placement, handlePositionChange };
+}
