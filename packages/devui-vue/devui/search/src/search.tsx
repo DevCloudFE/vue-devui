18ab08fba707343d6832148f855c23c51eae4783
行言 <2311595895@qq.com>
2022-03-15 09:47:37 +0800

    refactor: search (#240)
    * refactor(badge): 位置和偏移量参数重命名
    
    * refactor(Search): 修复Search的bug
    
    * fix(Search): search-fn重命名为search

--- packages/devui-vue/devui/search/src/search.tsx
+++ packages/devui-vue/devui/search/src/search.tsx
@@ -1,40 +1,36 @@
-import { defineComponent } from 'vue'
-import { SearchProps, searchProps } from './search-types'
-import { getRootClass } from './hooks/use-search-class'
-import { keywordsHandles } from './hooks/use-search-keywords'
-import { keydownHandles } from './hooks/use-search-keydown'
+import { defineComponent } from 'vue';
+import { SearchProps, searchProps } from './search-types';
+import { getRootClass } from './hooks/use-search-class';
+import { keywordsHandles } from './hooks/use-search-keywords';
+import { keydownHandles } from './hooks/use-search-keydown';
 import DInput from '../../input/src/input';
-import './search.scss'
+import './search.scss';
 
 export default defineComponent({
   name: 'DSearch',
   props: searchProps,
-  emits: ['update:modelValue', 'onSearch'],
+  emits: ['update:modelValue', 'search'],
   setup(props: SearchProps, ctx) {
-    const rootClasses = getRootClass(props)
-    // 输入框内容定义、删改
-    const {keywords, clearIconShow, onClearHandle} = keywordsHandles(ctx, props)
+    const rootClasses = getRootClass(props);
+    const { keywords, clearIconShow, onClearHandle } = keywordsHandles(ctx, props);
+    const { onInputKeydown, onClickHandle, useEmitKeyword } = keydownHandles(ctx, keywords, props);
 
-    // 键盘回车事件
-    const { onInputKeydown, onClickHandle, useEmitKeyword } = keydownHandles(ctx, keywords, props.delay)
-    
-    // 双向绑定
     const onInputUpdate = (event: string) => {
       if (props.isKeyupSearch) {
-        useEmitKeyword(event)
+        useEmitKeyword(event);
       }
-      keywords.value = event
-      ctx.emit('update:modelValue', event)
-    }
+      keywords.value = event;
+      ctx.emit('update:modelValue', event);
+    };
 
     return () => {
       return (
         <div class={rootClasses.value}>
-          {props.iconPosition === 'left' &&
-            <div class="devui-search__icon" onClick={onClickHandle}>
-              <d-icon name="search" size="inherit" key="search"></d-icon>
+          {props.iconPosition === 'left' && (
+            <div class='devui-search__icon' onClick={onClickHandle}>
+              <d-icon name='search' size='inherit' key='search'></d-icon>
             </div>
-          }
+          )}
           <DInput
             size={props.size}
             disabled={props.disabled}
@@ -44,23 +40,19 @@ export default defineComponent({
             placeholder={props.placeholder}
             cssClass={props.cssClass}
             onKeydown={onInputKeydown}
-            onUpdate:modelValue={onInputUpdate}
-          ></DInput>
-            {clearIconShow.value &&
-              <div
-                class="devui-search__clear"
-                onClick={onClearHandle}
-              >
-                <d-icon name="close" size="inherit" key="close"></d-icon>
-              </div>
-            }
-            {props.iconPosition === 'right' &&
-              <div class="devui-search__icon" onClick={onClickHandle}>
-                <d-icon name="search" size="inherit" key="search"></d-icon>
-              </div>
-            }
+            onUpdate:modelValue={onInputUpdate}></DInput>
+          {clearIconShow.value && (
+            <div class='devui-search__clear' onClick={onClearHandle}>
+              <d-icon name='close' size='inherit' key='close'></d-icon>
+            </div>
+          )}
+          {props.iconPosition === 'right' && (
+            <div class='devui-search__icon' onClick={onClickHandle}>
+              <d-icon name='search' size='inherit' key='search'></d-icon>
+            </div>
+          )}
         </div>
-      )
-    }
-  }
-})
+      );
+    };
+  },
+});
