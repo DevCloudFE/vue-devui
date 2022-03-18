802fc43b3d86f858a45f7e71c7369d4e7045710e
Kagol <kagol@sina.com>
2022-03-18 11:32:03 +0800

    refactor(icon): Extract icon-types.ts to keep consistent

--- packages/devui-vue/devui/icon/__tests__/icon.spec.ts
+++ packages/devui-vue/devui/icon/__tests__/icon.spec.ts
@@ -4,26 +4,26 @@ import Icon from '../src/icon';
 describe('d-icon', () => {
   it('name', () => {
     const wrapper = mount(Icon, {
-      props: { name:'add' },
+      props: { name: 'add' },
     });
     expect(wrapper.find('.icon-add').exists()).toBeTruthy();
   });
 
   it('classPrefix', () => {
     const wrapper = mount(Icon, {
-      props: { name:'add',classPrefix:'dev' },
+      props: { name: 'add', classPrefix: 'dev' },
     });
     expect(wrapper.find('.dev-add').exists()).toBeTruthy();
   });
   it('size', () => {
     const wrapper = mount(Icon, {
-      props: { name:'add',size:'80px'},
+      props: { name: 'add', size: '80px' },
     });
     expect(wrapper.find('.icon-add').attributes('style').includes('font-size: 80px')).toBeTruthy();
   });
   it('color', () => {
     const wrapper = mount(Icon, {
-      props: { name:'add',color:'red'},
+      props: { name: 'add', color: 'red' },
     });
     expect(wrapper.find('.icon-add').attributes('style').includes('color: red')).toBeTruthy();
   });
