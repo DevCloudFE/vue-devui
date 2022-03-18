35accce3487a3fdb5b986fe8214c3c70fbd70f25
GaoNeng <31283122+GaoNeng-wWw@users.noreply.github.com>
2022-03-18 10:52:33 +0800

    fix(panel): fix issues #219 (#222)
    * fix(panel): 修复了issues 219提出的部分问题 & 修复vite构建脚本步通过ls门禁问题
    
    * fix(devui-theme): 修复了无法通过门禁的问题
    
    * fix(panel): 修复了文档错误
    
    * fix(panel): 修复了文档错误
    
    * fix(panel): 修复了文档中的错误

--- packages/devui-vue/devui/panel/src/store/store.ts
+++ packages/devui-vue/devui/panel/src/store/store.ts
@@ -1,16 +1,16 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import {ref,reactive} from 'vue';
 
-export const option = reactive({});
+export const option = reactive({}) as Record<string,unknown>;
 
 class Store {
-  public static getByKey(timeStamp){
+  public static getByKey(timeStamp: string){
     return option[timeStamp];
   }
   public static state() {
     return option;
   }
-  public static setData(key,value){
+  public static setData(key: string ,value: unknown){
     option[key] = ref(value);
   }
 }
