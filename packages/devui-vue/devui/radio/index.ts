1a1e619eaa0183121f85bdcdbb6a22d76527e5d3
Jay <xuehongjiej@163.com>
2022-03-19 15:14:48 +0800

    refactor(radio): 修复 eslint 报错、格式化代码统一代码风格 (#299)
    * refactor(radio): 修复 eslint 报错
    
    * style(radio): 格式化代码，统一代码风格

--- packages/devui-vue/devui/radio/index.ts
+++ packages/devui-vue/devui/radio/index.ts
@@ -2,14 +2,6 @@ import type { App } from 'vue';
 import Radio from './src/radio';
 import RadioGroup from './src/radio-group';
 
-Radio.install = function(app: App) {
-  app.component(Radio.name, Radio);
-};
-
-RadioGroup.install = function(app: App) {
-  app.component(RadioGroup.name, RadioGroup);
-};
-
 export { Radio, RadioGroup };
 
 export default {
@@ -17,7 +9,7 @@ export default {
   category: '数据录入',
   status: '100%',
   install(app: App): void {
-    app.use(Radio as any);
-    app.use(RadioGroup as any);
-  }
+    app.component(Radio.name, Radio);
+    app.component(RadioGroup.name, RadioGroup);
+  },
 };
