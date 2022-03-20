8d6e96933d44d4340a4dd3dc8f6dc6e0dbf3a4d3
Bob <731653765@qq.com>
2022-03-20 22:00:50 +0800

    refactor(slider): 修复 eslint 的报错和警告，格式化代码 (#305)
    refactor(slider): test gpg
    
    refactor(slider): update local git user.email
    
    refactor(slider): add the pnpm-debug.log to ignore
    
    Co-authored-by: unknown <bob>

--- packages/devui-vue/devui/slider/src/slider.tsx
+++ packages/devui-vue/devui/slider/src/slider.tsx
@@ -18,39 +18,50 @@ export default defineComponent({
     const newPostion = ref<number>(0);
     // 当前的位置以百分比显示
     const percentDispaly = ref<string>('');
-    const renderShowInput = () => {
-      return props.showInput ? (
-        <div class='devui-input__out-wrap'>
-          <input onInput={handleOnInput} value={inputValue.value + ''}></input>
-        </div>
-      ) : (
-        ''
-      );
-    };
-    // 当传入modelValue时用以定位button的位置
-    if (props.modelValue > props.max) {
-      percentDispaly.value = '100%';
-    } else if (props.modelValue < props.min) {
-      percentDispaly.value = '0%';
-    } else {
-      percentDispaly.value = ((props.modelValue - props.min) * 100) / (props.max - props.min) + '%';
+    // 输入框内的值
+    function handleOnInput(event) {
+      inputValue.value = parseInt(event.target.value);
+      if (!inputValue.value) {
+        inputValue.value = props.min;
+        percentDispaly.value = '0%';
+      } else {
+        if (inputValue.value < props.min || inputValue.value > props.max) {
+          return;
+        }
+        const re = /^(?:[1-9]?\d|100)$/;
+        if (re.test(`${inputValue.value}`)) {
+          percentDispaly.value = ((inputValue.value - props.min) * 100) / (props.max - props.min) + '%';
+          ctx.emit('update:modelValue', inputValue.value);
+        }
+      }
     }
-    // 一挂载就进行当前位置的计算，以后的移动基于当前的位置移动
-    onMounted(() => {
-      const sliderWidth = sliderRunway.value.clientWidth;
-      currentPosition.value = (sliderWidth * (inputValue.value - props.min)) / (props.max - props.min);
-    });
-    function handleButtonMousedown(event: MouseEvent) {
-      popoverShow.value = true;
-      // props.disabled状态是不能点击拖拽的
-      if (props.disabled) {return;}
-      // 阻止默认事件
-      event.preventDefault();
-      dragStart(event);
-      // 当鼠标开始移动时，进行坐标计算
-      window.addEventListener('mousemove', onDragging);
-      // 当鼠标抬起时，停止计算
-      window.addEventListener('mouseup', onDragEnd);
+    function setPostion(newPosition: number) {
+      // 获取slider的实际长度的像素
+      const sliderWidth: number = Math.round(sliderRunway.value.clientWidth);
+      if (newPosition < 0) {
+        newPosition = 0;
+      }
+      // 计算slider的实际像素每段的长度
+      const LengthPerStep = sliderWidth / ((props.max - props.min) / props.step);
+      // 计算实际位移的取整段数
+      const steps = Math.round(newPosition / LengthPerStep);
+      // 实际的偏移像素
+      const value: number = steps * LengthPerStep;
+      // 要是刚好划过半段切刚好超出最大长度的情况进行限定
+      if (Math.round(value) >= sliderWidth) {
+        currentPosition.value = sliderWidth;
+        inputValue.value = props.max;
+        percentDispaly.value = '100%';
+        ctx.emit('update:modelValue', props.max);
+        return;
+      }
+      // 向左偏移百分比的值
+      percentDispaly.value = Math.round((value * 100) / sliderWidth) + '%';
+      // 更新输入框的值
+      inputValue.value = Math.round((value * (props.max - props.min)) / sliderWidth) + props.min;
+      // 设置当前所在的位置
+      currentPosition.value = newPosition;
+      ctx.emit('update:modelValue', inputValue.value);
     }
     function dragStart(event: MouseEvent) {
       // 防止mouseup触发父元素的click事件
@@ -86,33 +97,41 @@ export default defineComponent({
       window.removeEventListener('mousemove', onDragging);
       window.removeEventListener('mouseup', onDragEnd);
     }
-    function setPostion(newPosition: number) {
-      // 获取slider的实际长度的像素
-      const sliderWidth: number = Math.round(sliderRunway.value.clientWidth);
-      if (newPosition < 0) {
-        newPosition = 0;
-      }
-      // 计算slider的实际像素每段的长度
-      const LengthPerStep = sliderWidth / ((props.max - props.min) / props.step);
-      // 计算实际位移的取整段数
-      const steps = Math.round(newPosition / LengthPerStep);
-      // 实际的偏移像素
-      const value: number = steps * LengthPerStep;
-      // 要是刚好划过半段切刚好超出最大长度的情况进行限定
-      if (Math.round(value) >= sliderWidth) {
-        currentPosition.value = sliderWidth;
-        inputValue.value = props.max;
-        percentDispaly.value = '100%';
-        ctx.emit('update:modelValue', props.max);
+    const renderShowInput = () => {
+      return props.showInput ? (
+        <div class='devui-input__out-wrap'>
+          <input onInput={handleOnInput} value={inputValue.value + ''}></input>
+        </div>
+      ) : (
+        ''
+      );
+    };
+    // 当传入modelValue时用以定位button的位置
+    if (props.modelValue > props.max) {
+      percentDispaly.value = '100%';
+    } else if (props.modelValue < props.min) {
+      percentDispaly.value = '0%';
+    } else {
+      percentDispaly.value = ((props.modelValue - props.min) * 100) / (props.max - props.min) + '%';
+    }
+    // 一挂载就进行当前位置的计算，以后的移动基于当前的位置移动
+    onMounted(() => {
+      const sliderWidth = sliderRunway.value.clientWidth;
+      currentPosition.value = (sliderWidth * (inputValue.value - props.min)) / (props.max - props.min);
+    });
+    function handleButtonMousedown(event: MouseEvent) {
+      popoverShow.value = true;
+      // props.disabled状态是不能点击拖拽的
+      if (props.disabled) {
         return;
       }
-      // 向左偏移百分比的值
-      percentDispaly.value = Math.round((value * 100) / sliderWidth) + '%';
-      // 更新输入框的值
-      inputValue.value = Math.round((value * (props.max - props.min)) / sliderWidth) + props.min;
-      // 设置当前所在的位置
-      currentPosition.value = newPosition;
-      ctx.emit('update:modelValue', inputValue.value);
+      // 阻止默认事件
+      event.preventDefault();
+      dragStart(event);
+      // 当鼠标开始移动时，进行坐标计算
+      window.addEventListener('mousemove', onDragging);
+      // 当鼠标抬起时，停止计算
+      window.addEventListener('mouseup', onDragEnd);
     }
     // 当在滑动条触发鼠标事件时处理,
     function handleRunwayMousedown(event) {
@@ -123,23 +142,6 @@ export default defineComponent({
         handleButtonMousedown(event);
       } else {
         return;
-      }
-    }
-    // 输入框内的值
-    function handleOnInput(event) {
-      inputValue.value = parseInt(event.target.value);
-      if (!inputValue.value) {
-        inputValue.value = props.min;
-        percentDispaly.value = '0%';
-      } else {
-        if (inputValue.value < props.min || inputValue.value > props.max) {
-          return;
-        }
-        const re = /^(?:[1-9]?\d|100)$/;
-        if (re.test(`${inputValue.value}`)) {
-          percentDispaly.value = ((inputValue.value - props.min) * 100) / (props.max - props.min) + '%';
-          ctx.emit('update:modelValue', inputValue.value);
-        }
       }
     }
     // 添加disabled类
@@ -158,10 +160,10 @@ export default defineComponent({
       <div class='devui-slider'>
         {/* 整个的长度 */}
         <div
-          ref={sliderRunway} class={'devui-slider__runway' + disableClass.value}
+          ref={sliderRunway}
+          class={'devui-slider__runway' + disableClass.value}
           onMousedown={handleRunwayMousedown}
-          onMouseout={() => (popoverShow.value = false)}
-        >
+          onMouseout={() => (popoverShow.value = false)}>
           {/* 滑动后左边的进度条 */}
           <div class={'devui-slider__bar' + disableClass.value} style={{ width: percentDispaly.value }}></div>
           <div
@@ -169,8 +171,7 @@ export default defineComponent({
             style={{ left: percentDispaly.value }}
             onMousedown={handleButtonMousedown}
             onMouseenter={() => (popoverShow.value = true)}
-            onMouseout={() => (popoverShow.value = false)}
-          ></div>
+            onMouseout={() => (popoverShow.value = false)}></div>
           {props.tipsRenderer === 'null' ? '' : popover()}
         </div>
         <span class='devui-min_count'>{props.min}</span>
