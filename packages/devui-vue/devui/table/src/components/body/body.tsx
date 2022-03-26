ff7362ad31e45de21fb1df16f24a409f2e82f1b4
wangyupei <2311595895@qq.com>
2022-03-26 17:52:23 +0800

    feat(Table): 完成Table组件合并单元格功能

--- packages/devui-vue/devui/table/src/components/body/body.tsx
+++ packages/devui-vue/devui/table/src/components/body/body.tsx
@@ -3,6 +3,7 @@ import { TABLE_TOKEN } from '../../table-types';
 import TD from '../body-td/body-td';
 import { Checkbox } from '../../../../checkbox';
 import { useNamespace } from '../../../../shared/hooks/use-namespace';
+import { useBody } from './use-body';
 import './body.scss';
 
 export default defineComponent({
@@ -12,8 +13,9 @@ export default defineComponent({
     const { _data: data, _columns: columns, _checkList: checkList, isFixedLeft } = table.store.states;
     const ns = useNamespace('table');
     const hoverEnabled = computed(() => table.props.rowHoveredHighlight);
-
+    const { tableSpans, removeCells } = useBody();
     const tdAttrs = computed(() => (isFixedLeft.value ? { class: `${ns.m('sticky-cell')} left`, style: 'left:0;' } : null));
+
     const renderCheckbox = (index: number) =>
       table.props.checkable ? (
         <td {...tdAttrs.value}>
@@ -27,9 +29,15 @@ export default defineComponent({
           return (
             <tr key={rowIndex} class={{ 'hover-enabled': hoverEnabled.value }}>
               {renderCheckbox(rowIndex)}
-              {columns.value.map((column, index) => (
-                <TD column={column} index={index} row={row} />
-              ))}
+              {columns.value.map((column, columnIndex) => {
+                const cellId = `${rowIndex}-${columnIndex}`;
+                const [rowspan, colspan] = tableSpans.value[cellId] ?? [1, 1];
+
+                if (removeCells.value.includes(cellId)) {
+                  return null;
+                }
+                return <TD column={column} index={columnIndex} row={row} rowspan={rowspan} colspan={colspan} />;
+              })}
             </tr>
           );
         })}
