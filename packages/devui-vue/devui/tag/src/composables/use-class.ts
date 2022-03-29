063828bf1bfbccf8ab628ee8b3651f490c80b278
c0dedance <38075730+c0dedance@users.noreply.github.com>
2022-03-29 14:06:26 +0800

    fix: 完成组件问题清零 (#358)

--- packages/devui-vue/devui/tag/src/composables/use-class.ts
+++ packages/devui-vue/devui/tag/src/composables/use-class.ts
@@ -1,7 +1,8 @@
 import { computed } from 'vue';
-import { tagProps, TagProps } from '../tag-types';
+import type { ComputedRef } from 'vue';
+import { TagProps } from '../tag-types';
 
-export default function (props: TagProps) {
+export default function (props: TagProps): ComputedRef<string> {
   return computed(() => {
     const { type, color, deletable } = props;
     return `devui-tag-item devui-tag-${type || (color ? 'colorful' : '') || 'default'}${deletable ? ' devui-tag-deletable' : ''}`;
