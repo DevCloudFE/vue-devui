572383e63ca29a50b26c64583dd1a0ec5bcb9e35
Kagol <kagol@sina.com>
2022-03-29 16:12:59 +0800

    test(tree): 完善tree组件基本功能的单元测试

--- packages/devui-vue/devui/tree/__tests__/basic-tree/basic-tree.spec.tsx
+++ packages/devui-vue/devui/tree/__tests__/basic-tree/basic-tree.spec.tsx
@@ -5,8 +5,7 @@ import { basicData } from './basic-data';
 
 describe('Basic tree should include rendering of nested nodes and responses to hover, click and expand/collapse events.', () => {
   let wrapper: VueWrapper<ComponentPublicInstance>;
-  let firstNode: Omit<DOMWrapper<Element>, 'exists'>;
-  let lastNode: Omit<DOMWrapper<Element>, 'exists'>;
+  let childNodes: DOMWrapper<Element>[];
 
   beforeAll(() => {
     wrapper = mount({
@@ -17,8 +16,7 @@ describe('Basic tree should include rendering of nested nodes and responses to h
       },
     });
 
-    firstNode = wrapper.get('.devui-tree-node:first-child');
-    lastNode = wrapper.get('.devui-tree-node:last-child');
+    childNodes = wrapper.findAll('.devui-tree-node');
   });
 
   afterAll(() => {
@@ -26,46 +24,60 @@ describe('Basic tree should include rendering of nested nodes and responses to h
   });
 
   it('Should render tree container correctly.', () => {
-    expect(wrapper.classes()).toContain('devui-tree');
+    expect(wrapper.find('.devui-tree').exists()).toBe(true);
   });
 
   it('Should render correct number of child nodes.', () => {
-    expect(wrapper.element.childElementCount).toBe(5);
+    expect(childNodes).toHaveLength(5);
   });
 
   it('Should render correct node content.', () => {
-    expect(firstNode.text()).toBe('Parent node 1');
-    expect(lastNode.text()).toBe('Leaf node 2');
+    expect(childNodes[0].text()).toBe('Parent node 1');
+    expect(childNodes[childNodes.length - 1].text()).toBe('Leaf node 2');
   });
 
   it('Should render the style of child node correctly.', () => {
-    const childNodeParent = wrapper.get('.devui-tree-node:nth-child(2)');
-    const childNodeLeaf = wrapper.get('.devui-tree-node:nth-child(3)');
-
-    expect(childNodeParent.attributes('style').indexOf('padding-left: 24px;') > -1).toBe(true);
-    expect(childNodeLeaf.attributes('style').indexOf('padding-left: 24px;') > -1).toBe(true);
+    expect(childNodes[0].attributes('style')).toContain('padding-left: 0px');
+    expect(childNodes[1].attributes('style')).toContain('padding-left: 24px');
+    expect(childNodes[2].attributes('style')).toContain('padding-left: 24px');
   });
 
-  it.todo('Should render the style of node connection line correctly.');
+  it('The node should be highlighted when clicked.', async () => {
+    // 可点击且非高亮的的节点，点击之后应该高亮
+    expect(childNodes[1].find('.devui-tree-node__content').classes()).not.toContain('active');
+    await childNodes[1].find('.devui-tree-node__content').trigger('click');
+    expect(childNodes[1].find('.devui-tree-node__content').classes()).toContain('active');
 
-  it.todo('The node should be highlighted when clicked.');
+    // 点击非高亮节点，该节点高亮，已高亮节点应该取消高亮
+    expect(childNodes[2].find('.devui-tree-node__content').classes()).not.toContain('active');
+    await childNodes[2].find('.devui-tree-node__content').trigger('click');
+    expect(childNodes[2].find('.devui-tree-node__content').classes()).toContain('active');
+    expect(childNodes[1].find('.devui-tree-node__content').classes()).not.toContain('active');
+  });
 
-  it.todo('The node should be disabled and unclickable when disabled is set to true.');
+  it('The node should be disabled and unclickable when disabled is set to true.', async () => {
+    // 设置了 disabled: true 的节点为禁用态，不可点击
+    expect(childNodes[0].find('.devui-tree-node__content').classes()).not.toContain('active');
+    await childNodes[0].trigger('click');
+    expect(childNodes[0].find('.devui-tree-node__content').classes()).not.toContain('active');
+  });
 
   it('The node should expand and collapse correctly when the expand-collapse button is clicked.', async () => {
     // 初始状态，节点是展开的
-    expect(firstNode.classes()).toContain('devui-tree-node__open');
+    expect(childNodes[0].classes()).toContain('devui-tree-node__open');
 
     // 点击之后，节点收起
-    await wrapper.get('.devui-tree-node__folder:first-child').trigger('click');
+    await childNodes[0].get('.devui-tree-node__folder').trigger('click');
     await nextTick();
-    expect(firstNode.classes()).not.toContain('devui-tree-node__open');
+    expect(childNodes[0].classes()).not.toContain('devui-tree-node__open');
 
     // 再次点击，节点展开
-    await wrapper.get('.devui-tree-node__folder:first-child').trigger('click');
+    await childNodes[0].get('.devui-tree-node__folder').trigger('click');
     await nextTick();
-    expect(firstNode.classes()).toContain('devui-tree-node__open');
+    expect(childNodes[0].classes()).toContain('devui-tree-node__open');
   });
+
+  it.todo('Should render the style of node connection line correctly.');
 
   it.todo('The node should be disabled and unclickable when disableToggle is set to true.');
 });
