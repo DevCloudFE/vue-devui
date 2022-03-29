f4f1da8c909f1bc0dace408ad71b8043a87c947d
Kagol <kagol@sina.com>
2022-03-29 19:38:09 +0800

    test(tree): 完善tree组件基本功能的单元测试 (#359)
    * test(tree): 完善tree组件基本功能的单元测试
    
    * test(tree): 增加tree组件节点合并的单元测试
    
    * test(tree): 可勾选树功能的单元测试基本设计
    
    * test(tree): 完善可勾选树checkbox渲染和基本勾选交互的单元测试

--- packages/devui-vue/devui/tree/__tests__/tree.spec.ts
+++ packages/devui-vue/devui/tree/__tests__/tree.spec.ts
@@ -2,15 +2,11 @@ describe('tree', () => {
   // 测试节点懒加载功能是否正常
   it.todo('should loading child nodes dynamicly when click open button');
 
-  // 当节点下只有一个子节点时，应该合并这些节点
-  it.todo('should merge nodes when there is only one child node');
-
   /**
-   * 测试嵌套节点渲染和基本交互
+   * basic-tree 测试嵌套节点渲染和基本交互
+   * node-merge 当节点下只有一个子节点时，应该合并这些节点
    * 测试节点懒加载功能是否正常
-   * 当节点下只有一个子节点时，应该合并这些节点
-   * 节点的勾选功能正常
-   * 勾选复选框的禁用状态正常
+   * 节点的勾选功能正常，勾选复选框的禁用状态正常
    * 父子check控制功能正常
    * 自定义图标功能正常
    * 节点的增删改（操作按钮）功能正常
