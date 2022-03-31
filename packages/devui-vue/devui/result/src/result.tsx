90fc0f795dfb1840ff85de30a44eca0bad676909
Kagol <kagol@sina.com>
2022-03-31 10:17:57 +0800

    chore(result): 修复result组件的eslint问题

--- packages/devui-vue/devui/result/src/result.tsx
+++ packages/devui-vue/devui/result/src/result.tsx
@@ -7,21 +7,26 @@ export default defineComponent({
   name: 'DResult',
   props: resultProps,
   setup(props: ResultProps, ctx) {
-    enum IconEnum {success = 'right-o', danger='error-o', warning='warning-o', info='info-o'}
+    enum IconEnum {
+      success = 'right-o',
+      danger = 'error-o',
+      warning = 'warning-o',
+      info = 'info-o',
+    }
 
     return () => {
       return (
-        <div class='devui-result'>
-          {
-            ctx.slots.icon ? <div>{ctx.slots?.icon()}</div> :  <Icon name={IconEnum[props.icon]||''} class={`devui-result__icon-${props.icon}`} size="64px"/>
-          }
-          <div class='devui-result__title'>
-            {ctx.slots.title ? ctx.slots?.title() : props.title}
-          </div>
-          <div class='devui-result__desc'>{ctx.slots.desc ? ctx.slots?.desc() : props.desc}</div>
-          <div class='devui-result__extra'>{ctx.slots.extra ? ctx.slots?.extra() : ''}</div>
+        <div class="devui-result">
+          {ctx.slots.icon ? (
+            <div>{ctx.slots?.icon()}</div>
+          ) : (
+            <Icon name={IconEnum[props.icon] || ''} class={`devui-result__icon-${props.icon}`} size="64px" />
+          )}
+          <div class="devui-result__title">{ctx.slots.title ? ctx.slots?.title() : props.title}</div>
+          <div class="devui-result__desc">{ctx.slots.desc ? ctx.slots?.desc() : props.desc}</div>
+          <div class="devui-result__extra">{ctx.slots.extra ? ctx.slots?.extra() : ''}</div>
         </div>
       );
     };
-  }
+  },
 });
