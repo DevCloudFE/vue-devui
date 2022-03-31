a7abd8a7fc9d9449296d9d219e984169d336ab34
edentan <edentan@vesync.com.cn>
2022-03-31 16:12:22 +0800

    fix(layout): 修复eslint

--- packages/devui-vue/devui/layout/src/layout.tsx
+++ packages/devui-vue/devui/layout/src/layout.tsx
@@ -1,13 +1,17 @@
-import { defineComponent } from 'vue';
+import { defineComponent, VNode } from 'vue';
 import './layout.scss';
+
+interface SlotComponent extends VNode {
+  name?: string;
+}
 
 export default defineComponent({
   name: 'DLayout',
   emits: [],
   setup(props, { slots }) {
     return () => {
-      const slotDefault = slots.default?.();
-      const isAside = slotDefault.some(item => (item.type as any).name === 'DAside');
+      const slotDefault: SlotComponent[]  = slots.default?.();
+      const isAside = slotDefault.some(item => item.name === 'DAside');
       const classNames = `${isAside ? 'devui-layout-aside ': ''}devui-layout`;
       return <div class={classNames}>{ slotDefault }</div>;
     };
