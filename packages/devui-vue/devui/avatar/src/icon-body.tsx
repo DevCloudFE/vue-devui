00bacf56c09b17979e7a6bb876c95f6b3fc173e0
gxuud <igaoxian@foxmail.com>
2022-04-02 16:01:34 +0800

    style: svg分层改用反斜杠

--- packages/devui-vue/devui/avatar/src/icon-body.tsx
+++ packages/devui-vue/devui/avatar/src/icon-body.tsx
@@ -1,5 +1,4 @@
 import type { IconPropsType } from "./avatar-types";
-import { BODY_ICON_PATH } from "./config";
 
 export const IconBody = (props: IconPropsType): JSX.Element => {
   const { width, height } = props;
@@ -22,7 +21,15 @@ export const IconBody = (props: IconPropsType): JSX.Element => {
         fill-rule="evenodd"
       >
         <circle id="Oval" fill="#CACFD8" cx="15" cy="15" r="15"></circle>
-        <path d={BODY_ICON_PATH} id="形状结合" fill="#FFFFFF" ></path>
+        <path
+          d="M14.9997866,16 C12.5145053,16 10.4997866,13.9852814 10.4997866,\
+            11.5 C10.4997866,9.01471863 12.5145053,7 14.9997866,7 C17.485068,\
+            7 19.4997866,9.01471863 19.4997866,11.5 C19.4997866,13.9852814 17.485068,\
+            16 14.9997866,16 Z M23,23 L7,22.998553 C7,19.0122153 10.8892296,\
+            16.5 14.9997866,16.5 C19.1103437,16.5 23,20 23,23 Z"
+          id="形状结合"
+          fill="#FFFFFF"
+        ></path>
       </g>
     </svg>
   );
