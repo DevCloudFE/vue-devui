19fdb6950c0d3ab95d5511c7416e0927ff73886a
gxuud <igaoxian@foxmail.com>
2022-04-02 14:03:55 +0800

    style(avatar): 属性名称修改

--- packages/devui-vue/devui/avatar/src/icon-nobody.tsx
+++ packages/devui-vue/devui/avatar/src/icon-nobody.tsx
@@ -1,5 +1,5 @@
 import type { IconPropsType } from "./avatar-types";
-import { NOBODAY_ICON_SHAPE_PATH, NOBODAY_ICON_MASK_PATH } from "./config";
+import { NOBODY_ICON_SHAPE_PATH, NOBODY_ICON_MASK_PATH } from "./config";
 
 export const IconNobody = (props: IconPropsType): JSX.Element => {
   const { width, height } = props;
@@ -15,8 +15,8 @@ export const IconNobody = (props: IconPropsType): JSX.Element => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <path d={NOBODAY_ICON_SHAPE_PATH} id="形状结合" fill="#959EB2" ></path>
-        <path d={NOBODAY_ICON_MASK_PATH} id="蒙版" fill="#CACFD8" fill-rule="nonzero" ></path>
+        <path d={NOBODY_ICON_SHAPE_PATH} id="形状结合" fill="#959EB2" ></path>
+        <path d={NOBODY_ICON_MASK_PATH} id="蒙版" fill="#CACFD8" fill-rule="nonzero" ></path>
       </g>
     </svg>
   );
