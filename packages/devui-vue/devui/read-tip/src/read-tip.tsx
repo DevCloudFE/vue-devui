d2a211d5cd6ea10ac72c8fd99b792f852b512630
linxiang <40119767+linxiang07@users.noreply.github.com>
2022-04-06 11:18:10 +0800

    chore(read-tip): 修复eslint文件 (#396)

--- packages/devui-vue/devui/read-tip/src/read-tip.tsx
+++ packages/devui-vue/devui/read-tip/src/read-tip.tsx
@@ -1,7 +1,21 @@
 import { defineComponent, ref, onMounted, reactive, onUnmounted } from 'vue';
-import { readTipProps, ReadTipProps, ReadTipOptions } from './read-tip-types';
+import { readTipProps } from './read-tip-types';
+import type { ReadTipProps, ReadTipOptions, ReadTipRules, ReadTipRule } from './read-tip-types';
 import './read-tip.scss';
 import TipsTemplate from './read-tip-template';
+
+// 把传入的props.rules统一转为数组对象格式
+const rules = (ruleList: ReadTipRules) => {
+  if (ruleList === null) { return []; }
+  if (typeof ruleList === 'object' && !Array.isArray(ruleList)) {
+    ruleList = [ruleList];
+  }
+  ruleList = [...ruleList];
+  Array.isArray(ruleList) && ruleList.map(rule => {
+    rule.status = false;
+  });
+  return ruleList;
+};
 
 export default defineComponent({
   name: 'DReadTip',
@@ -21,8 +35,9 @@ export default defineComponent({
     };
     // 合并基础配置
     const options = { ...defaultOptions, ...props.readTipOptions };
-    const defaultSlot = ref(null);
-    const onMouseenter = (rule) => () => {
+    const defaultSlot = ref<Document | null>(null);
+    const refRules = reactive(rules(options.rules));
+    const onMouseenter = (rule: ReadTipRule) => () => {
       setTimeout(() => {
         if (rule.id) {
           const a = refRules.find(u => u.id === rule.id);
@@ -31,7 +46,7 @@ export default defineComponent({
         rule.status = true;
       }, rule.mouseenterTime || options.mouseenterTime);
     };
-    const onMouseleave = (rule) => () => {
+    const onMouseleave = (rule: ReadTipRule) => () => {
       setTimeout(() => {
         if (rule.id) {
           const a = refRules.find(u => u.id === rule.id);
@@ -42,17 +57,17 @@ export default defineComponent({
       }, rule.mouseleaveTime || options.mouseleaveTime);
     };
 
-    const init = (rules, trigger = 'hover') => {
-      rules.map(rule => {
+    const init = (ruleList, trigger = 'hover') => {
+      ruleList.map(rule => {
         rule.status = false;
         trigger = rule.trigger || trigger;
         rule.overlayClassName = rule.overlayClassName || options.overlayClassName;
         rule.position = rule.position || options.position;
         rule.contentTemplate = !!(ctx.slots.contentTemplate);
-        if (!('appendToBody' in rule)) {rule.appendToBody = options.appendToBody;}
-        const doms = defaultSlot.value.querySelectorAll(rule.selector);
+        if (!('appendToBody' in rule)) { rule.appendToBody = options.appendToBody; }
+        const doms = defaultSlot.value?.querySelectorAll(rule.selector);
         [...doms].map((dom, index) => {
-          if (rule.appendToBody === false) {dom.style.position = 'relative';}
+          if (rule.appendToBody === false) { dom.style.position = 'relative'; }
           let newRule = reactive({
             id: null
           });
@@ -61,34 +76,20 @@ export default defineComponent({
             newRule = { ...rule };
             dom.id = id;
             newRule.id = id;
-            rules.push(newRule);
+            ruleList.push(newRule);
           }
-
           if (trigger === 'hover') {
             dom.addEventListener('mouseenter', onMouseenter(newRule.id ? newRule : rule,));
             dom.addEventListener('mouseleave', onMouseleave(newRule.id ? newRule : rule));
           }
         });
-
       });
-      return rules;
+      return ruleList;
     };
-    function show(dom, rule) {
+    function show(_: unknown, rule) {
       rule.status = true;
     }
-    // 把传入的props.rules统一转为数组对象格式
-    const rules = (rules) => {
-      if (rules === null) {return;}
-      if (typeof rules === 'object' && !Array.isArray(rules)) {
-        rules = [rules];
-      }
-      rules = [...rules];
-      Array.isArray(rules) && rules.map(rule => {
-        rule.status = false;
-      });
-      return rules;
-    };
-    const refRules = reactive(rules(options.rules));
+
     const clickFn = () => {
       refRules.forEach(element => {
         element.status = false;
@@ -109,56 +110,42 @@ export default defineComponent({
     // 添加点击事件 当前元素是click事件目标则弹框展示
     const onClick = (e: Event) => {
       for (const rule of refRules) {
-        const doms = defaultSlot.value.querySelectorAll(rule.selector);
+        const doms = defaultSlot.value?.querySelectorAll(rule.selector);
         for (const dom of doms) {
           if (doms.length > 1) {
             if (dom === e.target && rule.id) {
               show(dom, rule);
               return;
-
             } else if (dom === e.target && !rule.id && !dom.id) {
               show(dom, rule);
               return;
             }
-
-          } else
-
-          if (dom === e.target) {
+          } else if (dom === e.target) {
             show(dom, rule);
             return;
           } else {
             rule.status = false;
           }
         }
-
       }
-
     };
     return () => {
       return (
         <div class="devui-read-tip" >
-          <div ref={defaultSlot}
-            onClick={onClick}
-          >
-            {
-              ctx.slots?.default()
-            }
+          <div ref={defaultSlot} onClick={onClick}>
+            {ctx.slots?.default?.()}
           </div>
-
           {(refRules).map(rule => (
-            <div
-              data-test="todo"
-            >
-              {rule.status && (<TipsTemplate defaultTemplateProps={{ ...rule }} >
-                {
-                  rule.contentTemplate && ctx.slots?.contentTemplate()
-                }
-              </TipsTemplate>)
-
-              }
+            <div data-test="todo">
+              {rule.status && (
+                <TipsTemplate defaultTemplateProps={{ ...rule }} >
+                  {
+                    rule.contentTemplate && ctx.slots?.contentTemplate?.()
+                  }
+                </TipsTemplate>
+              )}
             </div>
-          )
-          )}
+          ))}
         </div>
       );
     };
