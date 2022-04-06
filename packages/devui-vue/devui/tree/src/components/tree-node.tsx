0fc22b2d501f3fd45f60370435a70ffcd3e79f69
Kagol <kagol@sina.com>
2022-04-06 12:43:01 +0800

    feat(tree): 增加节点连接线 (#400)

--- packages/devui-vue/devui/tree/src/components/tree-node.tsx
+++ packages/devui-vue/devui/tree/src/components/tree-node.tsx
@@ -1,8 +1,9 @@
 import { defineComponent, inject, PropType, toRefs } from 'vue';
-import { USE_TREE_TOKEN } from '../const';
+import { NODE_HEIGHT, USE_TREE_TOKEN } from '../const';
 import { IInnerTreeNode } from '../core/use-tree-types';
 import DTreeNodeToggle from './tree-node-toggle';
 import { Checkbox } from '../../../checkbox';
+import useTreeNode from './use-tree-node';
 
 export default defineComponent({
   name: 'DTreeNode',
@@ -15,22 +16,32 @@ export default defineComponent({
     const { data } = toRefs(props);
     const { selectNode, toggleCheckNode } = inject(USE_TREE_TOKEN);
 
+    const {
+      nodeClass,
+      nodeStyle,
+      nodeContentClass,
+      nodeTitleClass,
+      nodeVLineClass,
+      nodeVLineStyle,
+      nodeHLineClass,
+    } = useTreeNode(data);
+
     return () => {
       return (
-        <div
-          class={['devui-tree-node', data.value?.expanded && 'devui-tree-node__open']}
-          style={{ paddingLeft: `${24 * (data.value?.level - 1)}px` }}>
-          <div class={['devui-tree-node__content', data.value?.selected && 'active']} onClick={() => {
-            selectNode(data.value);
-          }}>
+        <div class={nodeClass.value} style={nodeStyle.value}>
+          <span class={nodeVLineClass.value} style={nodeVLineStyle.value}></span>
+          <div class={nodeContentClass.value} onClick={() => { selectNode(data.value); }}>
+            <span class={nodeHLineClass.value}></span>
             <DTreeNodeToggle data={data.value} />
-            <div class="devui-tree-node__content--value-wrapper">
-              { <Checkbox disabled={data.value?.disableCheck} key={data.value?.id} modelValue={data.value?.checked} onUpdate:modelValue={() => {
-                toggleCheckNode(data.value);
-              }} onClick={(event: MouseEvent) => {
-                event.stopPropagation();
-              }} /> }
-              <span class={['devui-tree-node__title', data.value?.disableSelect && 'select-disabled']}>{data.value?.label}</span>
+            <div class="devui-tree-node__content--value-wrapper" style={{ height: `${NODE_HEIGHT}px`}}>
+              <Checkbox
+                key={data.value?.id}
+                disabled={data.value?.disableCheck}
+                modelValue={data.value?.checked}
+                onUpdate:modelValue={() => { toggleCheckNode(data.value); }}
+                onClick={(event: MouseEvent) => { event.stopPropagation(); }}
+              />
+              <span class={nodeTitleClass.value}>{data.value?.label}</span>
             </div>
           </div>
         </div>
