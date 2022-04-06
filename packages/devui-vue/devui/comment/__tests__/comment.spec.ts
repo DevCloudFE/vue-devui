969ae5bf4e592c52ac8db0d078472978350dcb6d
gxuud <igaoxian@foxmail.com>
2022-04-06 16:26:02 +0800

    test(comment): 添加单元测试

--- packages/devui-vue/devui/comment/__tests__/comment.spec.ts
+++ packages/devui-vue/devui/comment/__tests__/comment.spec.ts
@@ -1,3 +1,22 @@
+import { mount } from '@vue/test-utils';
+import { Comment } from '..';
+
 describe('comment test', () => {
-  it.todo('comment init render');
+  it('comment init render', async () => {
+    const wrapper = mount(Comment, {
+      slots: {
+        actions: 'Comment Actions',
+        author: 'Comment Author',
+        avatar: 'Comment Avatar',
+        content: 'Comment Content',
+        datetime: 'Comment Datetime',
+      },
+    });
+    expect(wrapper.find('.devui-comment').exists()).toBe(true);
+    expect(wrapper.find('.devui-comment-actions').text()).toBe('Comment Actions');
+    expect(wrapper.find('.devui-comment-author').text()).toBe('Comment Author');
+    expect(wrapper.find('.devui-comment-avatar').text()).toBe('Comment Avatar');
+    expect(wrapper.find('.devui-comment-content').text()).toBe('Comment Content');
+    expect(wrapper.find('.devui-comment-datetime').text()).toBe('Comment Datetime');
+  });
 });
