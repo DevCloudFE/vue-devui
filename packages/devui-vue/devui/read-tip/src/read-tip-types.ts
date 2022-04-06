901c3d65cbcc89363817fae7ef0b680072e088d9
linxiang <40119767+linxiang07@users.noreply.github.com>
2022-04-06 11:18:10 +0800

    chore(read-tip): 修复eslint文件 (#396)

--- packages/devui-vue/devui/read-tip/src/read-tip-types.ts
+++ packages/devui-vue/devui/read-tip/src/read-tip-types.ts
@@ -1,16 +1,30 @@
 import type { PropType, ExtractPropTypes } from 'vue';
 
-export const readTipProps = {
-  readTipOptions: {
-    type: Object as PropType<ReadTipOptions>
-  },
-  defaultTemplateProps: {
-    type: Object as PropType<DefaultTemplateProps>
-  }
-} as const;
-
 export type Position = 'top' | 'left' | 'right' | 'bottom';
 export type Trigger = 'hover' | 'click';
+
+export interface ReadTipRule {
+  id?: string;
+  key?: string;
+  selector: string | null;
+  trigger?: Trigger;
+  title?: string;
+  content?: string;
+  showAnimate?: boolean;
+  mouseenterTime?: number;
+  mouseleaveTime?: number;
+  position?: Position;
+  overlayClassName?: string;
+  appendToBody?: boolean;
+  status?: boolean;
+  // customData与template搭配使用，customData为传入模板的上下文，可以自定义模板内容
+  dataFn?: ({
+    element,
+    rule: ReadTipRule,
+  }) => { title?: string; content?: string };
+}
+
+export type ReadTipRules = ReadTipRule | ReadTipRule[];
 
 export type DefaultTemplateProps = {
   title?: string;
@@ -41,26 +55,13 @@ export interface ReadTipOptions {
 
 }
 
-export type ReadTipRules = ReadTipRule | ReadTipRule[];
-
-export interface ReadTipRule {
-  key?: string;
-  selector: string;
-  trigger?: Trigger;
-  title?: string;
-  content?: string;
-  showAnimate?: boolean;
-  mouseenterTime?: number;
-  mouseleaveTime?: number;
-  position?: Position;
-  overlayClassName?: string;
-  appendToBody?: boolean;
-  // customData与template搭配使用，customData为传入模板的上下文，可以自定义模板内容
-  dataFn?: ({
-    element,
-    rule: ReadTipRule,
-  }) => { title?: string; content?: string };
-}
-
+export const readTipProps = {
+  readTipOptions: {
+    type: Object as PropType<ReadTipOptions>
+  },
+  defaultTemplateProps: {
+    type: Object as PropType<DefaultTemplateProps>
+  }
+} as const;
 
 export type ReadTipProps = ExtractPropTypes<typeof readTipProps>;
