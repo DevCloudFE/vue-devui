e5f3d093ad3fe2e63fcc43d94112b895c381942e
gxuud <igaoxian@foxmail.com>
2022-04-08 09:20:41 +0800

    feat: 虚拟列表基本结构

--- packages/devui-vue/devui/virtual-list/src/virtual-list-types.ts
+++ packages/devui-vue/devui/virtual-list/src/virtual-list-types.ts
@@ -1,9 +1,38 @@
-import type { ExtractPropTypes } from 'vue';
+import type { PropType, ExtractPropTypes, CSSProperties } from 'vue';
 
 export const virtualListProps = {
-  /* test: {
-    type: Object as PropType<{ xxx: xxx }>
-  } */
+  data: {
+    type: Array,
+    default: () => [],
+  },
+  style: {
+    type: Object as PropType<CSSProperties>,
+  },
+  class: {
+    type: String,
+    default: '',
+  },
+  component: {
+    type: String,
+    default: 'div',
+  },
+  height: {
+    type: Number,
+  },
+} as const;
+
+export const virtualListFllterProps = {
+  data: {
+    type: Array,
+    default: () => [],
+  },
+} as const;
+
+export const virtualListScrollBarProps = {
+  data: {
+    type: Array,
+    default: () => [],
+  },
 } as const;
 
 export type VirtualListProps = ExtractPropTypes<typeof virtualListProps>;
