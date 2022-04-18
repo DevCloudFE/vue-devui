0d89ac8bbf6eb1fb46d05bba210d8cf86510d6de
行言 <2311595895@qq.com>
2022-04-18 17:34:02 +0800

    refactor(Form): 优化表单API和文档 (#512)
    * refactor(Form): 参考Rem规范重构代码样式
    
    * refactor(Form): 重构Form布局
    
    * refactor(Form): 优化表单参数和文档

--- packages/devui-vue/devui/form/src/components/form-label/use-form-label.ts
+++ packages/devui-vue/devui/form/src/components/form-label/use-form-label.ts
@@ -1,16 +1,16 @@
-import { computed, reactive, inject, toRefs } from 'vue';
+import { computed, inject, toRefs } from 'vue';
 import { FORM_TOKEN, IForm } from '../../form-types';
 import { FormLabelProps, UseFormLabel } from './form-label-types';
 import { useNamespace } from '../../../../shared/hooks/use-namespace';
 
 export function useFormLabel(props: FormLabelProps): UseFormLabel {
-  const Form = reactive(inject(FORM_TOKEN) as IForm);
-  const labelData = reactive(Form.labelData);
+  const { labelData } = inject(FORM_TOKEN) as IForm;
   const ns = useNamespace('form');
   const { required } = toRefs(props);
 
   const labelClasses = computed(() => ({
     [`${ns.e('label')}`]: true,
+    [`${ns.em('label', 'vertical')}`]: labelData.layout === 'vertical',
     [`${ns.em('label', labelData.labelSize)}`]: labelData.layout === 'horizontal',
     [`${ns.em('label', labelData.labelAlign)}`]: labelData.layout === 'horizontal',
   }));
