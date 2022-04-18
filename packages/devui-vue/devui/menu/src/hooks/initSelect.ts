6fcb23018290cec01cf7b9cadbbd839e69963812
GaoNengwWw <gaonenggg@outlook.com>
2022-04-18 16:59:05 +0800

    feat(menu): 菜单组件

---
+++ packages/devui-vue/devui/menu/src/hooks/initSelect.ts
@@ -0,0 +1,20 @@
+import { Ref, ref } from "vue";
+
+export function useInitSelect(defaultSelectKeys: string[], keys: string, isMutiple: boolean, disabled: Ref<boolean>):
+boolean{
+  const isSelect = ref(false);
+  if(!isMutiple){
+    if (defaultSelectKeys[0] === keys && !disabled.value){
+      isSelect.value = true;
+    } else {
+      isSelect.value = false;
+    }
+  } else{
+    if (defaultSelectKeys.includes(keys)){
+      isSelect.value = true;
+    } else {
+      isSelect.value = false;
+    }
+  }
+  return isSelect.value;
+}
