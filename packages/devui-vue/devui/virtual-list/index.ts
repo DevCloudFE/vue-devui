2ccf14f2cb3ba656c5c6652e8198cfc26b975576
gxuud <igaoxian@foxmail.com>
2022-04-18 11:41:27 +0800

    style(virtual-list): 移除没有使用的调用、更新进度

--- packages/devui-vue/devui/virtual-list/index.ts
+++ packages/devui-vue/devui/virtual-list/index.ts
@@ -6,9 +6,8 @@ export { VirtualList };
 export default {
   title: 'VirtualList 虚拟列表',
   category: '通用',
-  status: '10%',
+  status: '50%',
   install(app: App): void {
-    app.use(VirtualList as any);
     app.component(VirtualList.name, VirtualList);
   }
 };
