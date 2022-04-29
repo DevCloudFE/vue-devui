2c584eddfdd9eb247abb8c305f1c20012afe7311
行言 <2311595895@qq.com>
2022-04-29 16:10:29 +0800

    feat(Form): 支持validate事件&支持配置rules变更后立即触发校验 (#591)

--- packages/devui-vue/devui/form/src/form.tsx
+++ packages/devui-vue/devui/form/src/form.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, provide, reactive, SetupContext, toRefs } from 'vue';
+import { defineComponent, provide, reactive, SetupContext, toRefs, watch } from 'vue';
 import { formProps, FormProps, FORM_TOKEN } from './form-types';
 import { useNamespace } from '../../shared/hooks/use-namespace';
 import useFieldCollection from './composables/use-field-collection';
@@ -7,6 +7,7 @@ import useFormValidation from './composables/use-form-validation';
 export default defineComponent({
   name: 'DForm',
   props: formProps,
+  emits: ['validate'],
   setup(props: FormProps, ctx: SetupContext) {
     const ns = useNamespace('form');
     const { itemContexts, addItemContext, removeItemContext } = useFieldCollection();
@@ -16,10 +17,21 @@ export default defineComponent({
       e.preventDefault();
     };
 
+    watch(
+      () => props.rules,
+      () => {
+        if (props.validateOnRuleChange) {
+          validate();
+        }
+      },
+      { deep: true }
+    );
+
     provide(
       FORM_TOKEN,
       reactive({
         ...toRefs(props),
+        emit: ctx.emit,
         addItemContext,
         removeItemContext,
       })
