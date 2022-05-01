3952eaa011119fb242725c120d2a80ebf7f1155f
Echo <nts318@yeah.net>
2022-05-02 03:24:31 +0800

    fix: 解决transfer-pabel提交失败

--- packages/devui-vue/devui/transfer/src/components/transfer-panel.tsx
+++ packages/devui-vue/devui/transfer/src/components/transfer-panel.tsx
@@ -1,12 +1,59 @@
 import { defineComponent } from 'vue';
+import type { SetupContext } from 'vue';
+import { transferPanelProps, TTransferPanelProps , transferPanelState } from '../composables/use-transfer-panel';
+import transferHeader from './transfer-header';
+import transferBody from './transfer-body';
+import { TKey } from '../transfer-types';
 
 export default defineComponent({
   name: 'DTransferPanel',
   components: {
+    transferHeader,
+    transferBody
   },
-  setup() {
+  props: transferPanelProps,
+  emits: ['updteAllChecked', 'changeButtonState', 'changeChecked'],
+  setup(props: TTransferPanelProps, ctx: SetupContext) {
+    const {
+      allChecked,
+      allHalfchecked,
+      allNum,
+      fliterData,
+      query,
+      changeAllCheckedHandle,
+      updateModelValueHandle,
+      updateCheckedDataHandle
+    } = transferPanelState(props, ctx);
     return () => {
-      return <div>12</div>;
+      return <div class="devui-transfer-panel">
+        <transferHeader
+          title={props.title}
+          checkedNum={props.defaultChecked.length}
+          unit={props.unit}
+          checked={allChecked.value}
+          halfchecked={allHalfchecked.value}
+          total={allNum.value}
+          onChange={(value: boolean): void => {
+            changeAllCheckedHandle(value);
+          }}
+        >
+        </transferHeader>
+        {/* <transferBody
+          height={props.height}
+          isSearch={props.isSearch}
+          isKeyupSearch={props.isKeyupSearch}
+          placeholder={props.placeholder}
+          data={fliterData.value}
+          defaultChecked={props.defaultChecked}
+          modelValue={query.value}
+          onChange={(value: string[]) => {
+            updateCheckedDataHandle(value)
+          }}
+          onUpdate:modelValue={(value: TKey) => {
+            updateModelValueHandle(value)
+          }}
+        ></transferBody> */}
+      </div>;
     };
   }
 });
