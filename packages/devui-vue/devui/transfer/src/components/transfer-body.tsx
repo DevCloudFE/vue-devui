cd9d2c00732f337213f09a8e34b8eebf3b79fefe
Echo <nts318@yeah.net>
2022-05-02 03:35:43 +0800

    fix: 解决transfer-pabel提交失败

--- packages/devui-vue/devui/transfer/src/components/transfer-body.tsx
+++ packages/devui-vue/devui/transfer/src/components/transfer-body.tsx
@@ -1,59 +1,10 @@
 import { defineComponent } from 'vue';
-import type { SetupContext } from 'vue';
-import DCheckbox from '../../../checkbox/src/checkbox';
-import DCheckboxGroup from '../../../checkbox/src/checkbox-group';
-import DSearch from '../../../search/src/search';
-import DIcon from '../../../icon/src/icon';
-import { TKey } from '../transfer-types';
-import { transferBodyProps, TTransferBodyProps, transferBodyState } from '../composables/use-transfer-body';
 
 export default defineComponent({
   name: 'DTransferBody',
-  components: {
-    DSearch,
-    DCheckbox,
-    DIcon
-  },
-  props: transferBodyProps,
-  emits: ['change', 'update:modelValue'],
-  setup(props: TTransferBodyProps, ctx: SetupContext) {
-    const {
-      bodyHeight,
-      query,
-      updateFilterQueryHandle
-    } = transferBodyState(props, ctx);
-    const renderSearch = () => {
-      return <div class="devui-transfer-panel-body-search">
-        <DSearch
-          modelValue={query.value}
-          placeholder={props.placeholder}
-          is-keyup-search={props.isKeyupSearch}
-          onSearch={(value: TKey) => {
-            updateFilterQueryHandle(value);
-          }}
-        />
-      </div>;
-    };
-    const renderList = () => {
-      if (!props.data.length) {
-        return <div class="devui-transfer-panel-body-list-empty">暂无数据</div>;
-      }
-      return <DCheckboxGroup
-        modelValue={props.defaultChecked}
-        options={props.data}
-        onChange={(value) => {
-          ctx.emit('change', value);
-        }}
-      />;
-    };
+  setup() {
     return () => {
       return <div class="devui-transfer-panel-body">
-        {props.isSearch && renderSearch()}
-        <div class="devui-transfer-panel-body-list" style={{ height: bodyHeight.value }}>
-          {
-            renderList()
-          }
-        </div>
       </div>;
     };
   }
