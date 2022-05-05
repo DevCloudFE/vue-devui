4bb92ce7ad1f6a6f6256a0679a22035da206bd17
zcj <18137693952@163.com>
2022-05-05 20:22:29 +0800

    fix(Alert): icon 组件 ts 类型错误。

--- packages/devui-vue/devui/alert/src/alert.tsx
+++ packages/devui-vue/devui/alert/src/alert.tsx
@@ -42,12 +42,12 @@ export default defineComponent({
           <div ref={alertEl} v-show={!closing.value} class={[ns.b(), ns.m(props.type), props.cssClass, closing.value && ns.m('close')]}>
             {props.closeable ? (
               <div class={closeNs.b()} onClick={close}>
-                <AlertCloseIcon></AlertCloseIcon>
+                <AlertCloseIcon/>
               </div>
             ) : null}
             {props.showIcon !== false && props.type !== 'simple' ? (
               <span class={ns.e('icon-wrap')}>
-                <AlertTypeIcon type={props.type}></AlertTypeIcon>
+                <AlertTypeIcon type={props.type}/>
               </span>
             ) : null}
             {ctx.slots.default?.()}
