4c5a39f953c4f923b5e10db47a3514b8e67081d9
行言 <2311595895@qq.com>
2022-05-05 20:05:13 +0800

    refactor(Carousel): 重构样式命名 (#609)

--- packages/devui-vue/devui/carousel/src/item.tsx
+++ packages/devui-vue/devui/carousel/src/item.tsx
@@ -1,17 +1,12 @@
 import { defineComponent } from 'vue';
+import { useNamespace } from '../../shared/hooks/use-namespace';
 
 export default defineComponent({
   name: 'DCarouselItem',
-  render() {
-    const {
-      $slots
-    } = this;
-    const children = $slots.default?.();
+  setup(props, { slots }) {
+    const ns = useNamespace('carousel');
+    const children = slots.default?.();
 
-    return (
-      <div class="d-carousel-item ">
-        { children }
-      </div>
-    );
-  }
+    return () => <div class={ns.e('item')}>{children}</div>;
+  },
 });
