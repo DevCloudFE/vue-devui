2f0ca7b2ed7ddfabcc0fd18df7345f3e8558d709
wangyupei <2311595895@qq.com>
2022-05-05 18:43:53 +0800

    refactor(BackTop): 重构样式命名

--- packages/devui-vue/devui/back-top/src/back-top.tsx
+++ packages/devui-vue/devui/back-top/src/back-top.tsx
@@ -1,8 +1,9 @@
-import { defineComponent, onMounted, ref } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { backTopProps, BackTopProps } from './back-top-types';
 import { usePosition, useVisibility } from './hooks';
-import './back-top.scss';
 import IconTop from './assets/top.svg';
+import { useNamespace } from '../../shared/hooks/use-namespace';
+import './back-top.scss';
 
 export default defineComponent({
   name: 'DBackTop',
@@ -11,7 +12,7 @@ export default defineComponent({
   setup(props: BackTopProps, ctx) {
     const slots = ctx.slots;
     const backTopRef = ref(null);
-
+    const ns = useNamespace('back-top');
     const position = usePosition(props);
     const isVisible = useVisibility(props, backTopRef);
 
@@ -21,29 +22,21 @@ export default defineComponent({
         window.scrollTo({
           top: 0,
           left: 0,
-          behavior: 'smooth' // 平滑滚动
+          behavior: 'smooth', // 平滑滚动
         });
     };
 
     return () => (
       <div
-        class='devui-back-top'
+        class={ns.b()}
         ref={backTopRef}
         style={{
           ...position,
-          display: isVisible.value ? 'block' : 'none'
+          display: isVisible.value ? 'block' : 'none',
         }}
-        onClick={scrollToTop}
-      >
-        <div
-          class={[
-            'devui-back-top-base',
-            slots.default ? 'devui-backtop-custom' : 'devui-back-top-content'
-          ]}
-        >
-          {slots.default ? slots.default() : <IconTop />}
-        </div>
+        onClick={scrollToTop}>
+        <div class={[ns.e('base'), slots.default ? ns.e('custom') : ns.e('content')]}>{slots.default ? slots.default() : <IconTop />}</div>
       </div>
     );
-  }
+  },
 });
