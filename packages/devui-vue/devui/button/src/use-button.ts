dabc76673688fc46dddc4cc0306a9ff3af1e0cd6
wangyupei <2311595895@qq.com>
2022-05-05 19:21:38 +0800

    refactor(Button): 重构样式命名

--- packages/devui-vue/devui/button/src/use-button.ts
+++ packages/devui-vue/devui/button/src/use-button.ts
@@ -1,8 +1,10 @@
 import { computed } from 'vue';
 import type { SetupContext } from 'vue';
 import { ButtonProps, UseButtonReturnType } from './button-types';
+import { useNamespace } from '../../shared/hooks/use-namespace';
 
 export default function useButton(props: ButtonProps, ctx: SetupContext): UseButtonReturnType {
+  const ns = useNamespace('button');
   const hasContent = computed(() => ctx.slots.default);
   const colorMap = {
     solid: 'primary',
@@ -12,20 +14,20 @@ export default function useButton(props: ButtonProps, ctx: SetupContext): UseBut
   const defaultColor = colorMap[props.variant];
 
   const classes = computed(() => ({
-    'devui-btn': true,
-    [`devui-btn-${props.variant}`]: true,
-    [`devui-btn-${props.variant}-${props.color || defaultColor}`]: true,
-    [`devui-btn-${props.size}`]: true,
-    'devui-btn-icon-wrap': props.icon,
-    'devui-btn-icon': props.icon && !hasContent.value && props.variant !== 'solid',
-    'devui-btn-is-loading': props.loading,
+    [ns.b()]: true,
+    [ns.m(props.variant)]: true,
+    [`${ns.m(props.variant)}--${props.color || defaultColor}`]: true,
+    [ns.m(props.size)]: true,
+    [ns.e('icon-wrap')]: props.icon,
+    [ns.e('icon')]: props.icon && !hasContent.value && props.variant !== 'solid',
+    [ns.m('is-loading')]: props.loading,
   }));
 
   const iconClass = computed(() => {
     if (!props.icon) {
       return '';
     }
-    const origin = 'devui-icon-fix icon';
+    const origin = `${ns.e('icon-fix')} icon`;
     if (hasContent.value) {
       return `${origin} clear-right-5`;
     } else {
