bbecaf11f6f7806cb9b13caadccc313acb9e60c4
行言 <2311595895@qq.com>
2022-05-06 16:57:18 +0800

    refactor(List): 重构样式命名 (#623)

--- packages/devui-vue/devui/list/src/list-item.tsx
+++ packages/devui-vue/devui/list/src/list-item.tsx
@@ -1,9 +1,11 @@
 import { defineComponent } from 'vue';
+import { useNamespace } from '../../shared/hooks/use-namespace';
 import './list-item.scss';
 
 export default defineComponent({
   name: 'DListItem',
   setup(props, { slots }) {
-    return () => <div class='devui-list-item'>{slots.default?.()}</div>;
+    const ns = useNamespace('list-item');
+    return () => <div class={ns.b()}>{slots.default?.()}</div>;
   },
 });
