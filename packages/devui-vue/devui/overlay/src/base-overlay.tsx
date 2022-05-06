8ac1fd3807b3c88fb34c98e3f6c5d83a503eff4b
wangyupei <2311595895@qq.com>
2022-05-06 09:41:27 +0800

    refactor(Overlay): 重构样式命名

--- packages/devui-vue/devui/overlay/src/base-overlay.tsx
+++ packages/devui-vue/devui/overlay/src/base-overlay.tsx
@@ -1,13 +1,14 @@
 import { defineComponent, renderSlot, Teleport, Transition } from 'vue';
+import { useNamespace } from '../../shared/hooks/use-namespace';
 import './base-overlay.scss';
 
 export const CommonOverlay = defineComponent({
   setup(props, ctx) {
+    const ns = useNamespace('overlay');
+
     return () => (
       <Teleport to="#d-overlay-anchor">
-        <Transition name="devui-overlay-fade">
-          {renderSlot(ctx.slots, 'default')}
-        </Transition>
+        <Transition name={ns.e('fade')}>{renderSlot(ctx.slots, 'default')}</Transition>
       </Teleport>
     );
   },
--- packages/devui-vue/devui/overlay/src/flexible-overlay/index.tsx
+++ packages/devui-vue/devui/overlay/src/flexible-overlay/index.tsx
@@ -1,6 +1,7 @@
 import { defineComponent } from 'vue';
 import { flexibleOverlayProps, FlexibleOverlayProps } from './flexible-overlay-types';
 import { useOverlay } from './use-flexible-overlay';
+import { useNamespace } from '../../../shared/hooks/use-namespace';
 import './flexible-overlay.scss';
 
 export const FlexibleOverlay = defineComponent({
@@ -9,14 +10,15 @@ export const FlexibleOverlay = defineComponent({
   props: flexibleOverlayProps,
   emits: ['update:modelValue', 'positionChange'],
   setup(props: FlexibleOverlayProps, { slots, attrs, emit, expose }) {
+    const ns = useNamespace('flexible-overlay');
     const { arrowRef, overlayRef, updatePosition } = useOverlay(props, emit);
     expose({ updatePosition });
 
     return () =>
       props.modelValue && (
-        <div ref={overlayRef} class='devui-flexible-overlay' {...attrs}>
+        <div ref={overlayRef} class={ns.b()} {...attrs}>
           {slots.default?.()}
-          {props.showArrow && <div ref={arrowRef} class='devui-flexible-overlay-arrow'></div>}
+          {props.showArrow && <div ref={arrowRef} class={ns.e('arrow')}></div>}
         </div>
       );
   },
