7d4725f95473ed17b2d0da98bd940db843d575af
wangyupei <2311595895@qq.com>
2022-05-07 15:19:59 +0800

    refactor(Layout): 重构样式命名

--- packages/devui-vue/devui/layout/src/content.tsx
+++ packages/devui-vue/devui/layout/src/content.tsx
@@ -1,10 +1,11 @@
-import './content.scss';
-
 import { defineComponent } from 'vue';
+import { useNamespace } from '../../shared/hooks/use-namespace';
+import './content.scss';
 
 export default defineComponent({
   name: 'DContent',
-  setup (props, { slots }) {
-    return () => <div class="devui-content">{slots.default?.()}</div>;
-  }
+  setup(props, { slots }) {
+    const ns = useNamespace('layout');
+    return () => <div class={ns.e('content')}>{slots.default?.()}</div>;
+  },
 });
--- packages/devui-vue/devui/layout/src/footer.tsx
+++ packages/devui-vue/devui/layout/src/footer.tsx
@@ -1,10 +1,11 @@
-import './footer.scss';
-
 import { defineComponent } from 'vue';
+import { useNamespace } from '../../shared/hooks/use-namespace';
+import './footer.scss';
 
 export default defineComponent({
   name: 'DFooter',
-  setup (props, { slots }) {
-    return () => <div class="devui-footer">{ slots.default?.() }</div>;
-  }
+  setup(props, { slots }) {
+    const ns = useNamespace('layout');
+    return () => <div class={ns.e('footer')}>{slots.default?.()}</div>;
+  },
 });
