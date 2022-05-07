3c1ed9b3595d9ec0fc01f55bbdfa3f082d70764c
行言 <2311595895@qq.com>
2022-05-07 10:13:55 +0800

    refactor(Modal): 重构样式命名 (#642)

--- packages/devui-vue/devui/modal/src/components/header.tsx
+++ packages/devui-vue/devui/modal/src/components/header.tsx
@@ -1,8 +1,11 @@
 import { defineComponent } from 'vue';
+import { useNamespace } from '../../../shared/hooks/use-namespace';
 
 export default defineComponent({
   name: 'DModalHeader',
   setup(props, { slots }) {
-    return () => <div class='devui-modal-header'>{slots.default?.()}</div>;
+    const ns = useNamespace('modal');
+
+    return () => <div class={ns.e('header')}>{slots.default?.()}</div>;
   },
 });
