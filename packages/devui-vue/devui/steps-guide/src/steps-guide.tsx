86d49e1ea90ac6b68d36dd91e85ccb3d1916080a
行言 <2311595895@qq.com>
2022-05-07 15:42:59 +0800

    refactor(StepsGuide): 重构样式命名 (#650)

--- packages/devui-vue/devui/steps-guide/src/steps-guide.tsx
+++ packages/devui-vue/devui/steps-guide/src/steps-guide.tsx
@@ -1,6 +1,7 @@
 import { computed, ref, defineComponent, Teleport, onMounted } from 'vue';
 import { stepsGuideProps, StepsGuideProps, Step } from './steps-guide-types';
 import { useStepsGuidePosition, useStepsGuideCtrl } from '../composables';
+import { useNamespace } from '../../shared/hooks/use-namespace';
 import './steps-guide.scss';
 
 export default defineComponent({
@@ -8,67 +9,75 @@ export default defineComponent({
   props: stepsGuideProps,
   emits: ['guide-close', 'update:stepIndex'],
   setup(props: StepsGuideProps, ctx) {
+    const ns = useNamespace('steps-guide');
     const stepIndexData = ref<number>((props.stepIndex ?? 0) as number);
     const stepIndex = computed<number>({
-      set: val => {
-        if(props.stepIndex != null) {
+      set: (val) => {
+        if (props.stepIndex != null) {
           ctx.emit('update:stepIndex', val);
         }
         stepIndexData.value = val;
       },
-      get: () => stepIndexData.value
+      get: () => stepIndexData.value,
     });
     const currentStep = computed<Step>(() => {
       const _step = props.steps[stepIndex.value];
-      if(_step) {_step.position = _step.position || 'top';}
+      if (_step) {
+        _step.position = _step.position || 'top';
+      }
       return _step;
     });
 
-    const {
-      stepsRef,
-      guidePosition,
-      guideClassList,
-      updateGuidePosition } = useStepsGuidePosition(props, currentStep);
-    const {
-      stepsCount,
-      closeGuide,
-      setCurrentIndex } = useStepsGuideCtrl(props, ctx, updateGuidePosition, stepIndex);
+    const { stepsRef, guidePosition, guideClassList, updateGuidePosition } = useStepsGuidePosition(props, currentStep);
+    const { stepsCount, closeGuide, setCurrentIndex } = useStepsGuideCtrl(props, ctx, updateGuidePosition, stepIndex);
 
     onMounted(() => {
       updateGuidePosition();
     });
     ctx.expose({
       closeGuide,
-      setCurrentIndex
+      setCurrentIndex,
     });
-    return () => stepIndex.value > -1 && stepsCount.value > 0 ?
-      (<Teleport to="body">
-        <div style={ guidePosition } class={guideClassList} ref={ stepsRef }>
-          <div class="devui-shining-dot"></div>
-          <div class="devui-shining-plus"></div>
-          <div class="devui-arrow"></div>
-          <div class="devui-guide-container">
-            <p class="devui-title">{currentStep.value.title}</p>
-            {props.showClose ? <div class="icon icon-close" onClick={closeGuide}></div> : null}
-            <div class="devui-content">{currentStep.value.content}</div>
-            <div class="devui-ctrl">
-              {
-                props.showDots ?
-                  <div class="devui-dots">
+    return () =>
+      stepIndex.value > -1 && stepsCount.value > 0 ? (
+        <Teleport to="body">
+          <div style={guidePosition} class={guideClassList} ref={stepsRef}>
+            <div class={ns.e('shining-dot')}></div>
+            <div class={ns.e('shining-plus')}></div>
+            <div class={ns.e('arrow')}></div>
+            <div class={ns.e('container')}>
+              <p class={ns.e('title')}>{currentStep.value.title}</p>
+              {props.showClose ? <div class="icon icon-close" onClick={closeGuide}></div> : null}
+              <div>{currentStep.value.content}</div>
+              <div class={ns.e('ctrl')}>
+                {props.showDots ? (
+                  <div class={ns.e('dots')}>
                     {props.steps.map((step, index) => {
-                      return <em class={ ['icon icon-dot-status', currentStep.value === step ? 'devui-active' : ''] } key={ index }></em>;
+                      return <em class={['icon icon-dot-status', currentStep.value === step ? ns.e('active') : '']} key={index}></em>;
                     })}
-                  </div> : null
-              }
-              <div class="devui-guide-btn">
-                {stepIndex.value > 0 ? <div class="devui-prev-step" onClick={() => setCurrentIndex(stepIndex.value - 1)}>{'上一步'}</div> : null}
-                {stepIndex.value === stepsCount.value - 1 ?
-                  <div onClick={closeGuide}>{'我知道啦'}</div> :
-                  <div class="devui-next-step" onClick={() => { setCurrentIndex(stepIndex.value + 1); }}>{'我知道啦,继续'}</div>}
+                  </div>
+                ) : null}
+                <div class={ns.e('btn')}>
+                  {stepIndex.value > 0 ? (
+                    <div class={ns.e('prev-step')} onClick={() => setCurrentIndex(stepIndex.value - 1)}>
+                      {'上一步'}
+                    </div>
+                  ) : null}
+                  {stepIndex.value === stepsCount.value - 1 ? (
+                    <div onClick={closeGuide}>{'我知道啦'}</div>
+                  ) : (
+                    <div
+                      onClick={() => {
+                        setCurrentIndex(stepIndex.value + 1);
+                      }}>
+                      {'我知道啦,继续'}
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </Teleport>) : null;
-  }
+        </Teleport>
+      ) : null;
+  },
 });
