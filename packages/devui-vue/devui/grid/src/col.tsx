7538417415aa89f70b12b85e53c65e0a71dc8cff
行言 <2311595895@qq.com>
2022-05-07 09:08:10 +0800

    refactor(Grid): 重构样式命名 (#637)

--- packages/devui-vue/devui/grid/src/col.tsx
+++ packages/devui-vue/devui/grid/src/col.tsx
@@ -1,14 +1,14 @@
 import { defineComponent, computed, CSSProperties, Ref, inject } from 'vue';
 import { colProps, ColProps } from './grid-types';
-import { useSize, CLASS_PREFIX, useColClassNames } from './composables/use-grid';
+import { useSize, useColClassNames } from './composables/use-grid';
+import { useNamespace } from '../../shared/hooks/use-namespace';
 import './col.scss';
-
 
 export default defineComponent({
   name: 'DCol',
   props: colProps,
-  setup (props: ColProps, { slots }) {
-
+  setup(props: ColProps, { slots }) {
+    const ns = useNamespace('col');
     const formatFlex = (flex: typeof props.flex) => {
       if (typeof flex === 'number') {
         return `${flex} ${flex} auto`;
@@ -19,22 +19,21 @@ export default defineComponent({
       return flex;
     };
 
-    const colClassNames= useColClassNames(props);
+    const colClassNames = useColClassNames(props);
 
     const sizeClassNames = useSize(props);
 
     const colStyle = computed<CSSProperties>(() => ({
       flex: formatFlex(props.flex),
-      order: props.order
+      order: props.order,
     }));
 
     const gutterStyle = inject<Ref<CSSProperties>>('gutterStyle');
 
-    return () => <div
-      class={`${CLASS_PREFIX}${colClassNames.value}${sizeClassNames.value}`}
-      style={{ ...colStyle.value, ...gutterStyle.value }}
-    >
-      {slots.default?.()}
-    </div>;
-  }
+    return () => (
+      <div class={[ns.b(), colClassNames.value, sizeClassNames.value]} style={{ ...colStyle.value, ...gutterStyle.value }}>
+        {slots.default?.()}
+      </div>
+    );
+  },
 });
