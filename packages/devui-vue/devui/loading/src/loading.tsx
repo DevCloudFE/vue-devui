bf36a8b752ad30e6588f5106d8e5b68d9f6c3221
wangyupei <2311595895@qq.com>
2022-05-07 09:48:04 +0800

    refactor(Loading): 重构样式命名

--- packages/devui-vue/devui/loading/src/loading.tsx
+++ packages/devui-vue/devui/loading/src/loading.tsx
@@ -1,6 +1,6 @@
 import { CSSProperties, defineComponent, ref } from 'vue';
 import { loadingProps, LoadingProps } from './loading-types';
-
+import { useNamespace } from '../../shared/hooks/use-namespace';
 import './loading.scss';
 
 export default defineComponent({
@@ -8,11 +8,10 @@ export default defineComponent({
   inheritAttrs: false,
   props: loadingProps,
   setup(props: LoadingProps) {
-
     const style: CSSProperties = {
       top: props.view.top,
       left: props.view.left,
-      zIndex: props.zIndex
+      zIndex: props.zIndex,
     };
     if (!props.message) {
       style.background = 'none';
@@ -31,46 +30,32 @@ export default defineComponent({
       style,
       isShow,
       open,
-      close
+      close,
     };
   },
   render() {
-    const {
-      isShow,
-      isFull,
-      backdrop,
-      style,
-      message,
-      $slots
-    } = this;
+    const { isShow, isFull, backdrop, style, message, $slots } = this;
+    const ns = useNamespace('loading');
 
     return (
-      isShow &&
-      <div class={['devui-loading-contanier', isFull ? 'devui-loading--full' : '']}>
-        {
-          $slots.default?.() ||
-          <div class="devui-loading-wrapper">
-            {
-              backdrop
-                ? <div class="devui-loading-mask"></div>
-                : null
-            }
-            <div style={style} class="devui-loading-area">
-              <div class="devui-busy-default-spinner">
-                <div class="devui-loading-bar1"></div>
-                <div class="devui-loading-bar2"></div>
-                <div class="devui-loading-bar3"></div>
-                <div class="devui-loading-bar4"></div>
+      isShow && (
+        <div class={[ns.b(), isFull ? ns.m('full') : '']}>
+          {$slots.default?.() || (
+            <div class={ns.e('wrapper')}>
+              {backdrop ? <div class={ns.e('mask')}></div> : null}
+              <div style={style} class={ns.e('area')}>
+                <div class={ns.e('busy-default-spinner')}>
+                  <div class={ns.e('bar1')}></div>
+                  <div class={ns.e('bar2')}></div>
+                  <div class={ns.e('bar3')}></div>
+                  <div class={ns.e('bar4')}></div>
+                </div>
+                {message ? <span class={ns.e('text')}>{message}</span> : null}
               </div>
-              {
-                message
-                  ? <span class="devui-loading-text">{message}</span>
-                  : null
-              }
             </div>
-          </div>
-        }
-      </div>
+          )}
+        </div>
+      )
     );
-  }
+  },
 });
