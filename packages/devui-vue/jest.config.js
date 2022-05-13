6884244660745fab20818859a5f4b5a8906f3ad9
daviForevel <657434047@qq.com git config --global user.email 657434047@qq.com git config --global user.email 657434047@qq.com>
2022-05-13 17:10:20 +0800

    feat(button): 回退jest.config文件修改

--- packages/devui-vue/jest.config.js
+++ packages/devui-vue/jest.config.js
@@ -17,8 +17,7 @@ module.exports = {
   },
   transformIgnorePatterns: ['node_modules/?!(lodash-es)'],
   // The glob patterns Jest uses to detect test files
-  // testMatch: ['**/**/*.spec.(ts|tsx)'],
-  testRegex: 'button.spec.tsx',
+  testMatch: ['**/**/*.spec.(ts|tsx)'],
 
   // An array of file extensions your modules use
   moduleFileExtensions: ['js', 'json', 'ts', 'tsx'],
