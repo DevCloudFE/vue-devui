b73080d125aceb96ea8b8941e7c6aa7642e5d21f
lj111 <luo15001635033@163.com>
2022-05-18 16:16:12 +0800

    feat(checkbox): add size border

--- packages/devui-vue/devui/checkbox/src/checkbox.tsx
+++ packages/devui-vue/devui/checkbox/src/checkbox.tsx
@@ -55,6 +55,8 @@ export default defineComponent({
     const handleClick = () => {
       canChange(!isChecked.value, props.label).then((res) => res && toggle());
     };
+    const size = computed(() => checkboxGroupConf?.size.value ?? props.size);
+    const border = computed(() => checkboxGroupConf?.border.value ?? props.border);
 
     return {
       itemWidth,
@@ -66,6 +68,8 @@ export default defineComponent({
       mergedShowAnimation,
       handleClick,
       ns,
+      size,
+      border,
     };
   },
   render() {
@@ -84,6 +88,8 @@ export default defineComponent({
       value,
       mergedColor,
       ns,
+      size,
+      border,
       $slots,
     } = this;
 
@@ -117,6 +123,10 @@ export default defineComponent({
       [ns.e('tick')]: true,
       [ns.m('no-animation')]: !mergedShowAnimation,
     };
+    const labelCls = {
+      [ns.m(size)]: border,
+      [ns.m('bordered')]: border,
+    };
     const stopPropagation = ($event: Event) => $event.stopPropagation();
 
     const inputProps = {
@@ -126,7 +136,7 @@ export default defineComponent({
     return (
       <div class={wrapperCls} style={wrapperStyle}>
         <div class={checkboxCls}>
-          <label title={labelTitle} onClick={handleClick}>
+          <label title={labelTitle} onClick={handleClick} class={labelCls} style={{ width: itemWidth ? '100%' : 'auto' }}>
             <input
               name={name || value}
               class={ns.e('input')}
