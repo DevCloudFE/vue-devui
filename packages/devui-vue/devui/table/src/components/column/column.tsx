02a47a78f0a849c87ba88292378f3e1dd4f6c554
Kagol <kagol@sina.com>
2022-05-18 18:40:23 +0800

    feat(table): add table column checkable

--- packages/devui-vue/devui/table/src/components/column/column.tsx
+++ packages/devui-vue/devui/table/src/components/column/column.tsx
@@ -10,6 +10,7 @@ import {
   onBeforeMount,
   SetupContext,
 } from 'vue';
+import { isFunction } from 'lodash';
 import { tableColumnProps, TableColumnProps, TableColumn } from './column-types';
 import { TABLE_TOKEN, Table, DefaultRow } from '../../table-types';
 import { createColumn, useRender } from './use-column';
@@ -40,6 +41,13 @@ export default defineComponent({
       const children = isSubColumn.value ? parent.vnode.el.children : owner?.hiddenColumns.value?.children;
       const columnIndex = getColumnIndex(children || [], instance.vnode.el);
       columnIndex > -1 && owner?.store.insertColumn(column, isSubColumn.value ? parent.columnConfig : null);
+
+      // 行勾选控制
+      if (isFunction(props.checkable)) {
+        owner?.store.states._data.value.forEach((row, rowIndex) => {
+          owner.store.states._checkList.value[rowIndex] = props.checkable(row, rowIndex);
+        });
+      }
     });
 
     watch(
