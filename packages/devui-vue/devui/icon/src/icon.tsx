0a3670967d59df6d657a4fc7f195b468f63c46e7
Kagol <kagol@sina.com>
2022-05-18 17:35:04 +0800

    refactor(icon): 重构icon组件并支持svg图标

--- packages/devui-vue/devui/icon/src/icon.tsx
+++ packages/devui-vue/devui/icon/src/icon.tsx
@@ -1,30 +1,56 @@
-import { defineComponent, toRefs } from 'vue';
+import { computed, defineComponent, resolveDynamicComponent, toRefs } from 'vue';
+import { isUrl } from '../../shared/utils/url';
 import { iconProps, IconProps } from './icon-types';
+import svgIcon from './svg-icon';
 
 export default defineComponent({
   name: 'DIcon',
   props: iconProps,
-  setup(props: IconProps) {
-    const { name, size, color, classPrefix } = toRefs(props);
+  setup(props: IconProps, { attrs }) {
+    const { component, name, size, color, classPrefix } = toRefs(props);
+    const IconComponent = component.value ? resolveDynamicComponent(component.value) : resolveDynamicComponent(svgIcon);
+    const iconSize = computed(() => {
+      return typeof size.value === 'number' ? `${size.value}px` : size.value;
+    });
+
+    const svgIconDom = () => {
+      return <IconComponent name={name.value} color={color.value} size={iconSize.value} {...attrs}></IconComponent>;
+    };
+
+    const imgIconDom = () => {
+      return <img
+        src={name.value}
+        alt={name.value.split('/')[name.value.split('/').length - 1]}
+        style={{
+          width: iconSize.value || ''
+        }}
+        {...attrs}
+      />;
+    };
+
+    const fontIconDom = () => {
+      const fontIconClass = /^icon-/.test(name.value) ? name.value : `${classPrefix.value}-${name.value}`;
+      return <i
+        class={[classPrefix.value, fontIconClass]}
+        style={{
+          fontSize: iconSize.value,
+          color: color.value,
+        }}
+        {...attrs}
+      ></i>;
+    };
 
     return () => {
-      return /^((https?):)?\/\//.test(name.value) ? (
-        <img
-          src={name.value}
-          alt={name.value.split('/')[name.value.split('/').length - 1]}
-          style={{
-            width: size.value,
-            verticalAlign: 'text-bottom'
-          }}
-        />
-      ) : (
-        <i
-          class={`${classPrefix.value} ${classPrefix.value}-${name.value}`}
-          style={{
-            fontSize: size.value,
-            color: color.value
-          }}
-        ></i>
+      return (
+        <>
+          {
+            component.value
+              ? svgIconDom()
+              : isUrl(name.value)
+                ? imgIconDom()
+                : fontIconDom()
+          }
+        </>
       );
     };
   }
