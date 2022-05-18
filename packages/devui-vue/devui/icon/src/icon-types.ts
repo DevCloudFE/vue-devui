e649f61637ff9d42882a46d5d10716c9a355841f
Kagol <kagol@sina.com>
2022-05-18 17:35:04 +0800

    refactor(icon): 重构icon组件并支持svg图标

--- packages/devui-vue/devui/icon/src/icon-types.ts
+++ packages/devui-vue/devui/icon/src/icon-types.ts
@@ -1,4 +1,5 @@
 import { ExtractPropTypes } from 'vue';
+import { DEFAULT_PREFIX } from './const';
 
 export const iconProps = {
   name: {
@@ -7,17 +8,39 @@ export const iconProps = {
     required: true,
   },
   size: {
-    type: String,
+    type: [Number, String],
     default: 'inherit',
   },
   color: {
     type: String,
     default: 'inherit',
   },
+  component: {
+    type: Object,
+    default: null,
+  },
   classPrefix: {
     type: String,
-    default: 'icon',
+    default: DEFAULT_PREFIX,
+  },
+};
+
+export const svgIconProps = {
+  name: {
+    type: String,
+    default: '',
+    required: true,
+  },
+  color: {
+    type: String,
+    default: 'inherit'
+  },
+  size: {
+    type: [Number, String],
+    default: 'inherit'
   },
 };
 
 export type IconProps = ExtractPropTypes<typeof iconProps>;
+
+export type SvgIconProps = ExtractPropTypes<typeof svgIconProps>;
