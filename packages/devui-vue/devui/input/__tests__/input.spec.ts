c7b256238cf852cb591018f1b501eca282b1f754
caihaiqiang <805248710@qq.com>
2022-05-19 15:42:12 +0800

    feat(input): 支持prefix、suffix、prepend、append

--- packages/devui-vue/devui/input/__tests__/input.spec.ts
+++ packages/devui-vue/devui/input/__tests__/input.spec.ts
@@ -1,6 +1,24 @@
 import { mount } from '@vue/test-utils';
 import { ref, nextTick } from 'vue';
 import DInput from '../src/input';
+import { useNamespace } from '../../shared/hooks/use-namespace';
+
+const ns = useNamespace('input');
+const dotNs = useNamespace('input', true);
+const slotNs = useNamespace('input-slot');
+const dotSlotNs = useNamespace('input-slot', true);
+
+const innerClass = ns.e('inner');
+const smClass = ns.m('sm');
+const lgClass = ns.m('lg');
+const slotClass = slotNs.b();
+const prependClass = ns.m('prepend');
+const appendClass = ns.m('append');
+const dotErrorClass = dotNs.m('error');
+const dotSlotPrefixClass = dotSlotNs.e('prefix');
+const dotSlotSuffixClass = dotSlotNs.e('suffix');
+const dotSlotPrependClass = dotSlotNs.e('prepend');
+const dotSlotAppendClass = dotSlotNs.e('append');
 
 describe('d-input', () => {
   it('d-input render work', async () => {
@@ -17,7 +35,7 @@ describe('d-input', () => {
       },
     });
     const input = wrapper.find('input');
-    expect(input.classes()).toContain('devui-input__inner');
+    expect(input.classes()).toContain(innerClass);
     expect(input.element.value).toBe('abc');
 
     await input.setValue('def');
@@ -87,30 +105,32 @@ describe('d-input', () => {
         error: false,
       },
     });
-    expect(wrapper.classes()).not.toContain('devui-input--error');
+    const noError = wrapper.find(dotErrorClass);
+    expect(noError.exists()).toBe(false);
 
     await wrapper.setProps({
       error: true,
     });
-    expect(wrapper.classes()).toContain('devui-input--error');
+    const error = wrapper.find(dotErrorClass);
+    expect(error.exists()).toBe(true);
   });
 
   it('d-input size work', async () => {
     const wrapper = mount(DInput);
-    expect(wrapper.classes()).not.toContain('devui-input--sm');
-    expect(wrapper.classes()).not.toContain('devui-input--lg');
+    expect(wrapper.classes()).not.toContain(smClass);
+    expect(wrapper.classes()).not.toContain(lgClass);
 
     await wrapper.setProps({
       size: 'sm',
     });
-    expect(wrapper.classes()).toContain('devui-input--sm');
-    expect(wrapper.classes()).not.toContain('devui-input--lg');
+    expect(wrapper.classes()).toContain(smClass);
+    expect(wrapper.classes()).not.toContain(lgClass);
 
     await wrapper.setProps({
       size: 'lg',
     });
-    expect(wrapper.classes()).not.toContain('devui-input--sm');
-    expect(wrapper.classes()).toContain('devui-input--lg');
+    expect(wrapper.classes()).not.toContain(smClass);
+    expect(wrapper.classes()).toContain(lgClass);
   });
 
   it('d-input Method:select/focus/blur work', async () => {
@@ -139,5 +159,59 @@ describe('d-input', () => {
 
   it('d-input validate-event work', async () => {
     // TODO 需要结合form组件进行测试
+  });
+
+  it('d-input prefix/suffix props work', async () => {
+    const wrapper = mount({
+      components: { DInput },
+      template: `
+        <d-input prefix="like" suffix="search" />
+      `,
+    });
+    const icon = wrapper.find('.icon');
+    const prefix = wrapper.find(dotSlotPrefixClass);
+    const suffix = wrapper.find(dotSlotSuffixClass);
+    expect(icon.exists()).toBe(true);
+    expect(prefix.exists()).toBe(true);
+    expect(suffix.exists()).toBe(true);
+  });
+
+  it('d-input prefix/suffix/prepend/append slot work', async () => {
+    const wrapper = mount({
+      components: { DInput },
+      template: `
+        <d-input>
+          <template #prepend>
+            <d-button icon="like">测试</d-button>
+          </template>
+          <template #prefix>
+            <d-icon name="search" />
+          </template>
+          <template #suffix>
+            <d-icon name="search" />
+          </template>
+          <template #append>
+            <d-icon name="like" />
+          </template>
+        </d-input>
+      `,
+    });
+
+    expect(wrapper.classes()).toContain(slotClass);
+    expect(wrapper.classes()).toContain(appendClass);
+    expect(wrapper.classes()).toContain(prependClass);
+
+    const likeIcon = wrapper.find('.icon-like');
+    const searchIcon = wrapper.find('.icon-search');
+    const prefix = wrapper.find(dotSlotPrefixClass);
+    const suffix = wrapper.find(dotSlotSuffixClass);
+    const prepend = wrapper.find(dotSlotPrependClass);
+    const append = wrapper.find(dotSlotAppendClass);
+    expect(likeIcon.exists()).toBe(true);
+    expect(searchIcon.exists()).toBe(true);
+    expect(prefix.exists()).toBe(true);
+    expect(suffix.exists()).toBe(true);
+    expect(prepend.exists()).toBe(true);
+    expect(append.exists()).toBe(true);
   });
 });
