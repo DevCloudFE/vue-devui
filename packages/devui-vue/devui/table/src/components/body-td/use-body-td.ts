da6b4879ac664494a6b5162b20ef18a5e0bcf08c
行言 <2311595895@qq.com>
2022-05-20 17:59:18 +0800

    fix(Table): 修复监听单元格变化时range报错问题 (#721)

--- packages/devui-vue/devui/table/src/components/body-td/use-body-td.ts
+++ packages/devui-vue/devui/table/src/components/body-td/use-body-td.ts
@@ -1,4 +1,4 @@
-import { onMounted, ref, Text } from 'vue';
+import { onBeforeMount, onMounted, ref } from 'vue';
 import { BodyTdProps, UseBodyTd } from './body-td-types';
 import { inBrowser } from '../../../../shared/utils';
 
@@ -6,28 +6,36 @@ export function useBodyTd(props: BodyTdProps): UseBodyTd {
   const tooltipContent = ref();
   const isShowTooltip = ref(false);
   const tdRef = ref<HTMLElement>();
+  let observer: ResizeObserver;
 
   function getTooltipContent() {
     return tdRef.value?.innerText || tdRef.value?.textContent;
   }
 
   function shouldShowTooltip() {
-    const tdEl = tdRef.value as HTMLElement;
+    if (!tdRef.value) {
+      return;
+    }
     const range = document.createRange();
-    range.setStart(tdEl, 0);
-    range.setEnd(tdEl, tdEl.childNodes.length);
+    range.setStart(tdRef.value, 0);
+    range.setEnd(tdRef.value, tdRef.value.childNodes.length);
     const rangeWidth = range.getBoundingClientRect().width;
     const padding =
-      parseInt(window.getComputedStyle(tdEl)['paddingLeft'], 10) + parseInt(window.getComputedStyle(tdEl)['paddingRight'], 10);
-    isShowTooltip.value = props.column.showOverflowTooltip && rangeWidth + padding > tdEl.offsetWidth;
+      parseInt(window.getComputedStyle(tdRef.value)['paddingLeft'], 10) +
+      parseInt(window.getComputedStyle(tdRef.value)['paddingRight'], 10);
+    isShowTooltip.value = props.column.showOverflowTooltip && rangeWidth + padding > tdRef.value.offsetWidth;
   }
 
   onMounted(() => {
-    if (inBrowser && window.ResizeObserver) {
+    if (inBrowser && window.ResizeObserver && props.column.showOverflowTooltip) {
       const observer = new window.ResizeObserver(shouldShowTooltip);
       tdRef.value && observer.observe(tdRef.value);
     }
     tooltipContent.value = getTooltipContent();
+  });
+
+  onBeforeMount(() => {
+    tdRef.value && observer.unobserve(tdRef.value);
   });
 
   return { tdRef, isShowTooltip, tooltipContent };
