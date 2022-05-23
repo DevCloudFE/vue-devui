7fdecde5a5c0a14acfa65eaa255e6b713a9a06a8
caihaiqiang <805248710@qq.com>
2022-05-23 16:27:14 +0800

    feat(auto-complete): 支持prefix、suffix、prepend、append

--- packages/devui-vue/devui/auto-complete/src/auto-complete.tsx
+++ packages/devui-vue/devui/auto-complete/src/auto-complete.tsx
@@ -6,11 +6,13 @@ import useInputHandle from './composables/use-input-handle';
 import useSelectHandle from './composables/use-select-handle';
 import useLazyHandle from './composables/use-lazy-handle';
 import useKeyBoardHandle from './composables/use-keyboard-select';
+import { useAutoCompleteRender } from './composables/use-auto-complete-render';
 import DAutoCompleteDropdown from './components/dropdown';
 import ClickOutside from '../../shared/devui-directive/clickoutside';
 import { FlexibleOverlay } from '../../overlay/src/flexible-overlay';
 import { useNamespace } from '../../shared/hooks/use-namespace';
 import './auto-complete.scss';
+import { Icon } from '../../icon';
 
 export default defineComponent({
   name: 'DAutoComplete',
@@ -34,14 +36,10 @@ export default defineComponent({
       latestSource,
       showAnimation,
       valueParser,
+      placeholder,
     } = toRefs(props);
     const ns = useNamespace('auto-complete');
-    const formNs = useNamespace('form-group');
-    const feedbackNs = useNamespace('has-feedback');
-    const selectNs = useNamespace('select-open');
-    const formControlNs = useNamespace('form-control');
-    const dropdownNs = useNamespace('dropdown-origin');
-    const dropdownOpenNs = useNamespace('dropdown-origin-open');
+    const inputNs = useNamespace('auto-complete-input');
 
     const { handleSearch, searchList, showNoResultItemTemplate, recentlyFocus } = useSearchFn(
       ctx,
@@ -75,6 +73,12 @@ export default defineComponent({
       selectOptionClick,
       handleClose
     );
+    const { autoCompleteTopClasses, inputClasses, inputWrapperClasses, inputInnerClasses } = useAutoCompleteRender(
+      props,
+      ctx,
+      visible,
+      isFocus
+    );
     provide(DropdownPropsKey, {
       props,
       visible,
@@ -94,65 +98,74 @@ export default defineComponent({
     });
     const origin = ref<HTMLElement>();
 
-    const renderDropdown = () => {
-      if (appendToBody.value) {
-        return (
-          <Teleport to="body">
-            <Transition name={showAnimation ? 'fade' : ''}>
-              <FlexibleOverlay show-arrow origin={origin.value} position={position.value} v-model={visible.value}>
-                <div
-                  class={ns.e('menu')}
-                  style={{
-                    width: `
+    const prefixVisiable = ctx.slots.prefix || props.prefix;
+    const suffixVisiable = ctx.slots.suffix || props.suffix;
+
+    const renderBasicDropdown = () => {
+      return (
+        <Transition name={showAnimation ? 'fade' : ''}>
+          <FlexibleOverlay show-arrow origin={origin.value} position={position.value} v-model={visible.value}>
+            <div
+              class={ns.e('menu')}
+              style={{
+                width: `
                       ${width.value + 'px'}
                     `,
-                  }}>
-                  <DAutoCompleteDropdown>{customRenderSolts()}</DAutoCompleteDropdown>
-                </div>
-              </FlexibleOverlay>
-            </Transition>
-          </Teleport>
-        );
+              }}>
+              <DAutoCompleteDropdown>{customRenderSolts()}</DAutoCompleteDropdown>
+            </div>
+          </FlexibleOverlay>
+        </Transition>
+      );
+    };
+
+    const renderDropdown = () => {
+      if (appendToBody.value) {
+        return <Teleport to="body">{renderBasicDropdown()}</Teleport>;
       } else {
-        return (
-          <Transition name={showAnimation ? 'fade' : ''}>
-            <FlexibleOverlay show-arrow origin={origin.value} position={position.value} v-model={visible.value}>
-              <div
-                class={ns.e('menu')}
-                style={{
-                  width: `
-                    ${width.value + 'px'}
-                  `,
-                }}>
-                <DAutoCompleteDropdown>{customRenderSolts()}</DAutoCompleteDropdown>
-              </div>
-            </FlexibleOverlay>
-          </Transition>
-        );
+        return renderBasicDropdown();
       }
     };
     return () => {
       return (
         <div
-          class={[ns.b(), formNs.b(), feedbackNs.b(), visible.value && selectNs.b()]}
+          class={autoCompleteTopClasses.value}
           ref={origin}
           v-click-outside={handleClose}
           style={{
             width: `${width.value + 'px'}`,
           }}>
-          <input
-            disabled={disabled.value}
-            type="text"
-            onClick={toggleMenu}
-            class={[formControlNs.b(), dropdownNs.b(), isFocus.value && dropdownOpenNs.b(), disabled.value && 'disabled']}
-            placeholder="Search"
-            onInput={onInput}
-            onFocus={onFocus}
-            onBlur={onBlur}
-            value={modelValue.value}
-            ref={inputRef}
-            onKeydown={handlekeyDown}
-          />
+          <div class={inputClasses.value}>
+            {ctx.slots.prepend && <div class={inputNs.e('prepend')}>{ctx.slots.prepend?.()}</div>}
+            <div class={inputWrapperClasses.value}>
+              {prefixVisiable && (
+                <span class={inputNs.e('prefix')}>
+                  {ctx.slots.prefix && <div>{ctx.slots.prefix?.()}</div>}
+                  {props.prefix && <Icon size="inhriet" name={props.prefix} />}
+                </span>
+              )}
+              <input
+                disabled={disabled.value}
+                type="text"
+                onClick={toggleMenu}
+                class={inputInnerClasses.value}
+                placeholder={placeholder.value}
+                onInput={onInput}
+                onFocus={onFocus}
+                onBlur={onBlur}
+                value={modelValue.value}
+                ref={inputRef}
+                onKeydown={handlekeyDown}
+              />
+              {suffixVisiable && (
+                <span class={inputNs.e('suffix')}>
+                  {props.suffix && <Icon size="inhriet" name={props.suffix} />}
+                  {ctx.slots.suffix && <div>{ctx.slots.suffix?.()}</div>}
+                </span>
+              )}
+            </div>
+            {ctx.slots.append && <div class={inputNs.e('append')}>{ctx.slots.append?.()}</div>}
+          </div>
           {renderDropdown()}
         </div>
       );
