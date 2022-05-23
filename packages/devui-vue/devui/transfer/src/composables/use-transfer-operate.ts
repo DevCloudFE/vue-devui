a077daca851ecbd90c95037d03e06a5a1936d6eb
Echo <nts318@yeah.net>
2022-05-23 16:33:31 +0800

    Transfer Feat: 新增拖拽排序穿梭框 (#715)
    * refactor: 重构穿梭框组件
    
    * feat(transfer): 搜索穿梭框、排序穿梭框、自定义穿梭框
    
    * style: 解决复选框组类名更改后穿梭框样式不正确
    
    * feat: 拖拽排序穿梭框

--- packages/devui-vue/devui/transfer/src/composables/use-transfer-operate.ts
+++ packages/devui-vue/devui/transfer/src/composables/use-transfer-operate.ts
@@ -23,14 +23,14 @@ export type TTransferOperateProps = ExtractPropTypes<typeof transferOperateProps
 
 export const transferHeaderState = (props: TTransferOperateProps, ctx: SetupContext) => {
   /**
-     * toTargetHandle: 源向目标穿梭
-    */
+   * toTargetHandle: 源向目标穿梭
+  */
   const toTargetHandle = () => {
     ctx.emit('toTarget');
   };
   /**
-     * toSourceHandle: 目标向源穿梭
-    */
+   * toSourceHandle: 目标向源穿梭
+  */
   const toSourceHandle = () => {
     ctx.emit('toSource');
   };
