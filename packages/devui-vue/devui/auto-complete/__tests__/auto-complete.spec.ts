b3c64c943a34aebfd65bedb5858ae2efdc38ffaa
caihaiqiang <805248710@qq.com>
2022-05-23 17:32:23 +0800

    feat(auto-complete): 补充插槽测试用例和说明文档

--- packages/devui-vue/devui/auto-complete/__tests__/auto-complete.spec.ts
+++ packages/devui-vue/devui/auto-complete/__tests__/auto-complete.spec.ts
@@ -4,6 +4,7 @@ import { nextTick, ref } from 'vue';
 import DAutoComplete from '../src/auto-complete';
 
 const ns = useNamespace('auto-complete', true);
+const dotInputNs = useNamespace('auto-complete-input', true);
 const dropdownNS = useNamespace('dropdown', true);
 const selectNS = useNamespace('select', true);
 
@@ -16,14 +17,22 @@ const dropdownMenuClass = dropdownNS.b() + '-menu';
 
 const selectOpenClass = selectNS.b() + '-open';
 
+const dotInputPrefixClass = dotInputNs.e('prefix');
+const dotInputSuffixClass = dotInputNs.e('suffix');
+
+const dotSlotClass = ns.b() + '-slot';
+const dotAppendClass = ns.m('append');
+const dotPrependClass = ns.m('prepend');
+const dotInputPrependClass = dotInputNs.e('prepend');
+const dotInputAppendClass = dotInputNs.e('append');
+
 // delay api
-const wait = (delay = 300) =>
-  new Promise(resolve => setTimeout(() => resolve(true), delay));
+const wait = (delay = 300) => new Promise((resolve) => setTimeout(() => resolve(true), delay));
 
 describe('auto-complete', () => {
   it('init render & KeyboardEvent ', async () => {
     const wrapper = mount({
-      components: {'d-auto-complete': DAutoComplete },
+      components: { 'd-auto-complete': DAutoComplete },
       template: `
         <d-auto-complete
           :source="source"
@@ -33,18 +42,12 @@ describe('auto-complete', () => {
       `,
       setup() {
         const value = ref('');
-        const source = [
-          'C#',
-          'C',
-          'C++',
-          'CPython',
-          'CoffeeScript',
-        ];
+        const source = ['C#', 'C', 'C++', 'CPython', 'CoffeeScript'];
         return {
           value,
           source,
         };
-      }
+      },
     });
     expect(wrapper.find(baseClass).exists()).toBe(true);
     const input = wrapper.find('input');
@@ -53,9 +56,7 @@ describe('auto-complete', () => {
     await nextTick();
     expect(wrapper.find(selectOpenClass).exists()).toBe(true);
     expect(wrapper.find(dropdownItemClass).exists()).toBe(false);
-    expect(wrapper.find(baseClass).attributes('style')).toContain(
-      'width: 450px'
-    );
+    expect(wrapper.find(baseClass).attributes('style')).toContain('width: 450px');
     await input.setValue('c');
     await nextTick();
     expect(wrapper.find(dropdownMenuClass).exists()).toBe(true);
@@ -80,7 +81,7 @@ describe('auto-complete', () => {
 
   it('disabled ', async () => {
     const wrapper = mount({
-      components: {'d-auto-complete': DAutoComplete },
+      components: { 'd-auto-complete': DAutoComplete },
       template: `
         <div>
           <d-auto-complete
@@ -94,23 +95,17 @@ describe('auto-complete', () => {
       setup() {
         const value = ref('');
         const isDisabled = ref(false);
-        const source = [
-          'C#',
-          'C',
-          'C++',
-          'CPython',
-          'CoffeeScript',
-        ];
-        function toggle(){
-          isDisabled.value= !isDisabled.value;
+        const source = ['C#', 'C', 'C++', 'CPython', 'CoffeeScript'];
+        function toggle() {
+          isDisabled.value = !isDisabled.value;
         }
         return {
           value,
           source,
           isDisabled,
-          toggle
+          toggle,
         };
-      }
+      },
     });
     expect(wrapper.find(baseClass).exists()).toBe(true);
     const input = wrapper.find('input');
@@ -130,7 +125,7 @@ describe('auto-complete', () => {
     expect(wrapper.vm.value).toBe('C#');
     expect(wrapper.find(selectOpenClass).exists()).toBe(false);
     button.trigger('click');
-    await  nextTick();
+    await nextTick();
     expect(button.element.innerHTML).toBe('Enable AutoComplete');
     expect(input.element.disabled).toBe(true);
   });
@@ -141,7 +136,7 @@ describe('auto-complete', () => {
       disabled: boolean;
     };
     const wrapper = mount({
-      components: {'d-auto-complete': DAutoComplete },
+      components: { 'd-auto-complete': DAutoComplete },
       template: `
         <d-auto-complete
           v-model="value"
@@ -161,50 +156,61 @@ describe('auto-complete', () => {
         const value = ref('');
         const mySource = ref([
           {
-            label:'C#',
-            disabled:false
-          },{
-            label:'C++',
-            disabled:false
-          },{
-            label:'CPython',
-            disabled:false
-          },{
-            label:'Java',
-            disabled:false
-          },{
-            label:'JavaScript',
-            disabled:false
-          },{
-            label:'Go',
-            disabled:false
-          },{
-            label:'Ruby',
-            disabled:false
-          },{
-            label:'F#',
-            disabled:false
-          },{
-            label:'TypeScript',
-            disabled:false
-          },{
-            label:'SQL',
-            disabled:true
-          },{
-            label:'LiveScript',
-            disabled:false
-          },{
-            label:'CoffeeScript',
-            disabled:false
-          }
+            label: 'C#',
+            disabled: false,
+          },
+          {
+            label: 'C++',
+            disabled: false,
+          },
+          {
+            label: 'CPython',
+            disabled: false,
+          },
+          {
+            label: 'Java',
+            disabled: false,
+          },
+          {
+            label: 'JavaScript',
+            disabled: false,
+          },
+          {
+            label: 'Go',
+            disabled: false,
+          },
+          {
+            label: 'Ruby',
+            disabled: false,
+          },
+          {
+            label: 'F#',
+            disabled: false,
+          },
+          {
+            label: 'TypeScript',
+            disabled: false,
+          },
+          {
+            label: 'SQL',
+            disabled: true,
+          },
+          {
+            label: 'LiveScript',
+            disabled: false,
+          },
+          {
+            label: 'CoffeeScript',
+            disabled: false,
+          },
         ]);
-        const formatter = (item: ItemType) =>{
+        const formatter = (item: ItemType) => {
           return item.label;
         };
         // trem：input输入内容
-        const searchFn = async (trem: string)=>{
+        const searchFn = async (trem: string) => {
           const arr: ItemType[] = [];
-          await new Promise((resolve)=>{
+          await new Promise((resolve) => {
             setTimeout(() => {
               resolve(1);
             }, 500);
@@ -221,9 +227,9 @@ describe('auto-complete', () => {
         return {
           value,
           searchFn,
-          formatter
+          formatter,
         };
-      }
+      },
     });
     expect(wrapper.find(baseClass).exists()).toBe(true);
     const input = wrapper.find('input');
@@ -250,7 +256,7 @@ describe('auto-complete', () => {
 
   it('Customized template display', async () => {
     const wrapper = mount({
-      components: {'d-auto-complete': DAutoComplete },
+      components: { 'd-auto-complete': DAutoComplete },
       template: `
         <d-auto-complete
           :source="source"
@@ -289,9 +295,9 @@ describe('auto-complete', () => {
 
         return {
           value,
-          source
+          source,
         };
-      }
+      },
     });
     expect(wrapper.find(baseClass).exists()).toBe(true);
     const input = wrapper.find('input');
@@ -317,7 +323,7 @@ describe('auto-complete', () => {
     const transInputFocusEmitCB = jest.fn();
     const selectValueCB = jest.fn();
     const wrapper = mount({
-      components: {'d-auto-complete': DAutoComplete },
+      components: { 'd-auto-complete': DAutoComplete },
       template: `
         <d-auto-complete
           :source="source"
@@ -329,26 +335,20 @@ describe('auto-complete', () => {
       `,
       setup() {
         const value = ref('');
-        const source = [
-          'C#',
-          'C',
-          'C++',
-          'CPython',
-          'CoffeeScript',
-        ];
-        const selectValue = (e: Event)=>{
+        const source = ['C#', 'C', 'C++', 'CPython', 'CoffeeScript'];
+        const selectValue = (e: Event) => {
           selectValueCB(e);
         };
-        const transInputFocusEmit = (e: Event)=>{
+        const transInputFocusEmit = (e: Event) => {
           transInputFocusEmitCB(e);
         };
         return {
           value,
           source,
           selectValue,
-          transInputFocusEmit
+          transInputFocusEmit,
         };
-      }
+      },
     });
     expect(wrapper.find(baseClass).exists()).toBe(true);
     const input = wrapper.find('input');
@@ -368,10 +368,10 @@ describe('auto-complete', () => {
 
   it('allowEmptyValueSearch ', async () => {
     const div = document.createElement('div');
-    div.id="app";
+    div.id = 'app';
     document.body.appendChild(div);
     const wrapper = mount({
-      components: {'d-auto-complete': DAutoComplete },
+      components: { 'd-auto-complete': DAutoComplete },
       template: `
         <d-auto-complete
           :source="source"
@@ -382,20 +382,14 @@ describe('auto-complete', () => {
       setup() {
         const value = ref('');
         const allowEmptyValueSearch = ref(true);
-        const source = [
-          'C#',
-          'C',
-          'C++',
-          'CPython',
-          'CoffeeScript',
-        ];
+        const source = ['C#', 'C', 'C++', 'CPython', 'CoffeeScript'];
 
         return {
           value,
           source,
-          allowEmptyValueSearch
+          allowEmptyValueSearch,
         };
-      }
+      },
     });
     expect(wrapper.find(baseClass).exists()).toBe(true);
     const input = wrapper.find('input');
@@ -410,7 +404,7 @@ describe('auto-complete', () => {
 
   it('appendToBody & position', async () => {
     const wrapper = mount({
-      components: {'d-auto-complete': DAutoComplete },
+      components: { 'd-auto-complete': DAutoComplete },
       template: `
         <d-auto-complete
           :source="source"
@@ -422,21 +416,15 @@ describe('auto-complete', () => {
       setup() {
         const value = ref('');
         const appendToBody = ref(true);
-        const source = [
-          'CC#',
-          'C',
-          'C++',
-          'CPython',
-          'CoffeeScript',
-        ];
-        const position =  ref(['bottom']);
+        const source = ['CC#', 'C', 'C++', 'CPython', 'CoffeeScript'];
+        const position = ref(['bottom']);
         return {
           value,
           source,
           appendToBody,
-          position
+          position,
         };
-      }
+      },
     });
     expect(wrapper.find(baseClass).exists()).toBe(true);
     const input = wrapper.find('input');
@@ -450,22 +438,22 @@ describe('auto-complete', () => {
     expect(wrapper.find(selectOpenClass).exists()).toBe(true);
     const ul = document.querySelector(unstyledClass);
     let lis = 0;
-    if(ul&&ul.getElementsByTagName('li')){
-      lis=ul.getElementsByTagName('li').length;
+    if (ul && ul.getElementsByTagName('li')) {
+      lis = ul.getElementsByTagName('li').length;
     }
     expect(lis).toBe(5);
     const li_ed = document.querySelector('.selected');
     let li_text = '';
-    if(li_ed&&li_ed.getElementsByTagName('li')){
-      li_text=li_ed.innerHTML;
+    if (li_ed && li_ed.getElementsByTagName('li')) {
+      li_text = li_ed.innerHTML;
     }
     expect(li_text).toBe('CC#');
   });
 
-  it('latestSource',async () => {
+  it('latestSource', async () => {
     const wrapper = mount({
-      components: {'d-auto-complete': DAutoComplete },
-      template:`
+      components: { 'd-auto-complete': DAutoComplete },
+      template: `
         <div>
           <d-auto-complete
             :source="source"
@@ -474,23 +462,17 @@ describe('auto-complete', () => {
           />
         </div>
       `,
-      setup(){
+      setup() {
         const value = ref('');
-        const latestSource = ref(['JavaScript','TypeScript']);
-        const source = ref([
-          'C#',
-          'C',
-          'C++',
-          'Java',
-          'JavaScript'
-        ]);
+        const latestSource = ref(['JavaScript', 'TypeScript']);
+        const source = ref(['C#', 'C', 'C++', 'Java', 'JavaScript']);
 
         return {
           value,
           source,
-          latestSource
+          latestSource,
         };
-      }
+      },
     });
     expect(wrapper.find(baseClass).exists()).toBe(true);
     const input = wrapper.find('input');
@@ -507,10 +489,10 @@ describe('auto-complete', () => {
     expect(wrapper.vm.value).toBe('Java');
   });
 
-  it('enableLazyLoad',async () => {
+  it('enableLazyLoad', async () => {
     const wrapper = mount({
-      components: {'d-auto-complete': DAutoComplete },
-      template:`
+      components: { 'd-auto-complete': DAutoComplete },
+      template: `
         <div>
           <d-auto-complete
             ref="autoCompleteRef"
@@ -521,7 +503,7 @@ describe('auto-complete', () => {
           />
         </div>
       `,
-      setup(){
+      setup() {
         const value = ref('');
         const source = ref([
           'C#',
@@ -550,18 +532,18 @@ describe('auto-complete', () => {
 
         const loadMore = () => {
           setTimeout(() => {
-            source.value.push('lazyData'+source.value.length);
+            source.value.push('lazyData' + source.value.length);
             const _value = autoCompleteRef.value as any;
             _value.loadFinish();
-          },3000);
+          }, 3000);
         };
         return {
           value,
           source,
           loadMore,
-          autoCompleteRef
+          autoCompleteRef,
         };
-      }
+      },
     });
     expect(wrapper.find(baseClass).exists()).toBe(true);
     const input = wrapper.find('input');
@@ -574,11 +556,7 @@ describe('auto-complete', () => {
     await nextTick();
     expect(wrapper.find(dropdownItemClass).exists()).toBe(true);
     const ul = wrapper.find(unstyledClass);
-    const makeScroll = async (
-      dom: Element,
-      name: 'scrollTop',
-      offset: number
-    ) => {
+    const makeScroll = async (dom: Element, name: 'scrollTop', offset: number) => {
       const eventTarget = dom === document.documentElement ? window : dom;
       dom[name] = offset;
       const evt = new CustomEvent('scroll', {
@@ -603,5 +581,58 @@ describe('auto-complete', () => {
     await nextTick();
     expect(wrapper.find(dropdownItemClass).element.innerHTML).toBe('lazyData21');
   });
-});
 
+  it('d-input prefix/suffix props work', async () => {
+    const wrapper = mount({
+      components: { DAutoComplete },
+      template: `
+        <d-auto-complete prefix="like" suffix="search" />
+      `,
+    });
+    const icon = wrapper.find('.icon');
+    const prefix = wrapper.find(dotInputPrefixClass);
+    const suffix = wrapper.find(dotInputSuffixClass);
+    expect(icon.exists()).toBe(true);
+    expect(prefix.exists()).toBe(true);
+    expect(suffix.exists()).toBe(true);
+  });
+
+  it('d-input prefix/suffix/prepend/append slot work', async () => {
+    const wrapper = mount({
+      components: { DAutoComplete },
+      template: `
+        <d-auto-complete>
+          <template #prepend>
+            <d-button icon="like">测试</d-button>
+          </template>
+          <template #prefix>
+            <d-icon name="search" />
+          </template>
+          <template #suffix>
+            <d-icon name="search" />
+          </template>
+          <template #append>
+            <d-icon name="like" />
+          </template>
+        </d-auto-complete>
+      `,
+    });
+
+    expect(wrapper.find(dotSlotClass).exists()).toBe(true);
+    expect(wrapper.find(dotAppendClass).exists()).toBe(true);
+    expect(wrapper.find(dotPrependClass).exists()).toBe(true);
+
+    const likeIcon = wrapper.find('.icon-like');
+    const searchIcon = wrapper.find('.icon-search');
+    const prefix = wrapper.find(dotInputPrefixClass);
+    const suffix = wrapper.find(dotInputSuffixClass);
+    const prepend = wrapper.find(dotInputPrependClass);
+    const append = wrapper.find(dotInputAppendClass);
+    expect(likeIcon.exists()).toBe(true);
+    expect(searchIcon.exists()).toBe(true);
+    expect(prefix.exists()).toBe(true);
+    expect(suffix.exists()).toBe(true);
+    expect(prepend.exists()).toBe(true);
+    expect(append.exists()).toBe(true);
+  });
+});
