0443a0c1f39b2e54c462ff987db1699dbc30b622
lj111 <luo15001635033@163.com>
2022-05-26 19:23:36 +0800

    refactor(radio): refactor radio render and methods

---
+++ packages/devui-vue/devui/radio/src/use-radio.ts
@@ -0,0 +1,77 @@
+import { computed, inject, SetupContext, toRef, provide } from 'vue';
+import { RadioProps, RadioGroupProps, radioGroupInjectionKey, UseRadioFn, valueTypes } from './radio-types';
+
+export function useRadio(props: RadioProps, ctx: SetupContext): UseRadioFn {
+  const radioGroupConf = inject(radioGroupInjectionKey, null);
+
+  /** 是否禁用 */
+  const isDisabled = computed(() => {
+    return props.disabled || radioGroupConf?.disabled.value;
+  });
+  /** 判断是否勾选 */
+  const isChecked = computed(() => {
+    const _value = radioGroupConf ? radioGroupConf.modelValue.value : props.modelValue;
+
+    return props.value === _value;
+  });
+  /** radio 的 name 属性 */
+  const radioName = computed(() => {
+    return radioGroupConf ? radioGroupConf.name.value : props.name || void 0;
+  });
+
+  /** 判断是否允许切换 */
+  const judgeCanChange = (_value: valueTypes) => {
+    const beforeChange = props.beforeChange || (radioGroupConf ? radioGroupConf.beforeChange : null);
+
+    let flag = Promise.resolve(true);
+    if (beforeChange) {
+      const canChange = beforeChange(_value);
+      if (typeof canChange === 'undefined') {
+        return flag;
+      }
+      if (typeof canChange === 'boolean') {
+        flag = Promise.resolve(canChange);
+      } else {
+        flag = canChange;
+      }
+    }
+    return flag;
+  };
+
+  const handleChange = async (event: Event) => {
+    const _value = props.value;
+    const canChange = await judgeCanChange(_value);
+
+    // 不可以切换
+    if (!canChange) {
+      event.preventDefault();
+      return;
+    }
+    radioGroupConf?.emitChange(_value); // 触发父组件的change
+    ctx.emit('update:modelValue', _value);
+    ctx.emit('change', _value);
+  };
+  return {
+    isChecked,
+    radioName,
+    isDisabled,
+    handleChange,
+  };
+}
+
+export function useRadioGroup(props: RadioGroupProps, ctx: SetupContext): void {
+  /** change 事件 */
+  const emitChange = (radioValue: valueTypes) => {
+    ctx.emit('update:modelValue', radioValue);
+    ctx.emit('change', radioValue);
+  };
+
+  // 注入给子组件
+  provide(radioGroupInjectionKey, {
+    modelValue: toRef(props, 'modelValue'),
+    name: toRef(props, 'name'),
+    disabled: toRef(props, 'disabled'),
+    beforeChange: props.beforeChange,
+    emitChange,
+  });
+}
