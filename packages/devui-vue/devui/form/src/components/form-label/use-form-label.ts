4f994ce8d017bdbcd6db25377c62804f73a5fdcd
wangyupei <2311595895@qq.com>
2022-05-26 19:20:42 +0800

    refactor(Form): Form使用结构变更，简化为d-form&d-form-item两层

--- packages/devui-vue/devui/form/src/components/form-label/use-form-label.ts
+++ packages/devui-vue/devui/form/src/components/form-label/use-form-label.ts
@@ -1,19 +1,18 @@
 import { computed, inject } from 'vue';
-import { FORM_TOKEN, FormContext } from '../../form-types';
-import { FormItemContext, FORM_ITEM_TOKEN } from '../form-item/form-item-types';
+import { FormItemContext, FORM_ITEM_TOKEN, LabelData, LABEL_DATA } from '../form-item/form-item-types';
 import { UseFormLabel } from './form-label-types';
 import { useNamespace } from '../../../../shared/hooks/use-namespace';
 
 export function useFormLabel(): UseFormLabel {
-  const formContext = inject(FORM_TOKEN) as FormContext;
   const formItemContext = inject(FORM_ITEM_TOKEN) as FormItemContext;
+  const labelData = inject(LABEL_DATA) as LabelData;
   const ns = useNamespace('form');
 
   const labelClasses = computed(() => ({
     [`${ns.e('label')}`]: true,
-    [`${ns.em('label', 'vertical')}`]: formContext.layout === 'vertical',
-    [`${ns.em('label', formContext.labelSize)}`]: formContext.layout === 'horizontal',
-    [`${ns.em('label', formContext.labelAlign)}`]: formContext.layout === 'horizontal',
+    [`${ns.em('label', 'vertical')}`]: labelData.value.layout === 'vertical',
+    [`${ns.em('label', labelData.value.labelSize)}`]: labelData.value.layout === 'horizontal',
+    [`${ns.em('label', labelData.value.labelAlign)}`]: labelData.value.layout === 'horizontal',
   }));
 
   const labelInnerClasses = computed(() => ({
