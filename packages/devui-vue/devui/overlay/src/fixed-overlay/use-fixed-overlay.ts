8623be092f7c483271656d5de5e98511be5a482a
wangyupei <2311595895@qq.com>
2022-05-30 20:24:22 +0800

    refactor(FixedOverlay): 重构FixedOverlay

--- packages/devui-vue/devui/overlay/src/fixed-overlay/use-fixed-overlay.ts
+++ packages/devui-vue/devui/overlay/src/fixed-overlay/use-fixed-overlay.ts
@@ -1,61 +1,34 @@
-import { onUnmounted, watch, computed, ComputedRef, onMounted, SetupContext } from 'vue';
-import { OverlayProps, overlayEmits } from './fixed-overlay-types';
-import { useNamespace } from '../../../shared/hooks/use-namespace';
+import { onUnmounted, SetupContext, watch } from 'vue';
+import { FixedOverlayProps, UseFixedOverlay } from './fixed-overlay-types';
+import { lockScroll } from '../../../shared/utils/lock-scroll';
 
-interface CommonInfo {
-  backgroundClass: ComputedRef<string[]>;
-  overlayClass: ComputedRef<string>;
-  handleBackdropClick: (e: Event) => void;
-  handleOverlayBubbleCancel: (e: Event) => void;
-}
+export function useFixedOverlay(props: FixedOverlayProps, ctx: SetupContext): UseFixedOverlay {
+  let lockScrollCb: () => void;
 
-export function useOverlayLogic(props: OverlayProps, ctx: SetupContext<typeof overlayEmits>): CommonInfo {
-  const ns = useNamespace('overlay');
-  const backgroundClass = computed(() => {
-    return [ns.e('background'), props.backgroundClass, !props.hasBackdrop ? ns.em('background', 'disabled') : ns.em('background', 'color')];
-  });
-  const overlayClass = computed(() => {
-    return ns.b();
-  });
-
-  const handleBackdropClick = (event: Event) => {
+  const onClick = (event: Event) => {
     event.preventDefault();
-    props.onBackdropClick?.();
-    if (props.backdropClose) {
-      ctx.emit('update:visible', false);
+    ctx.emit('click', event);
+    if (props.closeOnClickOverlay) {
+      ctx.emit('update:modelValue', false);
     }
   };
 
-  const handleOverlayBubbleCancel = (event: Event) => (event.cancelBubble = true);
-
-  onMounted(() => {
-    const body = document.body;
-    const originOverflow = body.style.overflow;
-    const originPosition = body.style.position;
-    watch([() => props.visible, () => props.backgroundBlock], ([visible, backgroundBlock]) => {
-      if (backgroundBlock) {
-        const top = body.getBoundingClientRect().y;
-        if (visible) {
-          body.style.overflowY = 'scroll';
-          body.style.position = visible ? 'fixed' : '';
-          body.style.top = `${top}px`;
-        } else {
-          body.style.overflowY = originOverflow;
-          body.style.position = originPosition;
-          body.style.top = '';
-          window.scrollTo(0, -top);
-        }
-      }
-    });
-    onUnmounted(() => {
-      document.body.style.overflow = originOverflow;
-    });
-  });
-
-  return {
-    backgroundClass,
-    overlayClass,
-    handleBackdropClick,
-    handleOverlayBubbleCancel,
+  const removeBodyAdditions = () => {
+    lockScrollCb?.();
   };
+
+  watch(
+    () => props.modelValue,
+    (val) => {
+      if (val) {
+        props.lockScroll && (lockScrollCb = lockScroll());
+      } else {
+        removeBodyAdditions();
+      }
+    }
+  );
+
+  onUnmounted(removeBodyAdditions);
+
+  return { onClick };
 }
