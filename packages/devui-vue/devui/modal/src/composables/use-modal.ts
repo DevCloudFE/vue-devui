30d63b7d5723e691f8f14fbe5546cb5d338a0c7c
wangyupei <2311595895@qq.com>
2022-05-31 16:58:04 +0800

    feat(Modal): 新增show-overlay&append-to-body参数&优化动画效果

--- packages/devui-vue/devui/modal/src/composables/use-modal.ts
+++ packages/devui-vue/devui/modal/src/composables/use-modal.ts
@@ -1,18 +1,24 @@
-import { onMounted, onUnmounted } from 'vue';
-import { ModalProps, EmitEventFn, UseModalFn } from '../modal-types';
+import { onMounted, onUnmounted, watch, ref, nextTick } from 'vue';
+import { ModalProps, EmitEventFn, UseModal, UseModalRender } from '../modal-types';
+import { lockScroll } from '../../../shared/utils/lock-scroll';
 
-export function useModal(props: ModalProps, emit: EmitEventFn): UseModalFn {
+export function useModal(props: ModalProps, emit: EmitEventFn): UseModal {
   function close(): void {
     emit('update:modelValue', false);
   }
-  function handleVisibleChange(val: boolean): void {
-    if (!val) {
-      props.beforeClose ? props.beforeClose(close) : close();
-    }
+  function execClose() {
+    props.beforeClose ? props.beforeClose(close) : close();
   }
+  function onOverlayClick() {
+    props.closeOnClickOverlay && execClose();
+  }
+  function onCloseBtnClick() {
+    execClose();
+  }
+
   function onKeydown(event: KeyboardEvent) {
     if (event['keyCode'] === 27) {
-      close();
+      execClose();
     }
   }
 
@@ -28,5 +34,36 @@ export function useModal(props: ModalProps, emit: EmitEventFn): UseModalFn {
     }
   });
 
-  return { handleVisibleChange };
+  return { onCloseBtnClick, onOverlayClick };
+}
+
+export function useModalRender(props: ModalProps): UseModalRender {
+  const showContainer = ref(false);
+  const showModal = ref(false);
+  let lockScrollCb: () => void;
+  const removeBodyAdditions = () => {
+    lockScrollCb?.();
+  };
+  watch(
+    () => props.modelValue,
+    (val) => {
+      if (val) {
+        props.lockScroll && (lockScrollCb = lockScroll());
+        showContainer.value = true;
+        nextTick(() => {
+          showModal.value = true;
+        });
+      } else {
+        removeBodyAdditions();
+        showModal.value = false;
+        setTimeout(() => {
+          showContainer.value = false;
+        }, 100);
+      }
+    }
+  );
+
+  onUnmounted(removeBodyAdditions);
+
+  return { showContainer, showModal };
 }
