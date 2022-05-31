d6edb2c286d36390a2572c23360769f6242bdf53
行言 <2311595895@qq.com>
2022-05-31 17:24:59 +0800

    feat(Modal): 新增show-overlay&append-to-body参数&优化动画效果 (#800)

--- packages/devui-vue/devui/modal/src/modal.tsx
+++ packages/devui-vue/devui/modal/src/modal.tsx
@@ -1,8 +1,8 @@
-import { defineComponent, toRefs, Transition } from 'vue';
+import { defineComponent, Teleport, toRefs, Transition } from 'vue';
 import { modalProps, ModalProps } from './modal-types';
 import { Icon } from '../../icon';
 import { FixedOverlay } from '../../overlay';
-import { useModal } from './composables/use-modal';
+import { useModal, useModalRender } from './composables/use-modal';
 import DModalHeader from './components/header';
 import DModalBody from './components/body';
 import { useNamespace } from '../../shared/hooks/use-namespace';
@@ -13,38 +13,34 @@ export default defineComponent({
   inheritAttrs: false,
   props: modalProps,
   emits: ['update:modelValue'],
-  setup(props: ModalProps, { slots, attrs, emit, expose }) {
+  setup(props: ModalProps, { slots, attrs, emit }) {
     const ns = useNamespace('modal');
-    const { modelValue, lockScroll, closeOnClickOverlay, title, showClose } = toRefs(props);
-    const { handleVisibleChange } = useModal(props, emit);
-    expose({ handleVisibleChange });
+    const { modelValue, title, showClose, showOverlay, appendToBody } = toRefs(props);
+    const { onCloseBtnClick, onOverlayClick } = useModal(props, emit);
+    const { showContainer, showModal } = useModalRender(props);
 
-    return () => {
-      const fixedOverlayProps = {
-        visible: modelValue.value,
-        'onUpdate:visible': handleVisibleChange,
-        'background-class': ns.e('mask'),
-        'background-block': lockScroll.value,
-        'backdrop-close': closeOnClickOverlay.value,
-      };
-      return (
-        <FixedOverlay {...fixedOverlayProps}>
-          <Transition name={ns.m('wipe')}>
-            <div class={ns.b()} {...attrs}>
-              {showClose.value && (
-                <Icon
-                  name="close"
-                  class="btn-close"
-                  size="var(--devui-font-size-md,12px)"
-                  onClick={() => handleVisibleChange(false)}></Icon>
+    return () => (
+      <Teleport to="body" disabled={!appendToBody.value}>
+        {showOverlay.value && (
+          <FixedOverlay v-model={modelValue.value} lock-scroll={false} style={{ zIndex: 'calc(var(--devui-z-index-modal, 1050) - 1)' }} />
+        )}
+        {showContainer.value && (
+          <div class={ns.e('container')} onClick={onOverlayClick}>
+            <Transition name={ns.m('wipe')}>
+              {showModal.value && (
+                <div class={ns.b()} {...attrs}>
+                  {showClose.value && (
+                    <Icon name="close" class="btn-close" size="var(--devui-font-size-md,12px)" onClick={onCloseBtnClick}></Icon>
+                  )}
+                  {slots.header ? slots.header() : title.value && <DModalHeader>{title.value}</DModalHeader>}
+                  <DModalBody>{slots.default?.()}</DModalBody>
+                  {slots.footer?.()}
+                </div>
               )}
-              {slots.header ? slots.header() : title.value && <DModalHeader>{title.value}</DModalHeader>}
-              <DModalBody>{slots.default?.()}</DModalBody>
-              {slots.footer?.()}
-            </div>
-          </Transition>
-        </FixedOverlay>
-      );
-    };
+            </Transition>
+          </div>
+        )}
+      </Teleport>
+    );
   },
 });
