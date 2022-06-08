15a4c04e4b52b696d298e96682a7e02b5d5dee12
lj111 <luo15001635033@163.com>
2022-06-08 15:39:10 +0800

    refactor(time-picker): refactor time-picker

--- packages/devui-vue/devui/time-picker/src/components/time-popup/index.tsx
+++ packages/devui-vue/devui/time-picker/src/components/time-popup/index.tsx
@@ -1,87 +1,92 @@
-import { defineComponent, reactive, ref,watch ,onMounted} from 'vue';
-import { initializeTimeData,setTimeAstrict } from '../../utils';
+import { defineComponent, ref, watch, onMounted } from 'vue';
+import { initializeTimeData, setTimeAstrict } from '../../utils';
 import TimeList from '../popup-line/index';
 import { Button } from '../../../../button/index';
 
 import './index.scss';
 export default defineComponent({
-  name:'DTimePopup',
-  components:{
-    TimeList,Button
+  name: 'DTimePopup',
+  components: {
+    TimeList,
+    Button,
   },
-  props:{
-    showPopup:{
-      type:Boolean,
-      default:false
+  props: {
+    showPopup: {
+      type: Boolean,
+      default: false,
     },
-    popupTop:{
-      type:Number,
-      default:-100
+    popupTop: {
+      type: Number,
+      default: -100,
     },
-    popupLeft:{
-      type:Number,
-      default:-100
+    popupLeft: {
+      type: Number,
+      default: -100,
     },
-    popupWidth:{
-      type:Number,
-      default:300
+    popupWidth: {
+      type: Number,
+      default: 300,
     },
-    popupFormat:{
-      type:String,
-      default:'hh:mm:ss'
+    popupFormat: {
+      type: String,
+      default: 'hh:mm:ss',
     },
-    minTime:{
-      type:String,
-      default:'00:00:00'
+    minTime: {
+      type: String,
+      default: '00:00:00',
     },
-    maxTime:{
-      type:String,
-      default:'23:59:59'
+    maxTime: {
+      type: String,
+      default: '23:59:59',
     },
-    bindData:{
-      type:String,
-      default:'00:00:00'
-    }
+    bindData: {
+      type: String,
+      default: '00:00:00',
+    },
   },
-  emits:['subData'],
-  setup(props,ctx){
+  emits: ['submitData'],
+  setup(props, ctx) {
     const popupDome = ref<Node>();
     const timeListDom = ref();
     const hourList = initializeTimeData('hour');
     const minuteList = initializeTimeData('minute');
     const secondList = initializeTimeData('second');
-
-    onMounted(()=>{
-      setTimeAstrict(hourList,minuteList,secondList,props.minTime,props.maxTime,props.popupFormat);
+    onMounted(() => {
+      setTimeAstrict(hourList, minuteList, secondList, props.minTime, props.maxTime, props.popupFormat);
+      timeListDom.value.setOuterTime(props.bindData);
     });
 
-    watch(()=>[props.showPopup,props.bindData],([showPopup,newTimeVal],[oldShowPopup,oldTimeVal])=>{
-      if(showPopup ||  newTimeVal != oldTimeVal){
-        timeListDom.value.setOutoTime(newTimeVal);
-      }else{
-        timeListDom.value.resetScrollTop();
+    watch(
+      () => [props.showPopup, props.bindData],
+      ([showPopup, newTimeVal], [, oldTimeVal]) => {
+        if (showPopup || newTimeVal !== oldTimeVal) {
+          timeListDom.value.setOuterTime(newTimeVal);
+        } else {
+          timeListDom.value.resetScrollTop();
+        }
       }
-    });
+    );
 
-    const changTimeData = ()=>{
+    const changTimeData = () => {
       return timeListDom.value.getNewTime();
     };
 
-    const subDataFun = (e: MouseEvent)=>{
+    const subDataFun = (e: MouseEvent) => {
       e.stopPropagation();
-      ctx.emit('subData');
+      ctx.emit('submitData');
     };
 
     ctx.expose({
-      changTimeData
+      changTimeData,
     });
 
-    return()=>{
-      return(
+    return () => {
+      return (
         <>
-          <div ref={popupDome}
+          <div
+            ref={popupDome}
             class={`devui-time-popup ${props.showPopup ? 'devui-show-time-popup' : ''}`}
-            style={{'width': props.popupWidth + 'px','top':props.popupTop+'px','left':props.popupLeft+'px'}}>
+            style={{ width: props.popupWidth + 'px', top: props.popupTop + 'px', left: props.popupLeft + 'px' }}>
             <TimeList
               ref={timeListDom}
               hourList={hourList}
@@ -92,21 +97,17 @@ export default defineComponent({
               format={props.popupFormat}
             />
 
-            <div class="devui-time-popup-btn" >
-              <div class='popup-slots'>
-                {
-                  ctx.slots.default?.()
-                }
-              </div>
+            <div class="devui-time-popup-btn">
+              <div class="popup-slots">{ctx.slots.default?.()}</div>
               <div onClick={subDataFun}>
-                <Button variant="common">确定</Button>
+                <Button variant="solid" color="secondary" size="sm">
+                  确定
+                </Button>
               </div>
-
             </div>
           </div>
         </>
       );
     };
-  }
+  },
 });
-
