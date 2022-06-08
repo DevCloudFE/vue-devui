4b40c16dec19a21a56369140ab4e5185aa47532f
lj111 <luo15001635033@163.com>
2022-06-08 15:39:10 +0800

    refactor(time-picker): refactor time-picker

--- packages/devui-vue/devui/time-picker/src/utils.ts
+++ packages/devui-vue/devui/time-picker/src/utils.ts
@@ -1,18 +1,18 @@
-import { Ref , reactive} from 'vue';
-import { ArrType } from './types';
+import { Ref, reactive } from 'vue';
+import { ArrType, timeDataType } from './types';
 
 /**
  * 动态调整弹窗位置
  * @param element
  * @returns { top , left }
  */
-export function getPositionFun(el: Element,left: Ref,top: Ref): any{
+export function getPositionFun(el: Element, left: Ref, top: Ref): void {
   const inputDom = el.getBoundingClientRect();
   const button = window.innerHeight - (inputDom.top + 20);
-  if(button > inputDom.top + 20){
+  if (button > inputDom.top + 20) {
     left.value = inputDom.x;
     top.value = inputDom.top + 20 + 10;
-  }else{
+  } else {
     left.value = inputDom.x;
     top.value = inputDom.top - 316;
   }
@@ -24,20 +24,21 @@ export function getPositionFun(el: Element,left: Ref,top: Ref): any{
  * @returns Array<time>
  */
 
-export function initializeTimeData(type: string): any{
-  const timeArr = reactive([]);
-  let arrLenght = 0;
-  if(type == 'hour'){
-    arrLenght = 24;
-  }else{
-    arrLenght = 60;
+export function initializeTimeData(type: timeDataType): ArrType[] {
+  const timeArr: ArrType[] = reactive([]);
+  let arrLength = 0;
+  if (type === 'hour') {
+    arrLength = 24;
+  } else {
+    arrLength = 60;
   }
-  for (let i = 0; i < arrLenght; i++) {
+  for (let i = 0; i < arrLength; i++) {
     timeArr.push({
-      time:i<10?'0'+i:i+'',
-      isActive:false,
-      flag:type,
-      isDisabled:false
+      time: i < 10 ? '0' + i : i + '',
+      isActive: false,
+      flag: type,
+      isDisabled: false,
+      type,
     });
   }
   return timeArr;
@@ -57,39 +58,37 @@ export const setTimeAstrict = (
   secondList: Array<ArrType>,
   minTime: string,
   maxTime: string,
-  format: string): void=>{
-
+  format: string
+): void => {
   const maxTimeHour = maxTime.split(':')[0];
   const maxTimeMinute = maxTime.split(':')[1];
   const minTimeHour = minTime.split(':')[0];
   const minTimeMinute = minTime.split(':')[1];
   const minTimeSecond = minTime.split(':')[2];
 
-  hourList.map((item,index)=>{
-    if(item.time < minTimeHour || item.time > maxTimeHour){
+  hourList.map((item) => {
+    if (item.time < minTimeHour || item.time > maxTimeHour) {
       item.isDisabled = true;
     }
   });
 
   // 如果为mm:ss格式，限制小时的选择范围
-  if(format == 'mm:ss'){
-    minuteList.map((item,index)=>{
-      if(item.time < minTimeMinute || item.time > maxTimeMinute){
+  if (format === 'mm:ss') {
+    minuteList.map((item) => {
+      if (item.time < minTimeMinute || item.time > maxTimeMinute) {
         item.isDisabled = true;
       }
     });
-  }else{
-    minuteList.map((item,index)=>{
-      if(item.time < minTimeMinute){
+  } else {
+    minuteList.map((item) => {
+      if (item.time < minTimeMinute) {
         item.isDisabled = true;
       }
     });
   }
-  secondList.map((item,index)=>{
-    if(item.time < minTimeSecond){
+  secondList.map((item) => {
+    if (item.time < minTimeSecond) {
       item.isDisabled = true;
     }
   });
 };
-
-
