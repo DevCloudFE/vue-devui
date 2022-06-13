38696ee0ac76085ebf4b958aaa127a88f5ae82c4
lj1990111 <luo15001635033@163.com>
2022-06-13 14:09:18 +0800

    feat(time-picker):  add time-picker size format readonly (#858)

--- packages/devui-vue/devui/time-picker/src/time-picker-types.ts
+++ packages/devui-vue/devui/time-picker/src/time-picker-types.ts
@@ -1,43 +1,52 @@
 import { ExtractPropTypes, PropType } from 'vue';
+import { sizeType } from './types';
 
 export const timePickerProps = {
-  modelValue:{
+  modelValue: {
     type: String,
-    default: ''
+    default: '',
   },
   placeholder: {
     type: String,
-    default: '00:00:00'
+    default: '00:00:00',
   },
   disabled: {
     type: Boolean,
-    default: false
+    default: false,
   },
   timePickerWidth: {
     type: Number,
-    default: 212
+    default: 212,
   },
   minTime: {
     type: String,
-    default: '00:00:00'
+    default: '00:00:00',
     // 默认时间优先级：minTime > modelValue
   },
   maxTime: {
     type: String,
-    default: '23:59:59'
+    default: '23:59:59',
   },
-  format:{
-    type:String,
-    default:'hh:mm:ss'
+  format: {
+    type: String,
+    default: 'hh:mm:ss',
   },
-  autoOpen:{
-    type:Boolean,
-    default:false
+  autoOpen: {
+    type: Boolean,
+    default: false,
   },
-  showAnimation:{
-    type:Boolean,
-    default:true
-  }
+  showAnimation: {
+    type: Boolean,
+    default: true,
+  },
+  size: {
+    type: String as PropType<sizeType>,
+    default: 'md',
+  },
+  readonly: {
+    type: Boolean,
+    default: false,
+  },
 } as const;
 
 export type TimePickerProps = ExtractPropTypes<typeof timePickerProps>;
