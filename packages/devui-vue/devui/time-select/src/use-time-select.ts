cbc92165d7add7fca5287d73e5fe8423b2f293a8
lj111 <luo15001635033@163.com>
2022-06-14 17:31:48 +0800

    feat(time-select): add time-select

---
+++ packages/devui-vue/devui/time-select/src/use-time-select.ts
@@ -0,0 +1,110 @@
+import { computed, SetupContext, ref } from 'vue';
+import { TimeSelectProps, useTimeSelectFn, Time, TimeListType } from './time-select-types';
+
+export function useTimeSelect(props: TimeSelectProps, ctx: SetupContext): useTimeSelectFn {
+  const select = ref();
+  const padTime = (time: number | string) => {
+    return `${time}`.padStart(2, '0');
+  };
+
+  const parseTimeToNumber = (time: string): null | Time => {
+    const timeArr = (time || '').split(':');
+    if (timeArr.length < 2) {
+      return null;
+    }
+    const hour = Number.parseInt(timeArr[0], 10);
+    const minute = Number.parseInt(timeArr[1], 10);
+    return {
+      hour,
+      minute,
+    };
+  };
+
+  const formatTime = (time: Time) => {
+    return `${padTime(time.hour)}:${padTime(time.minute)}`;
+  };
+
+  const nextTime = (time: string, step: string): string => {
+    const timeValue = parseTimeToNumber(time);
+    const stepValue = parseTimeToNumber(step);
+    const nextTimeObj: Time = {
+      hour: timeValue.hour,
+      minute: timeValue.minute,
+    };
+    nextTimeObj.minute += stepValue.minute;
+    nextTimeObj.hour += stepValue.hour;
+    nextTimeObj.hour += Math.floor(nextTimeObj.minute / 60);
+    nextTimeObj.minute = nextTimeObj.minute % 60;
+    return formatTime(nextTimeObj);
+  };
+
+  const start = computed(() => {
+    const startTime = parseTimeToNumber(props.start);
+    return formatTime(startTime);
+  });
+
+  const end = computed(() => {
+    const endTime = parseTimeToNumber(props.end);
+    return formatTime(endTime);
+  });
+
+  const step = computed(() => {
+    const stepTime = parseTimeToNumber(props.step);
+    return formatTime(stepTime);
+  });
+
+  const minTime = computed(() => {
+    const min = parseTimeToNumber(props.minTime);
+    return formatTime(min);
+  });
+
+  const maxTime = computed(() => {
+    const max = parseTimeToNumber(props.maxTime);
+    return formatTime(max);
+  });
+
+  const compareTime = (time1: string, time2: string): number => {
+    const time1Obj = parseTimeToNumber(time1);
+    const time2Obj = parseTimeToNumber(time2);
+    const minutes01 = time1Obj.minute + time1Obj.hour * 60;
+    const minutes02 = time2Obj.minute + time2Obj.hour * 60;
+    if (minutes01 === minutes02) {
+      return 0;
+    }
+    return minutes01 > minutes02 ? 1 : -1;
+  };
+
+  const options = computed(() => {
+    const list = [];
+    if (props.start && props.end && props.step) {
+      let current = start.value;
+      let currentTime;
+      while (compareTime(current, end.value) <= 0) {
+        currentTime = current;
+        list.push({
+          value: currentTime,
+          name: currentTime,
+          disabled: compareTime(current, minTime.value) <= 0 || compareTime(current, maxTime.value) > 0,
+        });
+        current = nextTime(current, step.value);
+      }
+    }
+    return list;
+  });
+
+  const changeData = (data: TimeListType) => {
+    ctx.emit('update:modelValue', data.value);
+    ctx.emit('change', data.value);
+  };
+
+  const clearData = (value: string) => {
+    ctx.emit('update:modelValue', value);
+  };
+
+  return {
+    options,
+    changeData,
+    select,
+    clearData,
+  };
+}
