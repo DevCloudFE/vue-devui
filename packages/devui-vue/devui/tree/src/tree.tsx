5575fc3f3024c800b1e618169a6dc37d27f9263b
gxuud <igaoxian@foxmail.com>
2022-06-15 19:13:47 +0800

    feat(tree): tree组件 虚拟滚动

--- packages/devui-vue/devui/tree/src/tree.tsx
+++ packages/devui-vue/devui/tree/src/tree.tsx
@@ -1,13 +1,15 @@
 import { defineComponent, provide, ref, renderSlot, toRefs, useSlots, watch } from 'vue';
+import type { IInnerTreeNode } from './composables/use-tree-types';
 import DTreeNode from './components/tree-node';
 import DTreeNodeContent from './components/tree-node-content';
 import DTreeNodeToggle from './components/tree-node-toggle';
+import { VirtualList } from '../../virtual-list';
 import useTree from './composables/use-tree';
 import useCheck from './composables/use-check';
 import useSelect from './composables/use-select';
 import useOperate from './composables/use-operate';
 import useMergeNodes from './composables/use-merge-nodes';
-import { USE_TREE_TOKEN } from './const';
+import { USE_TREE_TOKEN, NODE_HEIGHT } from './const';
 import { TreeProps, treeProps } from './tree-types';
 import { useNamespace } from '../../shared/hooks/use-namespace';
 import './tree.scss';
@@ -46,27 +48,44 @@ export default defineComponent({
       treeFactory,
     });
 
+    const renderDTreeNode = (treeNode: IInnerTreeNode) =>
+      slots.default ? (
+        renderSlot(useSlots(), 'default', {
+          treeFactory: treeFactory,
+          nodeData: treeNode,
+        })
+      ) : (
+        <DTreeNode data={treeNode} check={check.value}>
+          {{
+            default: () =>
+              slots.content ? renderSlot(useSlots(), 'content', { nodeData: treeNode }) : <DTreeNodeContent data={treeNode} />,
+            icon: () =>
+              slots.icon ? renderSlot(useSlots(), 'icon', { nodeData: treeNode, toggleNode }) : <DTreeNodeToggle data={treeNode} />,
+          }}
+        </DTreeNode>
+      );
+
     return () => {
+      const Component = props.height ? VirtualList : 'div';
+      const treeData = getExpendedTree?.().value;
+      const vSlotsProps = {
+        default: props.height || (() => treeData?.map(renderDTreeNode)),
+        item: props.height && ((treeNode: IInnerTreeNode) => renderDTreeNode(treeNode))
+      };
+      let virtualListProps = {};
+      if (props.height) {
+        virtualListProps = {
+          height: props.height,
+          data: treeData,
+          itemHeight: NODE_HEIGHT,
+        };
+      }
       return (
-        <div class={ns.b()}>
-          {getExpendedTree?.().value.map((treeNode) =>
-            slots.default ? (
-              renderSlot(useSlots(), 'default', {
-                treeFactory: treeFactory,
-                nodeData: treeNode,
-              })
-            ) : (
-              <DTreeNode data={treeNode} check={check.value}>
-                {{
-                  default: () =>
-                    slots.content ? renderSlot(useSlots(), 'content', { nodeData: treeNode }) : <DTreeNodeContent data={treeNode} />,
-                  icon: () =>
-                    slots.icon ? renderSlot(useSlots(), 'icon', { nodeData: treeNode, toggleNode }) : <DTreeNodeToggle data={treeNode} />,
-                }}
-              </DTreeNode>
-            )
-          )}
-        </div>
+        <Component
+          class={ns.b()}
+          {...virtualListProps}
+          v-slots={vSlotsProps}
+        />
       );
     };
   },
