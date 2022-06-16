e8b998a366e7659069e196948cc2fc6365b9cbe3
gxuud <igaoxian@foxmail.com>
2022-06-16 19:37:54 +0800

    test(tree): 添加单元测试

--- packages/devui-vue/devui/tree/__tests__/node-merge/node-merge-data.ts
+++ packages/devui-vue/devui/tree/__tests__/node-merge/node-merge-data.ts
@@ -6,13 +6,54 @@ export const nodeMergeData: TreeData = [
     children: [
       {
         label: 'Parent node 1-1',
-        open: true,
         children: [
           {
-            label: 'Leaf node 1-1-1',
-          }
-        ]
+            label: 'Parent node 1-1-1',
+            children: [
+              {
+                label: 'Parent node 1-1-1-1',
+              },
+            ],
+          },
+        ],
       },
-    ]
-  }
+    ],
+  },
+  {
+    label: 'Parent node 2',
+    children: [
+      {
+        label: 'Parent node 2-1',
+        children: [
+          {
+            label: 'Leaf node 2-1-1',
+          },
+          {
+            label: 'Leaf node 2-1-2',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: 'Parent node 3',
+    children: [
+      {
+        label: 'Leaf node 3-1',
+        children: [
+          {
+            label: 'Leaf node 3-1-1',
+            children: [
+              {
+                label: 'Leaf node 3-1-1-1',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        label: 'Leaf node 3-2',
+      },
+    ],
+  },
 ];
