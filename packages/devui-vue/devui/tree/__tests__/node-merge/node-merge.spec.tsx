76e529d5f2b2e7ddda9fadc0da372da9cf7d09e0
gxuud <igaoxian@foxmail.com>
2022-06-16 19:37:54 +0800

    test(tree): 添加单元测试

--- packages/devui-vue/devui/tree/__tests__/node-merge/node-merge.spec.tsx
+++ packages/devui-vue/devui/tree/__tests__/node-merge/node-merge.spec.tsx
@@ -1,31 +1,47 @@
-import { ComponentPublicInstance } from 'vue';
-import { DOMWrapper, mount, VueWrapper } from '@vue/test-utils';
+import type { ComponentPublicInstance } from 'vue';
+import { ref, onMounted } from 'vue';
+import { mount, VueWrapper } from '@vue/test-utils';
 import { Tree } from '../../';
 import { nodeMergeData } from './node-merge-data';
 
 describe('Tree node merge', () => {
   let wrapper: VueWrapper<ComponentPublicInstance>;
-  let childNodes: DOMWrapper<Element>[];
 
   beforeAll(() => {
     wrapper = mount({
       setup() {
+        const treeRef = ref<any>(null);
+        onMounted(() => {
+          treeRef.value.treeFactory.mergeTreeNodes();
+          treeRef.value.treeFactory.expandAllNodes();
+        });
         return () => {
-          return <Tree data={nodeMergeData} />;
+          return <Tree data={nodeMergeData} ref={treeRef} />;
         };
       },
     });
-
-    childNodes = wrapper.findAll('.devui-tree-node');
   });
 
   afterAll(() => {
     wrapper.unmount();
   });
 
-  it('The parent nodes should be merged into one node when all parent nodes hava only one child node', () => {
-    expect(childNodes).toHaveLength(2);
-    expect(childNodes[0].text()).toBe('Parent node 1 / Parent node 1-1');
-    expect(childNodes[1].text()).toBe('Leaf node 1-1-1');
+  it('The parent nodes should be merged into one node when all parent nodes hava only one child node', async () => {
+    expect(wrapper.findAll('.devui-tree__node')).toHaveLength(9);
+    const TREE_NODE_DICT = [
+      'Parent node 1 / Parent node 1-1 / Parent node 1-1-1',
+      'Parent node 1-1-1-1',
+      'Parent node 2 / Parent node 2-1',
+      'Leaf node 2-1-1',
+      'Leaf node 2-1-2',
+      'Parent node 3',
+      'Leaf node 3-1 / Leaf node 3-1-1',
+      'Leaf node 3-1-1-1',
+      'Leaf node 3-2',
+    ];
+    const titleList = wrapper.findAll('.devui-tree__node-title');
+    TREE_NODE_DICT.forEach((item, index) => {
+      expect(titleList[index].text()).toBe(item);
+    });
   });
 });
