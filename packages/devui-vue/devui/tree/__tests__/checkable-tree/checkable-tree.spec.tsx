bc0b067100af4426f48ccdb5a807f5ab0439c373
gxuud <igaoxian@foxmail.com>
2022-06-16 19:37:54 +0800

    test(tree): 添加单元测试

--- packages/devui-vue/devui/tree/__tests__/checkable-tree/checkable-tree.spec.tsx
+++ packages/devui-vue/devui/tree/__tests__/checkable-tree/checkable-tree.spec.tsx
@@ -1,22 +1,24 @@
-import { ComponentPublicInstance } from 'vue';
+import type { ComponentPublicInstance } from 'vue';
+import { onMounted, ref } from 'vue';
 import { DOMWrapper, mount, VueWrapper } from '@vue/test-utils';
 import { Tree } from '../../';
 import { checkableTreeData } from './checkable-tree-data';
 
 describe('Checkable tree', () => {
   let wrapper: VueWrapper<ComponentPublicInstance>;
-  let childNodes: DOMWrapper<Element>[];
 
   beforeAll(() => {
     wrapper = mount({
       setup() {
+        const treeRef = ref<any>(null);
+        onMounted(() => {
+          treeRef.value.treeFactory.expandAllNodes();
+        });
         return () => {
-          return <Tree data={checkableTreeData} checkable />;
+          return <Tree data={checkableTreeData} checkable ref={treeRef} />;
         };
       },
     });
-
-    childNodes = wrapper.findAll('.devui-tree-node');
   });
 
   afterAll(() => {
@@ -24,14 +26,20 @@ describe('Checkable tree', () => {
   });
 
   it('Should render checkbox correctly.', () => {
-    expect(childNodes[0].find('.devui-checkbox').exists()).toBe(true);
-    expect(childNodes[0].find('.devui-checkbox').classes()).toContain('unchecked');
+    const childNodeList = wrapper.findAll('.devui-checkbox > label');
+    childNodeList.forEach((item) => {
+      expect(item.exists()).toBe(true);
+      expect(item.classes()).toContain('unchecked');
+    });
   });
 
   it('Should toggle the checked state of the node correctly.', async () => {
-    await childNodes[0].find('.devui-checkbox').trigger('click');
-    expect(childNodes[0].find('.devui-checkbox').classes()).toContain('active');
-    expect(childNodes[0].find('.devui-tree-node__content').classes()).not.toContain('active');
+    const childNodeList = wrapper.findAll('.devui-checkbox > label');
+    childNodeList.forEach(async (item) => {
+      expect(item.classes()).toContain('unchecked');
+      await item.trigger('click');
+      expect(item.classes()).toContain('active');
+    });
   });
 
   it.todo('The checkbox should be checked when setting checked to true.');
