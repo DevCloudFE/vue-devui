db89567b31c8fa85bb6d90935973a58aa9bd1487
gxuud <igaoxian@foxmail.com>
2022-06-17 12:32:17 +0800

    feat(tree): 添加单元测试

--- packages/devui-vue/devui/tree/__tests__/checkable-tree/checkable-tree.spec.tsx
+++ packages/devui-vue/devui/tree/__tests__/checkable-tree/checkable-tree.spec.tsx
@@ -1,32 +1,53 @@
 import type { ComponentPublicInstance } from 'vue';
 import { onMounted, ref } from 'vue';
-import { DOMWrapper, mount, VueWrapper } from '@vue/test-utils';
+import { mount, VueWrapper } from '@vue/test-utils';
 import { Tree } from '../../';
 import { checkableTreeData } from './checkable-tree-data';
 
 describe('Checkable tree', () => {
   let wrapper: VueWrapper<ComponentPublicInstance>;
+  let wrapper2: VueWrapper<ComponentPublicInstance>;
 
   beforeAll(() => {
-    wrapper = mount({
+    const mountData = {
       setup() {
         const treeRef = ref<any>(null);
+        const data = ref(checkableTreeData);
         onMounted(() => {
           treeRef.value.treeFactory.expandAllNodes();
         });
+        const onChangeCheck = () => {
+          const obj = [...data.value];
+          obj[0].checked = true;
+          data.value = obj;
+        };
+        const onChangeDisableCheck = () => {
+          const obj = [...data.value];
+          obj[0].disableCheck = true;
+          data.value = obj;
+        };
         return () => {
-          return <Tree data={checkableTreeData} checkable ref={treeRef} />;
+          return (
+            <>
+              <Tree data={data.value} check ref={treeRef} />
+              <button class="check" onClick={onChangeCheck}>click</button>
+              <button class="disable" onClick={onChangeDisableCheck}>click</button>
+            </>
+          );
         };
       },
-    });
+    };
+    wrapper = mount(mountData);
+    wrapper2 = mount(mountData);
   });
 
   afterAll(() => {
     wrapper.unmount();
+    wrapper2.unmount();
   });
 
   it('Should render checkbox correctly.', () => {
-    const childNodeList = wrapper.findAll('.devui-checkbox > label');
+    const childNodeList = wrapper.findAll('.devui-checkbox');
     childNodeList.forEach((item) => {
       expect(item.exists()).toBe(true);
       expect(item.classes()).toContain('unchecked');
@@ -34,15 +55,23 @@ describe('Checkable tree', () => {
   });
 
   it('Should toggle the checked state of the node correctly.', async () => {
-    const childNodeList = wrapper.findAll('.devui-checkbox > label');
+    const childNodeList = wrapper.findAll('.devui-checkbox');
     childNodeList.forEach(async (item) => {
       expect(item.classes()).toContain('unchecked');
-      await item.trigger('click');
+      await item.get('label').trigger('click');
       expect(item.classes()).toContain('active');
     });
   });
 
-  it.todo('The checkbox should be checked when setting checked to true.');
+  it('The checkbox should be checked when setting checked to true.', async () => {
+    expect(wrapper2.findAll('.devui-checkbox')[0].classes()).toContain('unchecked');
+    await wrapper2.get('.check').trigger('click');
+    expect(wrapper2.findAll('.devui-checkbox')[0].classes()).toContain('active');
+  });
 
-  it.todo('The checkbox can\'t  be checked when setting disableCheck to true.');
+  it('The checkbox can\'t  be checked when setting disableCheck to true.', async () => {
+    expect(wrapper2.findAll('.devui-checkbox')[0].classes()).toContain('active');
+    await wrapper2.get('.disable').trigger('click');
+    expect(wrapper2.findAll('.devui-checkbox')[0].classes()).toContain('active');
+  });
 });
