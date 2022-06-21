7893852c902427cdc9ea306dd2a840b7d5bfac81
Kagol <kagol@sina.com>
2022-06-21 09:44:37 +0800

    feat(tree): 增加节点点击事件

--- packages/devui-vue/devui/tree/src/components/tree-node.tsx
+++ packages/devui-vue/devui/tree/src/components/tree-node.tsx
@@ -1,6 +1,6 @@
 import type { ComputedRef } from 'vue';
 import { computed, defineComponent, inject, PropType, renderSlot, toRefs, useSlots } from 'vue';
-import { NODE_HEIGHT, USE_TREE_TOKEN } from '../const';
+import { NODE_HEIGHT, TREE_INSTANCE, USE_TREE_TOKEN } from '../const';
 import { IInnerTreeNode, IUseTree, ICheck } from '../composables/use-tree-types';
 import DTreeNodeToggle from './tree-node-toggle';
 import { Checkbox } from '../../../checkbox';
@@ -24,6 +24,7 @@ export default defineComponent({
   setup(props, { slots }) {
     const { data, check } = toRefs(props);
     const { toggleSelectNode, toggleCheckNode, toggleNode, getChildren } = inject(USE_TREE_TOKEN) as Partial<IUseTree>;
+    const treeInstance = inject(TREE_INSTANCE);
     const ns = useNamespace('tree');
 
     const { nodeClass, nodeStyle, nodeContentClass, nodeVLineClass, nodeVLineStyles, nodeHLineClass } = useTreeNode(
@@ -66,6 +67,7 @@ export default defineComponent({
             class={nodeContentClass.value}
             onClick={() => {
               toggleSelectNode?.(data.value);
+              treeInstance.emit('node-click', data.value);
             }}>
             <span class={nodeHLineClass.value}></span>
             {slots.icon ? renderSlot(useSlots(), 'icon', { nodeData: data, toggleNode }) : <DTreeNodeToggle data={data.value} />}
