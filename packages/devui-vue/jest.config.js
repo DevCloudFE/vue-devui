79c36e6b78db0e2d9010d47f970a89a51f7fb886
daviForevel <657434047@qq.com>
2022-06-30 10:27:28 +0800

    feat(date-picker-pro): 去除多余修改

--- packages/devui-vue/jest.config.js
+++ packages/devui-vue/jest.config.js
@@ -14,8 +14,7 @@ module.exports = {
   },
   transformIgnorePatterns: ['node_modules/?!(lodash-es)'],
   // The glob patterns Jest uses to detect test files
-  // testMatch: ['**/**/*.spec.(ts|tsx)'],
-  testRegex: '__tests__/month-picker.spec.tsx',
+  testMatch: ['**/**/*.spec.(ts|tsx)'],
 
   // An array of file extensions your modules use
   moduleFileExtensions: ['js', 'json', 'ts', 'tsx'],
