0c576668667dea9bdd578a50969cc45f4764883a
gxuud <igaoxian@foxmail.com>
2022-06-30 15:24:10 +0800

    style(anchor): 修复eslint错误

--- packages/devui-vue/devui/anchor/src/utils.ts
+++ packages/devui-vue/devui/anchor/src/utils.ts
@@ -1,9 +1,13 @@
 let repeatCount = 0;
-let cTimeout;
+let cTimeout: NodeJS.Timeout;
 const timeoutIntervalSpeed = 10;
-let hashName: string;
+let hashName: string | null;
 // 滚动是由于点击产生
 let scollFlag = false;
+let activeLink: Element | null = null;
+let rootActiveLink: Element | null = null;
+let rootClassName = '';
+
 function elementPosition(obj: HTMLElement) {
   let curleft = 0, curtop = 0;
   curleft = obj.offsetLeft;
@@ -11,92 +15,18 @@ function elementPosition(obj: HTMLElement) {
   return { x: curleft, y: curtop };
 }
 
-export function scrollToControl(elem: HTMLElement, container: HTMLElement): void {
-  hashName = elem.getAttribute('name');
-  scollFlag = true;
-  const tops = container.scrollTop>=0 ? container.scrollTop  : -(document.getElementsByClassName('mycontainer')[0] as HTMLElement).offsetTop;
-  let scrollPos: number = elementPosition(elem).y - tops ;
-
-  scrollPos = scrollPos - document.documentElement.scrollTop;
-  const remainder: number = scrollPos % timeoutIntervalSpeed;
-  const repeatTimes = Math.abs((scrollPos - remainder) / timeoutIntervalSpeed);
-  if (scrollPos < 0 && container || elem.getBoundingClientRect().top < container.offsetTop) {
-    window.scrollBy(0, elem.getBoundingClientRect().top-container.offsetTop-16);
-  }
-  // 多个计时器达到平滑滚动效果
-  scrollSmoothly(scrollPos, repeatTimes, container);
+function getSidebarLinks(curRootClassName: string): Array<HTMLAnchorElement> {
+  return [].slice.call(document.querySelectorAll(`.${curRootClassName} > .step-nav > li.bar-link-item > a`));
 }
 
-
-function scrollSmoothly(scrollPos: number, repeatTimes: number, container: HTMLElement): void {
-  if (repeatCount <= repeatTimes) {
-    scrollPos > 0
-      ? container.scrollBy(0, timeoutIntervalSpeed)
-      : container.scrollBy(0, -timeoutIntervalSpeed);
-  }
-  else {
-    repeatCount = 0;
-    clearTimeout(cTimeout);
-    history.replaceState(null, null, document.location.pathname + '#' + hashName);
-
-    hightLightFn(hashName);
-    setTimeout(() => {
-      scollFlag = false;
-    }, 310);
-    return ;
-
-  }
-  repeatCount++;
-  cTimeout = setTimeout(() => {
-    scrollSmoothly(scrollPos, repeatTimes, container);
-  }, 10);
+function getAnchors(sidebarLinks: Array<HTMLAnchorElement>): Array<HTMLAnchorElement> {
+  return [].slice
+    .call(document.querySelectorAll('.box-anchor'))
+    .filter((anchor: HTMLAnchorElement) => sidebarLinks.some((sidebarLink: HTMLAnchorElement) =>  sidebarLink.hash === anchor.hash));
 }
 
-// 高亮切换
-export function hightLightFn(hashName: string): void {
-
-  const childLength = document.getElementsByClassName('mysidebar')[0].children.length;
-
-  for (let i = 0; i < childLength; i++) {
-
-    if (document.getElementsByClassName('mysidebar')[0].children[i].classList.value.indexOf('active') > -1) {
-
-      document.getElementsByClassName('mysidebar')[0].children[i].classList.remove('active');
-    }
-  }
-  document.getElementById(hashName).classList.add('active');
-
-
-}
-let activeLink = null;
-let rootActiveLink = null;
-let rootClassName = '';
-export const setActiveLink = (timeId: string): void => {
-  if (scollFlag) { return; }
-  timeId ? rootClassName = timeId : rootClassName = document.getElementsByClassName('mymain')[0].id;
-
-  const sidebarLinks = getSidebarLinks(rootClassName);
-  const anchors = getAnchors(sidebarLinks);
-  try {
-    anchors.forEach((index,i)=> {
-
-      const anchor: HTMLAnchorElement = anchors[i];
-      const nextAnchor: HTMLAnchorElement = anchors[i + 1];
-
-      const [isActive, hash] = isAnchorActive(i, anchor, nextAnchor);
-      if (isActive) {
-        history.replaceState(null, document.title, hash ? hash as string : ' ');
-        activateLink(hash);
-        throw Error(hash+'');
-      }
-    });
-  } catch (e) {
-  }
-
-};
-
-function throttleAndDebounce(fn: any, delay: number): any {
-  let timeout: any;
+function throttleAndDebounce(fn: () => void, delay: number): () => void {
+  let timeout: NodeJS.Timeout;
   let called = false;
   return () => {
     if (timeout) {
@@ -115,9 +45,31 @@ function throttleAndDebounce(fn: any, delay: number): any {
   };
 }
 
-export const onScroll = throttleAndDebounce(setActiveLink, 300);
+function deactiveLink(link: Element | null): void {
+  link && link.classList.remove('active');
+}
 
-function activateLink(hash: string | boolean): void {
+
+// 高亮切换
+export function hightLightFn(curHashName: string | null): void {
+
+  const childLength = document.getElementsByClassName('mysidebar')[0].children.length;
+
+  for (let i = 0; i < childLength; i++) {
+
+    if (document.getElementsByClassName('mysidebar')[0].children[i].classList.value.indexOf('active') > -1) {
+
+      document.getElementsByClassName('mysidebar')[0].children[i].classList.remove('active');
+    }
+  }
+
+  if (curHashName) {
+    document.getElementById(curHashName)?.classList.add('active');
+  }
+
+}
+
+function activateLink(hash: string | boolean | null): void {
   deactiveLink(activeLink);
   deactiveLink(rootActiveLink);
   hash
@@ -143,16 +95,14 @@ function activateLink(hash: string | boolean): void {
     rootActiveLink = null;
   }
 }
-function deactiveLink(link: HTMLElement): void {
-  link && link.classList.remove('active');
-}
+
 function getPageOffset(): number {
   return (document.querySelector('.mysidebar ') as HTMLElement).getBoundingClientRect().y;
 }
 
 function getAnchorTop(anchor: HTMLAnchorElement): number {
   const pageOffset = getPageOffset();
-  return anchor.parentElement.offsetTop - pageOffset - 5;
+  return (anchor.parentElement?.offsetTop as number) - pageOffset - 5;
 }
 
 function isAnchorActive(index: number, anchor: HTMLAnchorElement, nextAnchor: HTMLAnchorElement) {
@@ -175,12 +125,70 @@ function isAnchorActive(index: number, anchor: HTMLAnchorElement, nextAnchor: HT
   return [false, null];
 }
 
-function getSidebarLinks(rootClassName: string): Array<HTMLAnchorElement> {
-  return [].slice.call(document.querySelectorAll(`.${rootClassName} > .step-nav > li.bar-link-item > a`));
+function scrollSmoothly(scrollPos: number, repeatTimes: number, container: HTMLElement): void {
+  if (repeatCount <= repeatTimes) {
+    scrollPos > 0
+      ? container.scrollBy(0, timeoutIntervalSpeed)
+      : container.scrollBy(0, -timeoutIntervalSpeed);
+  }
+  else {
+    repeatCount = 0;
+    clearTimeout(cTimeout);
+    history.replaceState(null, '', document.location.pathname + '#' + hashName);
+
+    hightLightFn(hashName);
+    setTimeout(() => {
+      scollFlag = false;
+    }, 310);
+    return ;
+
+  }
+  repeatCount ++;
+  cTimeout = setTimeout(() => {
+    scrollSmoothly(scrollPos, repeatTimes, container);
+  }, 10);
 }
 
-function getAnchors(sidebarLinks: Array<HTMLAnchorElement>): Array<HTMLAnchorElement> {
-  return [].slice
-    .call(document.querySelectorAll('.box-anchor'))
-    .filter((anchor: HTMLAnchorElement) => sidebarLinks.some((sidebarLink: HTMLAnchorElement) =>  sidebarLink.hash === anchor.hash));
+export function scrollToControl(elem: HTMLElement, container: HTMLElement): void {
+  hashName = elem.getAttribute('name');
+  scollFlag = true;
+  const tops = container.scrollTop >= 0 ?
+    container.scrollTop  :
+    -(document.getElementsByClassName('mycontainer')[0] as HTMLElement).offsetTop;
+  let scrollPos: number = elementPosition(elem).y - tops ;
+
+  scrollPos = scrollPos - document.documentElement.scrollTop;
+  const remainder: number = scrollPos % timeoutIntervalSpeed;
+  const repeatTimes = Math.abs((scrollPos - remainder) / timeoutIntervalSpeed);
+  if (scrollPos < 0 && container || elem.getBoundingClientRect().top < container.offsetTop) {
+    window.scrollBy(0, elem.getBoundingClientRect().top-container.offsetTop-16);
+  }
+  // 多个计时器达到平滑滚动效果
+  scrollSmoothly(scrollPos, repeatTimes, container);
 }
+
+export const setActiveLink = (timeId?: string): void => {
+  if (scollFlag) { return; }
+  timeId ? rootClassName = timeId : rootClassName = document.getElementsByClassName('mymain')[0].id;
+
+  const sidebarLinks = getSidebarLinks(rootClassName);
+  const anchors = getAnchors(sidebarLinks);
+  try {
+    anchors.forEach((index, i)=> {
+
+      const anchor: HTMLAnchorElement = anchors[i];
+      const nextAnchor: HTMLAnchorElement = anchors[i + 1];
+
+      const [isActive, hash] = isAnchorActive(i, anchor, nextAnchor);
+      if (isActive) {
+        history.replaceState(null, document.title, hash ? hash as string : ' ');
+        activateLink(hash);
+        throw Error(hash + '');
+      }
+    });
+  } catch (e) {
+  }
+
+};
+
+export const onScroll = throttleAndDebounce(setActiveLink, 300);
