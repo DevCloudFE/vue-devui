c195968725dffaf3493d9208ea8d797e00e23e65
gxuud <igaoxian@foxmail.com>
2022-06-30 15:24:10 +0800

    style(anchor): 修复eslint错误

--- packages/devui-vue/devui/anchor/src/d-anchor-link.ts
+++ packages/devui-vue/devui/anchor/src/d-anchor-link.ts
@@ -2,14 +2,14 @@ import { scrollToControl } from './utils';
 interface Bind {
   value: string;
 }
-
+type IScrollContainer = HTMLElement & Window & typeof globalThis;
 export default  {
-  name: 'd-anchor-link',
+  name: 'DAnchorLink',
   // 当被绑定的元素挂载到 DOM 中时……
   // 1.点击滚动到对应位置，并且高亮
   // 2.到对应位置后，改变url后hash
 
-  mounted(el: HTMLElement,binding: Bind): void {
+  mounted(el: HTMLElement, binding: Bind): void {
     const parent: Element = el.parentNode  as Element;
     if (!parent.className) {
       parent.className = 'mysidebar step-nav';
@@ -19,11 +19,11 @@ export default  {
     el.setAttribute('id', binding.value);
 
     el.onclick = () => {
-      let scrollContainer: any;
+      let scrollContainer: IScrollContainer;
       const scollToDomY = document.getElementsByName(binding.value)[0];
       document.getElementsByClassName('scrollTarget').length
-        ? scrollContainer = document.getElementsByClassName('scrollTarget')[0]
-        : scrollContainer = window;
+        ? scrollContainer = (document.getElementsByClassName('scrollTarget')[0] as IScrollContainer)
+        : scrollContainer = window as IScrollContainer;
       scrollToControl(scollToDomY, scrollContainer);
 
     };
