81f0c742d53bc234cb2a4b910d9c684abc063176
gxuud <igaoxian@foxmail.com>
2022-07-01 18:54:40 +0800

    style(date-picker): 修复eslint错误

--- packages/devui-vue/devui/date-picker/src/components/toolbar/svg-icon.tsx
+++ packages/devui-vue/devui/date-picker/src/components/toolbar/svg-icon.tsx
@@ -1,9 +1,16 @@
 import { TIconSvgProps } from '../types';
 
-export const Year = (props: TIconSvgProps) => {
+export const Year = (props: TIconSvgProps): JSX.Element => {
   const { color = '#585d6b', rotate = 0 } = props;
   return (
-    <svg style={{ transform: `rotate(${rotate}deg)` }} width="10px" height="10px" viewBox="0 0 10 10" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      style={{ transform: `rotate(${rotate}deg)` }}
+      width="10px"
+      height="10px"
+      viewBox="0 0 10 10"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <g fill={color} transform="translate(-1.000000, -1.000000)">
         <path d="M11,1.83333333 L11,10.1666667 L7,7.38833333 L7,10.1666667 L1,6 L7,1.83333333 L7,4.61033333 L11,1.83333333 Z" />
       </g>
@@ -11,10 +18,15 @@ export const Year = (props: TIconSvgProps) => {
   );
 };
 
-export const Month = (props: TIconSvgProps) => {
+export const Month = (props: TIconSvgProps): JSX.Element => {
   const { color = '#585d6b', rotate = 0 } = props;
   return (
-    <svg style={{ transform: `rotate(${rotate}deg)` }} width="6px" height="10px" viewBox="0 0 6 10" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      style={{ transform: `rotate(${rotate}deg)` }}
+      width="6px" height="10px" viewBox="0 0 6 10"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <g fill={color} transform="translate(-3.000000, -1.000000)">
         <polygon points="6 3 10.1666667 9 1.83333333 9" />
       </g>
