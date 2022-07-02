e85aea773e4852c893ace3848e313762e2d78188
gxuud <igaoxian@foxmail.com>
2022-07-02 16:32:10 +0800

    style(input-number): 修复eslint错误

--- packages/devui-vue/devui/input-number/index.ts
+++ packages/devui-vue/devui/input-number/index.ts
@@ -1,10 +1,6 @@
 import type { App } from 'vue';
 import InputNumber from './src/input-number';
 
-InputNumber.install = function(app: App) {
-  app.component(InputNumber.name, InputNumber);
-};
-
 export { InputNumber };
 
 export default {
@@ -12,6 +8,6 @@ export default {
   category: '数据录入',
   status: '50%',
   install(app: App): void {
-    app.use(InputNumber as any);
+    app.component(InputNumber.name, InputNumber);
   }
 };
