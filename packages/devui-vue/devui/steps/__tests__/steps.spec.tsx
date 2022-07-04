c6a2a1d080a102bca11d72fa91eb9fb181e38049
gxuud <igaoxian@foxmail.com>
2022-07-04 20:42:09 +0800

    style(auto-complate、steps): 修复eslint报错

--- packages/devui-vue/devui/steps/__tests__/steps.spec.tsx
+++ packages/devui-vue/devui/steps/__tests__/steps.spec.tsx
@@ -1,12 +1,12 @@
-import { ComponentPublicInstance } from 'vue';
-import { DOMWrapper, mount, VueWrapper } from '@vue/test-utils';
+// import { ComponentPublicInstance } from 'vue';
+import { mount } from '@vue/test-utils';
 import { Steps } from '..';
 
 describe('steps', () => {
-  let wrapper: VueWrapper<ComponentPublicInstance>;
+  // let wrapper: VueWrapper<ComponentPublicInstance>;
 
   it('steps init render', async () => {
-    wrapper = mount({
+    mount({
       setup() {
         return () => {
           return <Steps />;
