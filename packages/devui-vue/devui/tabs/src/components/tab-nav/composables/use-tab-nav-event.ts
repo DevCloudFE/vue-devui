74d783a3f63cca22c552b4f797b761bfd819d6b9
gxuud <igaoxian@foxmail.com>
2022-07-04 20:32:47 +0800

    style(tabs): 修复eslint报错

--- packages/devui-vue/devui/tabs/src/components/tab-nav/composables/use-tab-nav-event.ts
+++ packages/devui-vue/devui/tabs/src/components/tab-nav/composables/use-tab-nav-event.ts
@@ -1,8 +1,9 @@
 import type { SetupContext } from 'vue';
 import type { UseTabNavEvent } from '../tab-nav-types';
+import type { TabsStateData } from '../../../tabs-types';
 
 export function useTabNavEvent(ctx: SetupContext): UseTabNavEvent {
-  const onTabRemove = (item: any, ev: MouseEvent) => {
+  const onTabRemove = (item: TabsStateData, ev: MouseEvent) => {
     ev.stopPropagation();
     ctx.emit('tab-remove', item, ev);
     ctx.emit('tab-change', item.id, 'delete');
