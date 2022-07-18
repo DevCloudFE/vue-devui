4e162b19717a5201144484b8bda0467aade8043f
daviForevel <657434047@qq.com>
2022-07-18 09:47:14 +0800

    fix(date-picker-pro): eslint修复

--- packages/devui-vue/devui/date-picker-pro/src/const.ts
+++ packages/devui-vue/devui/date-picker-pro/src/const.ts
@@ -1,13 +1,11 @@
 export const DAY_DURATION = 24 * 60 * 60 * 1000;
 export const yearItemHeight = 30; // 日历面板左侧对应年份一个item高度
 export const calendarItemHeight = 186; // 一个月份的日历面板的高度
-export const yearCalendarItemHeight = 48; // 年选择器日历面板单行的高度
-export const monthCalendarItemHeight = 186; // 月选择器一个年份日历面板单行的高度
-
 export const yearListHeight = 305; // 日期模式下左侧年月虚拟列表的高度
 export const calendarListHeight = 270; // 日期模式下中间月份虚拟列表高度
 
 export const monthPickerHeight = 300; // 月选择器模式下左侧年月虚拟列表的高度
+export const monthCalendarItemHeight = 186; // 月选择器一个年份日历面板单行的高度
 
 export const yearPickerHeight = 186; // 年选择器模式下年份虚拟列表的高度
-export const yearPickerItemHeight = 48; // 年选择器模式下单行年份的高度
+export const yearCalendarItemHeight = 48; // 年选择器日历面板单行的高度
