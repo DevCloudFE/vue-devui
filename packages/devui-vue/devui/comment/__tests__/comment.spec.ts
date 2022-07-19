542c20fb45db8a833a4833e8de4f47fc4a713fc0
gxuud <igaoxian@foxmail.com>
2022-07-19 12:33:35 +0800

    test(comment、tag-input): 单元测试变动

--- packages/devui-vue/devui/comment/__tests__/comment.spec.ts
+++ packages/devui-vue/devui/comment/__tests__/comment.spec.ts
@@ -1,22 +1,37 @@
 import { mount } from '@vue/test-utils';
 import { Comment } from '..';
+import { useNamespace } from '../../shared/hooks/use-namespace';
 
+const ns = useNamespace('comment', true);
 describe('comment test', () => {
-  it('comment init render', async () => {
+  it('comment init render', () => {
+    const wrapper = mount(Comment);
+    expect(wrapper.find(ns.e('avatar')).exists()).toBeTruthy();
+    expect(wrapper.find(ns.e('head')).exists()).toBeTruthy();
+    expect(wrapper.find(ns.e('author')).exists()).toBeTruthy();
+    expect(wrapper.find(ns.e('datetime')).exists()).toBeTruthy();
+    expect(wrapper.find(ns.e('content')).exists()).toBeTruthy();
+  });
+  it('comment render slots', () => {
     const wrapper = mount(Comment, {
       slots: {
-        actions: 'Comment Actions',
-        author: 'Comment Author',
-        avatar: 'Comment Avatar',
-        content: 'Comment Content',
-        datetime: 'Comment Datetime',
+        avatar: '<label class="slots-avatar"></label>',
+        author: '<label class="slots-author"></label>r',
+        datetime: '<label class="slots-datetime"></label>',
+        default: '<label class="slots-default"></label>',
+        actions: '<label class="slots-actions"></label>',
       },
     });
-    expect(wrapper.find('.devui-comment').exists()).toBe(true);
-    expect(wrapper.find('.devui-comment-actions').text()).toBe('Comment Actions');
-    expect(wrapper.find('.devui-comment-author').text()).toBe('Comment Author');
-    expect(wrapper.find('.devui-comment-avatar').text()).toBe('Comment Avatar');
-    expect(wrapper.find('.devui-comment-content').text()).toBe('Comment Content');
-    expect(wrapper.find('.devui-comment-datetime').text()).toBe('Comment Datetime');
+
+    expect(wrapper.find('.slots-avatar').exists()).toBeTruthy();
+    expect(wrapper.find('.slots-author').exists()).toBeTruthy();
+    expect(wrapper.find('.slots-datetime').exists()).toBeTruthy();
+    expect(wrapper.find('.slots-default').exists()).toBeTruthy();
+    expect(wrapper.find('.slots-actions').exists()).toBeTruthy();
+
+    expect(wrapper.find(ns.e('avatar')).exists()).toBeFalsy();
+    expect(wrapper.find(ns.e('author')).exists()).toBeFalsy();
+    expect(wrapper.find(ns.e('datetime')).exists()).toBeFalsy();
+
   });
 });
