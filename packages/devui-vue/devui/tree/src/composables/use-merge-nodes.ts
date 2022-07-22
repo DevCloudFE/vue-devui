95488f62e41ce6bd64a4d9464c475b8443e60f85
daviForevel <657434047@qq.com>
2022-07-22 15:01:53 +0800

    fix(tree): 修复use-core执行两次的问题

--- packages/devui-vue/devui/tree/src/composables/use-merge-nodes.ts
+++ packages/devui-vue/devui/tree/src/composables/use-merge-nodes.ts
@@ -4,7 +4,7 @@ import { IInnerTreeNode, IUseCore, IUseMergeNodes } from './use-tree-types';
 
 export default function () {
   return function useMergeNodes(data: Ref<IInnerTreeNode[]>, core: IUseCore): IUseMergeNodes {
-    const { setNodeValue, getChildren } = core;
+    const { setNodeValue, getChildren, clearNodeMap } = core;
     const { removeNode } = useOperate()(data, core);
 
     const mergeTreeNodes = () => {
@@ -31,6 +31,8 @@ export default function () {
         }
       };
 
+      // 合并节点时，getChildren只是获取直接子节点，需要将原来的nodeMap清空。（nodeMap中报存的是所有子节点）
+      clearNodeMap();
       data.value
         .filter((item) => item.level === 1)
         .forEach((item) => {
