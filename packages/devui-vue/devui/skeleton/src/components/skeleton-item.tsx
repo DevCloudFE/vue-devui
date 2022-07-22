01b3e9afea24485f4d6fdec15f4d2555d2469791
wangyupei <2311595895@qq.com>
2022-07-22 15:41:01 +0800

    refactor(Skeleton): 重构Skeleton组件

--- packages/devui-vue/devui/skeleton/src/components/skeleton-item.tsx
+++ packages/devui-vue/devui/skeleton/src/components/skeleton-item.tsx
@@ -1,95 +1,16 @@
-import { defineComponent } from 'vue';
-import { itemProps, ItemProps } from './skeleton-item-types';
-import { useNamespace } from '../../../shared/hooks/use-namespace';
+import { defineComponent, toRefs } from 'vue';
+import { skeletonItemProps, SkeletonItemProps } from './skeleton-item-types';
+import { useSkeletonItem } from '../composables/use-skeleton-item';
+import { ImageIcon } from './image-icon';
 import './skeleton-item.scss';
 
 export default defineComponent({
   name: 'DSkeletonItem',
-  props: itemProps,
-  setup(props: ItemProps, ctx) {
-    const { slots } = ctx;
-    const ns = useNamespace('skeleton');
+  props: skeletonItemProps,
+  setup(props: SkeletonItemProps) {
+    const { variant } = toRefs(props);
+    const { classes } = useSkeletonItem(props);
 
-    function renderAnimate(isAnimated) {
-      return isAnimated ? ns.e('animated') : '';
-    }
-
-    function renderShapeParagraph(rowNum, rowWidth, round) {
-      const arr = [];
-
-      function pushIntoArray(type) {
-        for (let index = 0; index < rowNum; index++) {
-          arr.push({ width: type });
-        }
-      }
-      (function handleRowWidth() {
-        if (rowWidth instanceof Array) {
-          for (let index = 0; index < rowNum; index++) {
-            if (rowWidth[index]) {
-              switch (typeof rowWidth[index]) {
-              case 'string':
-                arr.push({ width: rowWidth[index] });
-                break;
-              case 'number':
-                arr.push({ width: `${rowWidth[index]}px` });
-              }
-            } else {
-              arr.push({ width: 1 });
-            }
-          }
-        } else {
-          switch (typeof rowWidth) {
-          case 'string':
-            pushIntoArray(rowWidth);
-            break;
-          case 'number':
-            pushIntoArray(`${rowWidth}px`);
-            break;
-          }
-        }
-      })();
-
-      return (
-        <div class={[ns.em('shape', 'paragraph'), renderAnimate(props.animate)]} {...ctx.attrs}>
-          {arr.map((item) => {
-            return <div class={ns.em('shape', 'paragraph-item')} style={round ? 'border-radius: 1em;' : '' + `width: ${item.width}`} />;
-          })}
-        </div>
-      );
-    }
-
-    function renderAvatarStyle(avatarShape) {
-      function renderAvatarShape() {
-        return avatarShape === 'square' ? '' : 'border-radius:50%;';
-      }
-
-      return renderAvatarShape(avatarShape);
-    }
-
-    return () => {
-      if (props.loading && props.shape) {
-        switch (props.shape) {
-        case 'avatar':
-          return (
-            <>
-              <div
-                class={[ns.em('shape', 'avatar'), renderAnimate(props.animate)]}
-                style={renderAvatarStyle(props.avatarShape)}
-                {...ctx.attrs}
-              />
-            </>
-          );
-        case 'paragraph':
-          return <>{renderShapeParagraph(props.row, props.rowWidth, props.round)}</>;
-        default:
-          return (
-            <>
-              <div class={[ns.em('shape', props.shape), renderAnimate(props.animate)]} {...ctx.attrs} />
-            </>
-          );
-        }
-      }
-      return <>{slots.default?.()}</>;
-    };
+    return () => <div class={classes.value}>{variant.value === 'image' && <ImageIcon />}</div>;
   },
 });
