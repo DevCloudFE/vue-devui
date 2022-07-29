03e063550a0f456ee32567442a7a81fae23621d7
zcj <18137693952@163.com>
2022-07-29 21:29:01 +0800

    feat: 增加vite @devui 路径别名配置

--- packages/devui-vue/docs/vite.config.ts
+++ packages/devui-vue/docs/vite.config.ts
@@ -1,11 +1,11 @@
-import path from 'path';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import svgLoader from 'vite-svg-loader';
 
 export default defineConfig({
   resolve: {
-    alias: [{ find: '@devui', replacement: path.resolve(__dirname, '../devui') }],
+    alias: [{ find: '@devui', replacement: resolve(__dirname, '../devui') }],
   },
   plugins: [vueJsx({}), svgLoader()],
   optimizeDeps: {
