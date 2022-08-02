02db9fc5c707a09326c639d79dca10f46cede9eb
GaoNengwWw <gaonenggg@outlook.com>
2022-08-02 12:56:43 +0800

    fix(menu): 删除无用注释

--- packages/devui-vue/devui/menu/src/menu.tsx
+++ packages/devui-vue/devui/menu/src/menu.tsx
@@ -132,7 +132,6 @@ export default defineComponent({
           class={rootClassName.value}
           style={[
             props['collapsed'] ? `width:${props['collapsedIndent'] * 2}px` : `width: ${props['width']}`,
-            // 'white-space: nowrap',
           ]}>
           {ctx.slots.default?.()}
           <SubMenu
