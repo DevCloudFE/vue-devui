11d46a170b45154d9bed07f6a118a11058d044cb
Kagol <kagol@sina.com>
2022-08-05 13:14:18 +0800

    Revert "fix(menu): 溢出容器中router点击无法跳转的问题"
    This reverts commit ed5d4d449ef20aa34aa263cc6c29fbcf30dfaeac.

--- packages/devui-vue/devui/menu/src/components/sub-menu/sub-menu.tsx
+++ packages/devui-vue/devui/menu/src/components/sub-menu/sub-menu.tsx
@@ -43,10 +43,10 @@ export default defineComponent({
       }
     }
     const clickHandle = (e: MouseEvent) => {
-      // e.preventDefault();
+      e.preventDefault();
       e.stopPropagation();
-      const ele = e.target as HTMLElement;
-      console.log(ele);
+      const ele = e.currentTarget as HTMLElement;
+
       if (ele.classList.contains(subMenuClass) && isHorizontal) {
         return;
       }
