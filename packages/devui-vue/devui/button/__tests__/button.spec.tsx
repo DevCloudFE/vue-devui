ff80ea193c18d4b3b4bc67c8f388c18dab407185
qinwencheng <157475034@qq.com>
2022-08-12 01:05:03 +0800

    test(button): 完成颜色测试

--- packages/devui-vue/devui/button/__tests__/button.spec.tsx
+++ packages/devui-vue/devui/button/__tests__/button.spec.tsx
@@ -1,15 +1,25 @@
 import { mount } from '@vue/test-utils';
 import { useNamespace } from '../../shared/hooks/use-namespace';
 import { Button } from '..';
+import type { IButtonVariant, IButtonColor } from '..';
 
 const ns = useNamespace('button', true);
 
 const baseClass = ns.b();
 const solidClass = ns.m('solid');
+const outlineClass = ns.m('outline');
+const textClass = ns.m('text');
 const smClass = ns.m('sm');
 const roundClass = ns.m('round');
 const circleClass = ns.m('circle');
 const iconClass = '.icon';
+const primaryClass = ns.m('primary');
+const secondaryClass = ns.m('secondary');
+const dangerClass = ns.m('danger');
+const genColorClass = (variant: IButtonVariant, color: IButtonColor) => {
+  return `${ns.m(variant)}--${color}`;
+};
+// `${ns.m(props.variant)}--${props.color || defaultColor}`
 
 describe('d-button', () => {
   it('variant', () => {
@@ -18,21 +28,55 @@ describe('d-button', () => {
         return () => {
           return <Button variant="solid">确定</Button>;
         };
-      }
+      },
     });
 
     expect(wrapper.find(solidClass).exists()).toBeTruthy();
   });
 
-  it.todo('props color(primary/secondary/danger) should work well.');
-
-  it('size', () => {
+  it('color', async () => {
+    // 不设置color和variant时，表现为 outline 下的 secondary
     const wrapper = mount({
+      setup() {
+        return () => {
+          return <Button>确定</Button>;
+        };
+      },
+    });
+    expect(wrapper.find(genColorClass('outline', 'secondary')).exists()).toBeTruthy();
+
+    await wrapper.setProps({
+      color: 'primary',
+    });
+    expect(wrapper.find(genColorClass('outline', 'primary')).exists()).toBeTruthy();
+
+    await wrapper.setProps({
+      variant: 'outline',
+      color: 'danger',
+    });
+    expect(wrapper.find(genColorClass('outline', 'danger')).exists()).toBeTruthy();
+
+    await wrapper.setProps({
+      variant: 'solid',
+      color: 'secondary',
+    });
+    expect(wrapper.find(genColorClass('solid', 'secondary')).exists()).toBeTruthy();
+
+    await wrapper.setProps({
+      variant: 'text',
+      color: 'primary',
+    });
+    expect(wrapper.find(genColorClass('text', 'primary')).exists()).toBeTruthy();
+
+
+  });
+  it('size', () => {
+    const wrapper = mount(Button, {
       setup() {
         return () => {
           return <Button size="sm">确定</Button>;
         };
-      }
+      },
     });
     expect(wrapper.find(smClass).exists()).toBeTruthy();
   });
@@ -43,7 +87,7 @@ describe('d-button', () => {
         return () => {
           return <Button icon="like">确定</Button>;
         };
-      }
+      },
     });
 
     expect(wrapper.find(iconClass).exists()).toBeTruthy();
@@ -55,7 +99,7 @@ describe('d-button', () => {
         return () => {
           return <Button type="submit">确定</Button>;
         };
-      }
+      },
     });
     expect(wrapper.find('button').attributes('type')).toBe('submit');
   });
@@ -67,7 +111,7 @@ describe('d-button', () => {
         return () => {
           return <Button onClick={handleClick}>确定</Button>;
         };
-      }
+      },
     });
     await wrapper.find(baseClass).trigger('click');
     expect(handleClick).toBeCalled();
@@ -79,9 +123,13 @@ describe('d-button', () => {
     const wrapper = mount({
       setup() {
         return () => {
-          return <Button loading={true} onClick={handleClick}>确定</Button>;
+          return (
+            <Button loading={true} onClick={handleClick}>
+              确定
+            </Button>
+          );
         };
-      }
+      },
     });
     await wrapper.trigger('click');
     expect(handleClick).not.toBeCalled();
@@ -92,9 +140,13 @@ describe('d-button', () => {
     const wrapper = mount({
       setup() {
         return () => {
-          return <Button disabled onClick={handleClick}>确定</Button>;
+          return (
+            <Button disabled onClick={handleClick}>
+              确定
+            </Button>
+          );
         };
-      }
+      },
     });
     await wrapper.trigger('click');
     expect(handleClick).not.toBeCalled();
@@ -107,7 +159,7 @@ describe('d-button', () => {
         return () => {
           return <Button>{btnText}</Button>;
         };
-      }
+      },
     });
     expect(wrapper.text()).toEqual(btnText);
   });
@@ -118,7 +170,7 @@ describe('d-button', () => {
         return () => {
           return <Button shape="round">确定</Button>;
         };
-      }
+      },
     });
     expect(wrapper.find(roundClass).exists()).toBeTruthy();
   });
@@ -129,7 +181,7 @@ describe('d-button', () => {
         return () => {
           return <Button shape="circle">确定</Button>;
         };
-      }
+      },
     });
     expect(wrapper.find(circleClass).exists()).toBeTruthy();
   });
