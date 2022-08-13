85f794eacdb386116258a538c30dfba383ea5b64
zcj <18137693952@163.com>
2022-08-13 14:49:41 +0800

    fix(icon): icon 字体文件加载失败，导致icon组件显示异常

--- packages/devui-vue/docs/.vitepress/config/head.ts
+++ packages/devui-vue/docs/.vitepress/config/head.ts
@@ -1,5 +1,14 @@
 import { HeadConfig } from 'vitepress';
 
-const head: HeadConfig[] = [['link', { rel: 'icon', type: 'image/svg+xml', href: '/assets/logo.svg' }]];
+const head: HeadConfig[] = [
+  ['link', { rel: 'icon', type: 'image/svg+xml', href: '/assets/logo.svg' }],
+  [
+    'link',
+    {
+      rel: 'stylesheet',
+      href: 'https://unpkg.com/@devui-design/icons/icomoon/devui-icon.css',
+    },
+  ],
+];
 
 export default head;
