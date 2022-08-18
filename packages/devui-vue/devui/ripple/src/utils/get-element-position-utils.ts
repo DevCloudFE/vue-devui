c3e71fed574d5a9a15f97aa3ce5a495b00e12620
ERKELSOT <1256029807@qq.com>
2022-08-18 22:14:33 +0800

    refactor(ripple): solve ripple width scale

--- packages/devui-vue/devui/ripple/src/utils/get-element-position-utils.ts
+++ packages/devui-vue/devui/ripple/src/utils/get-element-position-utils.ts
@@ -1,22 +1,27 @@
-
 // Pythagorean theorem
-export function magnitude(x1: number, y1: number, x2: number, y2: number): number {
+export function getPythagoreanDistance(x1: number, y1: number, x2: number, y2: number): number {
   const deltaX = x1 - x2;
   const deltaY = y1 - y2;
   return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
 }
 
-// 获取点击目标的位置到块级作用域边界的距离
-export function getDistanceToFurthestCorner(x: number, y: number, { width, height }: DOMRect): number {
-  const topLeft = magnitude(x, y, 0, 0);
-  const topRight = magnitude(x, y, width, 0);
-  const bottomLeft = magnitude(x, y, 0, height);
-  const bottomRight = magnitude(x, y, width, height);
-  return Math.max(topLeft, topRight, bottomLeft, bottomRight);
+interface DOMRectValue {
+  x: number;
+  y: number;
+  diameter: number;
 }
-
-// 获取点击距离container元素直角坐标
-export const getRelativePointer = ({ x, y }: PointerEvent, { top, left }: DOMRect): Partial<DOMRect> => ({
-  x: x - left,
-  y: y - top,
-});
+// 获取点击目标的位置到块级作用域直线边界的距离
+export function getDistanceToFurthestCorner(event: PointerEvent, { width, height, left, top }: DOMRect): DOMRectValue {
+  const x = event.clientX - left;
+  const y = event.clientY - top;
+  const topLeft = getPythagoreanDistance(x, y, 0, 0);
+  const topRight = getPythagoreanDistance(x, y, width, 0);
+  const bottomLeft = getPythagoreanDistance(x, y, 0, height);
+  const bottomRight = getPythagoreanDistance(x, y, width, height);
+  const diameter = Math.max(topLeft, topRight, bottomLeft, bottomRight);
+  return {
+    x,
+    y,
+    diameter,
+  };
+}
--- packages/devui-vue/devui/ripple/src/v-ripple.ts
+++ packages/devui-vue/devui/ripple/src/v-ripple.ts
@@ -1,10 +1,10 @@
 import { createContainer } from './utils/create-container-element';
 import { createRippleElement } from './utils/create-ripple-element';
-import { getDistanceToFurthestCorner, getRelativePointer } from './utils/get-element-position-utils';
+import { getDistanceToFurthestCorner } from './utils/get-element-position-utils';
 import { decrementRippleCount, deleteRippleCount, getRippleCount, incrementRippleCount } from './utils/ripple-count';
 import { IRippleDirectiveOptions } from './options';
 // Ensure that the current element can be completely overwritten
-// const MULTIPLE_NUMBER = 1;
+const MULTIPLE_NUMBER = 1.05;
 const ripple = (event: PointerEvent, el: HTMLElement, options: IRippleDirectiveOptions): void => {
   // Get Click element
   const rect = el.getBoundingClientRect();
@@ -13,11 +13,10 @@ const ripple = (event: PointerEvent, el: HTMLElement, options: IRippleDirectiveO
   // Create a hole ripple element 覆盖一个容器在el上面
   const rippleContainer = createContainer(computedStyles);
   // Get the current mouse click location
-  const { x, y } = getRelativePointer(event, rect);
-  // 利用勾股定理获取鼠标点击处到el四个角的直线距离， size 就是我们点击的位置距离整个el最远的点
-  // const size = MULTIPLE_NUMBER * getDistanceToFurthestCorner(x, y, rect);
-  const size = getDistanceToFurthestCorner(x, y, rect);
-  const rippleEl = createRippleElement(x, y, size, options, rect);
+  // 利用勾股定理获取鼠标点击处到el四个角的直线距离， diameter 就是我们点击的位置距离整个el最远的点
+  // const { diameter, x, y } = MULTIPLE_NUMBER * getDistanceToFurthestCorner(event, rect);
+  const { diameter, x, y } = getDistanceToFurthestCorner(event, rect);
+  const rippleEl = createRippleElement(x, y, diameter * MULTIPLE_NUMBER, options, rect);
   // 原始位置
   let originalPositionValue = '';
   // 是否需要移除ripple Do you need to remove ripple
@@ -73,14 +72,14 @@ const ripple = (event: PointerEvent, el: HTMLElement, options: IRippleDirectiveO
   }
 
   rippleContainer.appendChild(rippleEl);
-  el.appendChild(rippleContainer);
+  el.appendChild(rippleEl);
 
   document.addEventListener('pointerup', releaseRipple);
   document.addEventListener('pointercancel', releaseRipple);
 
   token = setTimeout(() => {
     document.removeEventListener('pointercancel', cancelRipple);
-    rippleEl.style.transform = `translate(-50%,-50%) scale(2)`;
+    rippleEl.style.transform = `translate(-50%,-50%) scale(1)`;
     rippleEl.style.opacity = `${options.finalOpacity}`;
     setTimeout(() => releaseRipple(), options.duration);
   }, options.delay);
