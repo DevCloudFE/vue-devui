0d4a008f48602c0e036bff22556258cfbec2e527
ERKELSOT <1256029807@qq.com>
2022-08-28 21:21:03 +0800

    chore(ripple): optimize code

--- packages/devui-vue/devui/ripple/src/utils/create-ripple-element.ts
+++ packages/devui-vue/devui/ripple/src/utils/create-ripple-element.ts
@@ -10,7 +10,6 @@ export const createRippleElement = (
   const rippleElement = document.createElement('div');
   rippleElement.style.position = 'absolute';
   rippleElement.style.width = options.center ? `${Math.sqrt(rect.width * rect.width + rect.height * rect.height)}px` : `${size * 2}px`;
-  // rippleElement.style.height = options.center ? `${size}px` : `${size}px`;
   rippleElement.style.height = options.center ? `${Math.sqrt(rect.width * rect.width + rect.height * rect.height)}px` : `${size * 2}px`;
   rippleElement.style.top = options.center ? `${rect.height / 2}px` : `${y}px`;
   rippleElement.style.left = options.center ? `${rect.width / 2}px` : `${x}px`;
@@ -19,7 +18,7 @@ export const createRippleElement = (
   rippleElement.style.borderRadius = '50%';
   rippleElement.style.opacity = `${options.initialOpacity}`;
   rippleElement.style.transform = `translate(-50%,-50%) scale(0)`;
-  rippleElement.style.transition = `transform ${options.duration / 1000}s   cubic-bezier(0, 0.5, 0.25, 1)
+  rippleElement.style.transition = `transform ${options.duration / 1000}s cubic-bezier(0, 0.5, 0.25, 1)
   , opacity ${options.duration / 1000}s
   cubic-bezier(0.0, 0, 0.2, 1)
   `;
