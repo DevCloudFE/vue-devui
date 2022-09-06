14871bcd79d06a56715fb0184f07cb07357c1a6a
Kagol <kagol@sina.com>
2022-09-06 21:40:23 +0800

    fix: fix doc 404 close #1310

---
+++ packages/devui-vue/docs/.vitepress/devui-theme/const.ts
@@ -0,0 +1,5 @@
+export const ZH_CN = 'zh-CN';
+export const EN_US = 'en-US';
+export const DEFAULT_LANG = ZH_CN;
+export const LANG_KEY = 'vue-devui-preferred-lang';
+export const CURRENT_LANG = localStorage.getItem(LANG_KEY) || DEFAULT_LANG;
