4f8726a3315ad08840b73206f5d9795931d81894
zcj <18137693952@163.com>
2022-09-21 11:22:39 +0800

    docs(update): vitepress 配置更新

--- packages/devui-vue/docs/.vitepress/config/index.ts
+++ packages/devui-vue/docs/.vitepress/config/index.ts
@@ -1,46 +1,58 @@
-import { defineConfig } from 'vitepress'
-import sidebar from './sidebar'
-import head from './head'
-import nav from './nav'
-import markdown from './markdown'
-import lang from './lang'
+import { defineConfig } from 'vitepress';
+import sidebar from './sidebar';
+import head from './head';
+import nav from './nav';
+import markdown from './markdown';
+import lang from './lang';
 
 const config = defineConfig({
   base: '/',
   title: 'Vue DevUI',
   description: 'Vue DevUI 组件库',
+  appearance: false, // 是否启用暗模式
   head,
   markdown,
   locales: {
     '/': {
       lang: 'zh-CN',
-      label: '简体中文'
+      label: '简体中文',
     },
     '/en-US': {
       lang: 'en-US',
-      label: 'English'
-    }
+      label: 'English',
+    },
   },
   themeConfig: {
     sidebar,
     nav,
+    // outline: [2, 6], todo 大纲中的标题等级 新版本启用
+    outlineTitle: '快速前往',
+    algolia: {},
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/DevCloudFE/vue-devui' },
+    ],
     demoblock: lang,
     logo: '../../assets/logo.svg',
     locales: {
       '/': {
         lang: 'zh-CN',
-        label: '简体中文'
+        label: '简体中文',
       },
       '/en-US': {
         lang: 'en-US',
-        label: 'English'
-      }
+        label: 'English',
+      },
     },
     footer: {
+      // todo 新版本中启用
+      // license: {
+      //   text: 'MIT License',
+      //   link: 'https://opensource.org/licenses/MIT'
+      // },
       message: 'MIT Licensed',
-      copyright: 'Copyright © 2021-present DevUI'
-    }
-  }
-})
+      copyright: `Copyright © 2021-${new Date().getFullYear()} DevUI`,
+    },
+  },
+});
 
-export default config
+export default config;
--- packages/devui-vue/docs/.vitepress/theme/index.ts
+++ packages/devui-vue/docs/.vitepress/theme/index.ts
@@ -1,16 +1,28 @@
-import DevUI from '../../../devui/vue-devui'
-import Locale from '../../../devui/locale'
+import DevUI from '../../../devui/vue-devui';
+import Locale from '../../../devui/locale';
+import '@devui-design/icons/icomoon/devui-icon.css';
+
+import { h } from 'vue';
 import Theme from 'vitepress/theme';
 import './styles/index.scss';
-import 'vitepress-theme-demoblock/theme/styles/index.css'
-import { registerComponents } from './register-components.js'
-import { insertBaiduScript } from './insert-baidu-script'
+
+import 'vitepress-theme-demoblock/theme/styles/index.css';
+import { registerComponents } from './register-components.js';
+import { insertBaiduScript } from './insert-baidu-script';
+
+import ThemePicker from './components/ThemePicker.vue';
+
 
 export default {
   ...Theme,
+  Layout() {
+    return h(Theme.Layout, null, {
+      'nav-bar-content-after': () => h(ThemePicker),
+    });
+  },
   enhanceApp({ app }) {
-    app.use(Locale).use(DevUI)
-    registerComponents(app)
-    insertBaiduScript()
-  }
-}
+    app.use(Locale).use(DevUI);
+    registerComponents(app);
+    insertBaiduScript();
+  },
+};
