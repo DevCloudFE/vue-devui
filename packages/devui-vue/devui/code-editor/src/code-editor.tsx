36769c571d794743db557164f01a080d3b87c08b
huaweidevcloud <44854357+huaweidevcloud@users.noreply.github.com>
2023-07-18 10:48:26 +0800

    feat: 添加codeEditor组件 (#1587)

---
+++ packages/devui-vue/devui/code-editor/src/code-editor.tsx
@@ -0,0 +1,16 @@
+import { defineComponent } from 'vue';
+import type { SetupContext } from 'vue';
+import { codeEditorProps, CodeEditorProps } from './code-editor-types';
+import { useCodeEditor } from './composables/use-code-editor';
+import './code-editor.scss'
+
+export default defineComponent({
+    name: 'DpCodeEditor',
+    props: codeEditorProps,
+    emits: ['update: modelValue', 'afterEditorInit', 'click'],
+    setup(props: codeEditorProps, ctx: SetupContext) {
+        const { editorEl } = useCodeEditor(props, ctx);
+
+        return () => <div ref={editorEl} class="devui-code-editor"></div>
+    }
+})
