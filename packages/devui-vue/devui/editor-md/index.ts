620b8d4836d7f43c137007c9f817d367a095cf9b
huaweidevcloud <devcloudmobile@huawei.com>
2023-07-21 09:21:54 +0800

    fix: 修复冲突

--- packages/devui-vue/devui/editor-md/index.ts
+++ packages/devui-vue/devui/editor-md/index.ts
@@ -1,14 +1,14 @@
 import type { App } from 'vue';
 import EditorMd from './src/editor-md';
-import MdRender from './src/components/md-render'
+import MdRender from './src/components/md-render';
 export * from './src/editor-md-types';
 
 export { EditorMd, MdRender };
 
 export default {
   title: 'Markdown MD编辑器',
-  category: '数据录入',
-  status: '80%',
+  category: '演进中',
+  status: '100%',
   install(app: App): void {
     app.component(EditorMd.name, EditorMd);
     app.component(MdRender.name, MdRender);
