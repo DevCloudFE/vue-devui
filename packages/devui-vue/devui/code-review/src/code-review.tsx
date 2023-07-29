73629060fbe2e7f70cd4b9d6af6eedaaa339404a
行言 <2311595895@qq.com>
2023-07-29 09:50:25 +0800

    fix: 修改版本号 (#1626)

--- packages/devui-vue/devui/code-review/src/code-review.tsx
+++ packages/devui-vue/devui/code-review/src/code-review.tsx
@@ -5,7 +5,6 @@ import { codeReviewProps } from './code-review-types';
 import type { CodeReviewProps } from './code-review-types';
 import { useNamespace } from '../../shared/hooks/use-namespace';
 import { useCodeReview } from './composables/use-code-review';
-import 'diff2html/bundles/css/diff2html.min.css';
 import './code-review.scss';
 
 export default defineComponent({
