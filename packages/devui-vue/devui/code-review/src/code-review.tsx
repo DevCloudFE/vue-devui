8ff1946d6e0a0159a59a2dcb8dc72bc619d4c52d
行言 <2311595895@qq.com>
2023-08-09 16:59:57 +0800

    feat(CodeReview): line-by-line模式支持评论功能 (#1645)

--- packages/devui-vue/devui/code-review/src/code-review.tsx
+++ packages/devui-vue/devui/code-review/src/code-review.tsx
@@ -1,24 +1,54 @@
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted } from 'vue';
 import type { SetupContext } from 'vue';
 import CodeReviewHeader from './components/code-review-header';
+import { CommentIcon } from './components/code-review-icons';
 import { codeReviewProps } from './code-review-types';
 import type { CodeReviewProps } from './code-review-types';
 import { useNamespace } from '../../shared/hooks/use-namespace';
 import { useCodeReview } from './composables/use-code-review';
+import { useCodeReviewComment } from './composables/use-code-review-comment';
 import './code-review.scss';
 
 export default defineComponent({
   name: 'DCodeReview',
   props: codeReviewProps,
-  emits: ['foldChange'],
+  emits: ['foldChange', 'addComment', 'afterViewInit'],
   setup(props: CodeReviewProps, ctx: SetupContext) {
     const ns = useNamespace('code-review');
-    const { renderHtml, isFold } = useCodeReview(props, ctx);
+    const { renderHtml, isFold, reviewContentRef, toggleFold } = useCodeReview(props, ctx);
+    const {
+      commentLeft,
+      commentTop,
+      onMouseEnter,
+      onMouseMove,
+      onMouseleave,
+      onCommentMouseLeave,
+      onCommentIconClick,
+      insertComment,
+      removeComment,
+    } = useCodeReviewComment(reviewContentRef, ctx);
+
+    onMounted(() => {
+      ctx.emit('afterViewInit', { toggleFold, insertComment, removeComment });
+    });
 
     return () => (
       <div class={ns.b()}>
         <CodeReviewHeader onClick={() => (isFold.value = !isFold.value)} />
-        <div class={[ns.e('content'), { 'hide-content': isFold.value }]} v-html={renderHtml.value}></div>
+        <div
+          class={[ns.e('content'), { 'hide-content': isFold.value }]}
+          v-html={renderHtml.value}
+          ref={reviewContentRef}
+          onMouseenter={onMouseEnter}
+          onMousemove={onMouseMove}
+          onMouseleave={onMouseleave}></div>
+        <div
+          class="comment-icon"
+          style={{ left: commentLeft.value + 'px', top: commentTop.value + 'px' }}
+          onClick={onCommentIconClick}
+          onMouseleave={onCommentMouseLeave}>
+          <CommentIcon />
+        </div>
       </div>
     );
   },
---
+++ packages/devui-vue/devui/code-review/src/const.ts
@@ -0,0 +1,2 @@
+export const ExpandLineReg = /^@@ -(\d+),(\d+) \+(\d+),(\d+) @@/;
+export const FirstLineReg = /^@@ -[0,1](?!\d)/;
