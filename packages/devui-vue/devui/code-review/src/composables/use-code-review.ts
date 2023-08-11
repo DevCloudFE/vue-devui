744c4e36bce822dbc98173cf6099426aba3b0496
行言 <2311595895@qq.com>
2023-08-11 09:42:26 +0800

    feat(CodeReview): 增加代码高亮功能 (#1649)

--- packages/devui-vue/devui/code-review/src/composables/use-code-review.ts
+++ packages/devui-vue/devui/code-review/src/composables/use-code-review.ts
@@ -1,56 +1,35 @@
-import { toRefs, ref, provide, onBeforeMount, watch, nextTick } from 'vue';
-import type { SetupContext } from 'vue';
+import { toRefs, ref, watch, nextTick } from 'vue';
+import type { SetupContext, Ref } from 'vue';
+import type { DiffFile } from 'diff2html/lib/types';
 import * as Diff2Html from 'diff2html';
-import { CodeReviewInjectionKey } from '../code-review-types';
+import { Diff2HtmlUI } from 'diff2html/lib/ui/js/diff2html-ui';
 import type { CodeReviewProps } from '../code-review-types';
 import { useCodeReviewExpand } from './use-code-review-expand';
 
 export function useCodeReview(props: CodeReviewProps, ctx: SetupContext) {
-  const { diff, fold, outputFormat, expandAllThreshold } = toRefs(props);
+  const { diff, outputFormat, expandAllThreshold } = toRefs(props);
   const renderHtml = ref('');
-  const isFold = ref(fold.value);
   const reviewContentRef = ref();
-  const diffFile = Diff2Html.parse(diff.value);
+  const diffFile: Ref<DiffFile[]> = ref([]);
   const { insertExpandButton } = useCodeReviewExpand(reviewContentRef, expandAllThreshold.value, outputFormat.value);
 
   const initDiffContent = () => {
-    renderHtml.value = Diff2Html.html(diffFile, {
-      drawFileList: false,
-      matching: 'lines',
-      outputFormat: outputFormat.value,
-      /* rawTemplates: {
-        'side-by-side-file-diff': '<table><tbody>{{{diffs.left}}}{{{diffs.right}}}</tbody></table>',
-      }, */
+    diffFile.value = Diff2Html.parse(diff.value);
+    nextTick(() => {
+      const diff2HtmlUi = new Diff2HtmlUI(reviewContentRef.value, diff.value, {
+        drawFileList: false,
+        matching: 'lines',
+        outputFormat: outputFormat.value,
+        highlight: true,
+      });
+      diff2HtmlUi.draw();
+      diff2HtmlUi.highlightCode();
+      insertExpandButton();
+      ctx.emit('contentRefresh', JSON.parse(JSON.stringify(diffFile.value)));
     });
-    nextTick(insertExpandButton);
   };
 
-  const toggleFold = (status?: boolean) => {
-    if (status !== undefined) {
-      isFold.value = status;
-    } else {
-      isFold.value = !isFold.value;
-    }
-  };
+  watch(diff, initDiffContent, { immediate: true });
 
-  watch(fold, (val) => {
-    isFold.value = val;
-  });
-
-  watch(isFold, () => {
-    if (!isFold.value && renderHtml.value === '') {
-      initDiffContent();
-    }
-    ctx.emit('foldChange', isFold.value);
-  });
-
-  onBeforeMount(() => {
-    if (!isFold.value) {
-      initDiffContent();
-    }
-  });
-
-  provide(CodeReviewInjectionKey, { reviewContentRef, diffInfo: diffFile[0], isFold, rootCtx: ctx });
-
-  return { renderHtml, isFold, reviewContentRef, toggleFold };
+  return { renderHtml, reviewContentRef, diffFile };
 }
