2a1c8ceb1d3591fb9248b64285eb3b8035a0d7af
GreatZP <greatzp@greatzp.cn>
2023-08-14 14:13:57 +0800

    fix(CodeReview): 解决网站构建失败问题

--- packages/devui-vue/devui/code-review/src/composables/use-code-review.ts
+++ packages/devui-vue/devui/code-review/src/composables/use-code-review.ts
@@ -2,6 +2,7 @@ import { toRefs, ref, watch, nextTick } from 'vue';
 import type { SetupContext, Ref } from 'vue';
 import type { DiffFile } from 'diff2html/lib/types';
 import * as Diff2Html from 'diff2html';
+import { inBrowser } from '../../../shared/utils/common-var';
 import type { CodeReviewProps } from '../code-review-types';
 import { useCodeReviewExpand } from './use-code-review-expand';
 import { parseDiffCode } from '../utils';
@@ -16,8 +17,10 @@ export function useCodeReview(props: CodeReviewProps, ctx: SetupContext) {
   const initDiffContent = () => {
     diffFile.value = Diff2Html.parse(diff.value);
     nextTick(() => {
-      parseDiffCode(reviewContentRef.value, diff.value, outputFormat.value);
-      insertExpandButton();
+      if (inBrowser) {
+        parseDiffCode(reviewContentRef.value, diff.value, outputFormat.value);
+        insertExpandButton();
+      }
       ctx.emit('contentRefresh', JSON.parse(JSON.stringify(diffFile.value)));
     });
   };
