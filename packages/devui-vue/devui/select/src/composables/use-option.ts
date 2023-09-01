3bf3ede306f0bc0c543d014cde6fac6c629dead1
GreatZP <greatzp@greatzp.cn>
2023-09-01 18:30:14 +0800

    fix(select): revert refactor to fix dropdown flexable

--- packages/devui-vue/devui/select/src/composables/use-option.ts
+++ packages/devui-vue/devui/select/src/composables/use-option.ts
@@ -2,7 +2,7 @@ import { computed, inject, onBeforeMount, onBeforeUnmount, ref } from 'vue';
 import { OptionProps, UseOptionReturnType } from '../select-types';
 import { SELECT_TOKEN, OPTION_GROUP_TOKEN } from '../const';
 import { className } from '../utils';
-import { useNamespace } from '@devui/shared/utils';
+import { useNamespace } from '../../../shared/hooks/use-namespace';
 export default function useOption(props: OptionProps): UseOptionReturnType {
   const ns = useNamespace('select');
   const select = inject(SELECT_TOKEN, null);
@@ -22,17 +22,16 @@ export default function useOption(props: OptionProps): UseOptionReturnType {
     }
   });
 
-  const isDisabled = computed(() => props.disabled || (optionGroup?.disabled ? true : false));
-
   const optionItem = computed(() => {
     return {
       name: props.name || props.value + '' || '',
       value: props.value,
       create: props.create,
       _checked: false,
-      disabled: isDisabled.value,
     };
   });
+
+  const isDisabled = computed(() => props.disabled || (optionGroup?.disabled ? true : false));
 
   const isObjectOption = ref(!!props.name);
 
@@ -40,8 +39,6 @@ export default function useOption(props: OptionProps): UseOptionReturnType {
     return className(ns.e('item'), {
       active: isOptionSelected.value,
       disabled: isDisabled.value,
-      [ns.em('item', 'sm')]: select?.selectSize === 'sm',
-      [ns.em('item', 'lg')]: select?.selectSize === 'lg',
     });
   });
 
