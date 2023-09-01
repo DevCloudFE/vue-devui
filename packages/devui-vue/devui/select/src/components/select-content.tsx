9e88ef3ee08c7112b5d6276d538fe2b592acfda4
GreatZP <greatzp@greatzp.cn>
2023-09-01 18:32:13 +0800

    fix(select): revert refactor to fix dropdown flexable (#1723)

--- packages/devui-vue/devui/select/src/components/select-content.tsx
+++ packages/devui-vue/devui/select/src/components/select-content.tsx
@@ -1,17 +1,16 @@
 import { defineComponent, inject, computed, withModifiers } from 'vue';
-import type { SetupContext } from 'vue';
 import AlertCloseIcon from '../../../alert/src/components/alert-close-icon';
 import SelectArrowIcon from './select-arrow-icon';
 import { Tag, SizeType } from '../../../tag';
 import { Popover } from '../../../popover';
-import { useNamespace } from '@devui/shared/utils';
+import { useNamespace } from '../../../shared/hooks/use-namespace';
 import useSelectContent from '../composables/use-select-content';
 import { OptionObjectItem } from '../select-types';
 import { FORM_ITEM_TOKEN } from '../../../form';
 
 export default defineComponent({
   name: 'SelectContent',
-  setup(_, ctx: SetupContext) {
+  setup() {
     const formItemContext = inject(FORM_ITEM_TOKEN, undefined);
     const ns = useNamespace('select');
     const clearCls = computed(() => ({
@@ -25,27 +24,18 @@ export default defineComponent({
     const multipleCls = ns.e('multiple');
     const multipleInputCls = ns.em('multiple', 'input');
     const {
-      singleInputRef,
       searchQuery,
-      singleSearchKey,
       selectedData,
       isSelectDisable,
       isSupportCollapseTags,
       isDisabledTooltip,
       isReadOnly,
-      isSupportFilter,
       selectionCls,
       inputCls,
       tagSize,
       placeholder,
-      singlePlaceholder,
-      singlePlaceholderWidth,
       isMultiple,
-      isPlaceholderDark,
       displayInputValue,
-      onSingleInputWrapClick,
-      onMultipleClick,
-      onArrowClick,
       handleClear,
       tagDelete,
       onFocus,
@@ -53,33 +43,18 @@ export default defineComponent({
       queryFilter,
     } = useSelectContent();
 
-    const clearSingleSearchKey = () => {
-      singleSearchKey.value = '';
-    };
-
-    const clearMultipleSearchKey = () => {
-      searchQuery.value = '';
-    };
-
-    ctx.expose({
-      clearSingleSearchKey,
-      clearMultipleSearchKey,
-    });
-
     return () => {
       return (
         <div class={selectionCls.value}>
           {isMultiple.value ? (
-            <div class={multipleCls} onClick={onMultipleClick}>
+            <div class={multipleCls}>
               {!isSupportCollapseTags.value &&
                 selectedData.value.length >= 1 &&
                 selectedData.value.map((item: OptionObjectItem) => (
                   <Tag
-                    deletable={!(isSelectDisable.value || item.disabled)}
+                    deletable
                     onTagDelete={withModifiers(() => tagDelete(item), ['prevent', 'stop'])}
                     key={item.value}
-                    maxWidth={'78%'}
-                    class={['multiple-tag', { disabled: isSelectDisable.value || item.disabled }]}
                     size={tagSize.value as SizeType}>
                     {item.name}
                   </Tag>
@@ -87,8 +62,6 @@ export default defineComponent({
               {isSupportCollapseTags.value && selectedData.value.length >= 1 && (
                 <Tag
                   deletable
-                  maxWidth={'75%'}
-                  class={'multiple-tag'}
                   onTagDelete={withModifiers(() => tagDelete(selectedData.value[0]), ['prevent', 'stop'])}
                   size={tagSize.value as SizeType}>
                   {selectedData.value[0].name}
@@ -97,7 +70,6 @@ export default defineComponent({
               {isSupportCollapseTags.value && selectedData.value.length > 1 && (
                 <Popover
                   trigger="hover"
-                  auto-update-position
                   disabled={isDisabledTooltip.value}
                   v-slots={{
                     default: () => <Tag size={tagSize.value as SizeType}>{`+${selectedData.value.length - 1}`}</Tag>,
@@ -110,7 +82,6 @@ export default defineComponent({
                                 deletable
                                 onTagDelete={withModifiers(() => tagDelete(item), ['prevent', 'stop'])}
                                 key={item.value}
-                                class="popover-tag"
                                 size={tagSize.value as SizeType}>
                                 {item.name}
                               </Tag>
@@ -122,45 +93,37 @@ export default defineComponent({
               )}
               <div class={multipleInputCls}>
                 <input
-                  v-show={!selectedData.value.length || isSupportFilter.value}
                   ref="input"
                   value={searchQuery.value}
                   type="text"
                   class={inputCls.value}
                   placeholder={placeholder.value}
-                  readonly={isReadOnly.value || !isSupportFilter.value}
+                  readonly={isReadOnly.value}
                   disabled={isSelectDisable.value}
                   onInput={queryFilter}
+                  onFocus={onFocus}
+                  onBlur={onBlur}
                 />
               </div>
             </div>
           ) : (
-            <div class="single-inner-input" onClick={onSingleInputWrapClick}>
-              {!singleSearchKey.value && (
-                <span
-                  class={['input-placeholder', { 'placeholder-dark': isPlaceholderDark.value }]}
-                  style={{ width: singlePlaceholderWidth.value }}
-                  title={displayInputValue.value || singlePlaceholder.value}>
-                  {displayInputValue.value || singlePlaceholder.value}
-                </span>
-              )}
-              <input
-                ref={singleInputRef}
-                type="text"
-                v-model={singleSearchKey.value}
-                class={inputCls.value}
-                readonly={isReadOnly.value}
-                disabled={isSelectDisable.value}
-                onFocus={onFocus}
-                onBlur={onBlur}
-                onInput={queryFilter}
-              />
-            </div>
+            <input
+              ref="input"
+              value={displayInputValue.value}
+              type="text"
+              class={inputCls.value}
+              placeholder={placeholder.value}
+              readonly={isReadOnly.value}
+              disabled={isSelectDisable.value}
+              onFocus={onFocus}
+              onBlur={onBlur}
+              onInput={queryFilter}
+            />
           )}
           <span onClick={handleClear} class={clearCls.value}>
             <AlertCloseIcon />
           </span>
-          <span class={arrowCls.value} onClick={onArrowClick}>
+          <span class={arrowCls.value}>
             <SelectArrowIcon />
           </span>
         </div>
--- packages/devui-vue/devui/select/src/select.tsx
+++ packages/devui-vue/devui/select/src/select.tsx
@@ -6,10 +6,11 @@ import {
   Transition,
   toRefs,
   getCurrentInstance,
-  onBeforeMount,
-  Teleport,
-  withModifiers,
   onMounted,
+  Teleport,
+  watch,
+  withModifiers,
+  onUnmounted,
   nextTick,
 } from 'vue';
 import type { SetupContext } from 'vue';
@@ -18,18 +19,17 @@ import { selectProps, SelectProps, SelectContext } from './select-types';
 import { SELECT_TOKEN } from './const';
 import { Checkbox } from '../../checkbox';
 import Option from './components/option';
-import { useNamespace } from '@devui/shared/utils';
+import { useNamespace } from '../../shared/hooks/use-namespace';
 import SelectContent from './components/select-content';
 import useSelectFunction from './composables/use-select-function';
-import { useSelectMenuSize } from './composables/use-select-menu-size';
 import './select.scss';
 import { createI18nTranslate } from '../../locale/create';
-import { FlexibleOverlay } from '../../overlay';
+import { FlexibleOverlay, Placement } from '../../overlay';
 
 export default defineComponent({
   name: 'DSelect',
   props: selectProps,
-  emits: ['toggle-change', 'value-change', 'update:modelValue', 'focus', 'blur', 'remove-tag', 'clear', 'load-more'],
+  emits: ['toggle-change', 'value-change', 'update:modelValue', 'focus', 'blur', 'remove-tag', 'clear'],
   setup(props: SelectProps, ctx: SetupContext) {
     const app = getCurrentInstance();
     const t = createI18nTranslate('DSelect', app);
@@ -39,6 +39,7 @@ export default defineComponent({
     const {
       selectDisabled,
       selectSize,
+      originRef,
       dropdownRef,
       isOpen,
       selectCls,
@@ -59,15 +60,11 @@ export default defineComponent({
       toggleChange,
       isShowCreateOption,
     } = useSelect(props, selectRef, ctx, focus, blur, isSelectFocus, t);
-    const dropdownContainer = ref();
-    const { originRef, dropdownWidth } = useSelectMenuSize(selectRef, dropdownRef, isOpen);
 
+    const dropdownContainer = ref();
     const scrollbarNs = useNamespace('scrollbar');
     const ns = useNamespace('select');
-    const dropdownCls = {
-      [ns.e('dropdown')]: true,
-      [ns.em('dropdown', 'multiple')]: props.multiple,
-    };
+    const dropdownCls = ns.e('dropdown');
     const listCls = {
       [ns.e('dropdown-list')]: true,
       [scrollbarNs.b()]: true,
@@ -75,9 +72,18 @@ export default defineComponent({
     const dropdownEmptyCls = ns.em('dropdown', 'empty');
     ctx.expose({ focus, blur, toggleChange });
     const isRender = ref<boolean>(false);
+    const position = ref<Placement[]>(['bottom-start', 'top-start']);
+    const dropdownWidth = ref('0');
 
-    onBeforeMount(() => {
-      isRender.value = true;
+    const updateDropdownWidth = () => {
+      dropdownWidth.value = originRef?.value?.clientWidth ? originRef.value.clientWidth + 'px' : '100%';
+    };
+
+    watch(selectRef, (val) => {
+      if (val) {
+        originRef.value = val.$el;
+        updateDropdownWidth();
+      }
     });
 
     const scrollToBottom = () => {
@@ -89,9 +95,16 @@ export default defineComponent({
     };
 
     onMounted(() => {
+      isRender.value = true;
+      updateDropdownWidth();
+      window.addEventListener('resize', updateDropdownWidth);
       nextTick(() => {
         dropdownContainer.value.addEventListener('scroll', scrollToBottom);
       });
+    });
+
+    onUnmounted(() => {
+      window.removeEventListener('resize', updateDropdownWidth);
     });
 
     provide(
@@ -101,10 +114,8 @@ export default defineComponent({
         selectDisabled,
         selectSize,
         isOpen,
-        isSelectFocus,
         selectedOptions,
         filterQuery,
-        dropdownWidth,
         valueChange,
         handleClear,
         updateInjectOptions,
@@ -120,7 +131,7 @@ export default defineComponent({
           class={selectCls.value}
           onClick={withModifiers(() => {
             toggleChange(!isOpen.value);
-          }, [])}>
+          }, ['stop'])}>
           <SelectContent ref={selectRef}></SelectContent>
           <Teleport to="body">
             <Transition name="fade">
@@ -128,19 +139,15 @@ export default defineComponent({
                 v-model={isRender.value}
                 ref={dropdownRef}
                 origin={originRef.value}
+                align="start"
                 offset={4}
-                place-strategy="no-space"
-                position={props.position}
+                position={position.value}
                 style={{
                   visibility: isOpen.value ? 'visible' : 'hidden',
                   'z-index': isOpen.value ? 'var(--devui-z-index-dropdown, 1052)' : -1,
                 }}>
-                <div class={dropdownCls} style={{ width: `${dropdownWidth.value}px`, visibility: isOpen.value ? 'visible' : 'hidden' }}>
-                  <ul
-                    class={listCls}
-                    v-show={!isLoading.value}
-                    style={{ padding: isShowEmptyText.value ? '0' : '12px' }}
-                    ref={dropdownContainer}>
+                <div class={dropdownCls} style={{ width: `${dropdownWidth.value}`, visibility: isOpen.value ? 'visible' : 'hidden' }}>
+                  <ul class={listCls} v-show={!isLoading.value} ref={dropdownContainer}>
                     {isShowCreateOption.value && (
                       <Option value={filterQuery.value} name={filterQuery.value} create>
                         {props.multiple ? <Checkbox modelValue={false} label={filterQuery.value} /> : filterQuery.value}
@@ -152,7 +159,7 @@ export default defineComponent({
                       mergeOptions.value.map((item) => (
                         <Option key={item.value} value={item.value} name={item.name} disabled={isDisabled(item)}>
                           {props.multiple ? (
-                            <Checkbox modelValue={item._checked} label={item.name} disabled={isDisabled(item)} class={'select-checkbox'} />
+                            <Checkbox modelValue={item._checked} label={item.name} disabled={isDisabled(item)} />
                           ) : (
                             item.name || item.value
                           )}
