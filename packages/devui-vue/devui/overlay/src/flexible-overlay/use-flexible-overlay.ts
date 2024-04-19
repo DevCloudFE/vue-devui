06ff1646f0c4550b5ab4a7c80cdad1879a3009b3
行言 <2311595895@qq.com>
2024-04-19 10:16:34 +0800

    feat(Dropdown): 优化展开方向识别逻辑 (#1835) (#1836)

--- packages/devui-vue/devui/overlay/src/flexible-overlay/use-flexible-overlay.ts
+++ packages/devui-vue/devui/overlay/src/flexible-overlay/use-flexible-overlay.ts
@@ -1,7 +1,6 @@
-import { ref, unref, watch, nextTick, onUnmounted } from 'vue';
-import { arrow, autoPlacement, computePosition, offset, shift } from '@floating-ui/dom';
+import { ref, unref, watch, nextTick, onUnmounted, toRefs } from 'vue';
+import { arrow, computePosition, offset, flip } from '@floating-ui/dom';
 import { FlexibleOverlayProps, Placement, Point, UseOverlayFn, EmitEventFn, Rect } from './flexible-overlay-types';
-import { getScrollParent } from './utils';
 
 function adjustArrowPosition(isArrowCenter: boolean, point: Point, placement: Placement, originRect: Rect): Point {
   let { x, y } = point;
@@ -25,9 +24,10 @@ function adjustArrowPosition(isArrowCenter: boolean, point: Point, placement: Pl
 }
 
 export function useOverlay(props: FlexibleOverlayProps, emit: EmitEventFn): UseOverlayFn {
+  const { position, showArrow } = toRefs(props);
   const overlayRef = ref<HTMLElement | undefined>();
   const arrowRef = ref<HTMLElement | undefined>();
-  let originParent = null;
+
   const updateArrowPosition = (arrowEl: HTMLElement, placement: Placement, point: Point, overlayEl: HTMLElement) => {
     const { x, y } = adjustArrowPosition(props.isArrowCenter, point, placement, overlayEl.getBoundingClientRect());
     const staticSide = {
@@ -48,54 +48,46 @@ export function useOverlay(props: FlexibleOverlayProps, emit: EmitEventFn): UseO
     const hostEl = <HTMLElement>props.origin;
     const overlayEl = <HTMLElement>unref(overlayRef.value);
     const arrowEl = <HTMLElement>unref(arrowRef.value);
-    const middleware = [
-      offset(props.offset),
-      autoPlacement({
-        alignment: props.align,
-        allowedPlacements: props.position,
-      }),
-    ];
-    props.showArrow && middleware.push(arrow({ element: arrowEl }));
-    props.shiftOffset !== undefined && middleware.push(shift());
-    if (!overlayEl) {
-      return;
+
+    const [mainPosition, ...fallbackPosition] = position.value;
+    const middleware = [offset(props.offset)];
+    middleware.push(fallbackPosition.length ? flip({ fallbackPlacements: fallbackPosition }) : flip());
+    if (showArrow.value) {
+      middleware.push(arrow({ element: arrowRef.value! }));
     }
     const { x, y, placement, middlewareData } = await computePosition(hostEl, overlayEl, {
       strategy: 'fixed',
+      placement: mainPosition,
       middleware,
     });
     let applyX = x;
     let applyY = y;
-    if (props.shiftOffset !== undefined) {
-      const { x: shiftX, y: shiftY } = middlewareData.shift;
-      shiftX < 0 && (applyX -= props.shiftOffset);
-      shiftX > 0 && (applyX += props.shiftOffset);
-      shiftY < 0 && (applyY -= props.shiftOffset);
-      shiftY > 0 && (applyY += props.shiftOffset);
-    }
     emit('positionChange', placement);
     Object.assign(overlayEl.style, { top: `${applyY}px`, left: `${applyX}px` });
     props.showArrow && updateArrowPosition(arrowEl, placement, middlewareData.arrow, overlayEl);
+  };
+
+  const scrollCallback = (e: Event) => {
+    const scrollElement = e.target as HTMLElement;
+    if (scrollElement?.contains(props.origin?.$el ?? props.origin)) {
+      updatePosition();
+    }
   };
   watch(
     () => props.modelValue,
     () => {
       if (props.modelValue && props.origin) {
-        originParent = getScrollParent(props.origin);
         nextTick(updatePosition);
-        originParent?.addEventListener('scroll', updatePosition);
-        originParent !== window && window.addEventListener('scroll', updatePosition);
+        window.addEventListener('scroll', scrollCallback, true);
         window.addEventListener('resize', updatePosition);
       } else {
-        originParent?.removeEventListener('scroll', updatePosition);
-        originParent !== window && window.removeEventListener('scroll', updatePosition);
+        window.removeEventListener('scroll', scrollCallback, true);
         window.removeEventListener('resize', updatePosition);
       }
     }
   );
   onUnmounted(() => {
-    originParent?.removeEventListener('scroll', updatePosition);
-    originParent !== window && window.removeEventListener('scroll', updatePosition);
+    window.removeEventListener('scroll', scrollCallback, true);
     window.removeEventListener('resize', updatePosition);
   });
 
