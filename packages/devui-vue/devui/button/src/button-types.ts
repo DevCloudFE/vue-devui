05b62d30d12c9118540462679a77f23db0cde0da
苏向夜 <fu050409@163.com>
2024-05-30 00:31:09 -0400

    docs(button): add `iconPos` for prop definition

--- packages/devui-vue/devui/button/src/button-types.ts
+++ packages/devui-vue/devui/button/src/button-types.ts
@@ -5,6 +5,7 @@ export type IButtonColor = 'secondary' | 'primary' | 'danger';
 export type IButtonSize = 'lg' | 'md' | 'sm';
 export type IButtonShape = 'round' | 'circle';
 export type IButtonType = 'button' | 'submit' | 'reset';
+export type IButtonIconPos = 'left' | 'right';
 
 export const buttonProps = {
   variant: {
@@ -23,7 +24,7 @@ export const buttonProps = {
     default: '',
   },
   iconPos: {
-    type: String as PropType<'left' | 'right'>,
+    type: String as PropType<IButtonIconPos>,
     default: 'left',
   },
   loading: {
