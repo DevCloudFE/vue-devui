e4815e33b8115e8093d7f9b7d6551ce536373df4
苏向夜 <fu050409@163.com>
2024-06-21 17:55:28 +0800

    fix(demo): try use official demo block instead of custom
    Related Issue: #1879

---
+++ packages/devui-vue/docs/.vitepress/theme/useComponents.js
@@ -0,0 +1,10 @@
+// Don't remove this file, because it registers the demo components.
+import Demo from 'vitepress-theme-demoblock/dist/client/components/Demo.vue'
+import DemoBlock from 'vitepress-theme-demoblock/dist/client/components/DemoBlock.vue'
+// import Demo from '../demo/Demo.vue';
+// import DemoBlock from '../demo/DemoBlock.vue';
+
+export function useComponents(app) {
+  app.component('Demo', Demo)
+  app.component('DemoBlock', DemoBlock)
+}
